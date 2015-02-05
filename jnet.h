f2cc31f3581c2945e17cda0c761ab8920c74bd75
Deniz Yuret <denizyuret@gmail.com>
2015-02-05 18:18:33 +0200

    Added forwback.

--- jnet.h
+++ jnet.h
@@ -8,6 +8,7 @@ Layer soft(int wrows, int wcols, float *w, float *b);
 void lfree(Layer l);
 int lsize(Layer l, int i);
 void forward(Layer *net, float *x, float *y, int nlayer, int xcols, int batch);
+void forwback(Layer *net, float *x, float *y, int nlayer, int xcols, int batch);
 
 #ifdef __cplusplus
 }
