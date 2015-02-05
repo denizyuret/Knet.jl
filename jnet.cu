66324ad26f358a39844af594e64cb2749ef42ce3
Deniz Yuret <denizyuret@gmail.com>
2015-02-05 22:13:50 +0200

    Started update().

--- jnet.cu
+++ jnet.cu
@@ -1,9 +1,16 @@
 // nvcc --shared --compiler-options -fPIC -o libjnet.so jnet.cu -lcublas
+// TODO:
+// train
+// update: check L2, momentum, nesterov
+// update: implement maxnorm, L1
+// dropout
+// compare with caffe
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
 #include <cublas.h>
+#include <assert.h>
 #include "jnet.h"
 #define BLK 128
 #define THR 128
@@ -11,28 +18,34 @@
 typedef enum { NOOP, RELU, SOFT } LayerType;
 
 typedef struct LayerS {
+  int wrows, wcols, xcols;	// size params
+
   LayerType type; // type of activation function	
   float *w;	// weight matrix (wrows,wcols)
   float *b;	// bias vector (wrows)
+  float *x;	// last input (wcols,xcols)
+  float *y;	// last output (wrows,xcols)
+  float *xones;	// vector of ones for bias calculation (xcols)
+  float *xmask;	// input mask for dropout
 
   float *dw;	// gradient wrt weight matrix
+  float *db;	// gradient wrt bias vector
+  float *dx;	// gradient wrt input
+  float *dy;	// gradient wrt output
+
   float *dw1;	// moving average of gradients for momentum
   float *dw2;	// sum of squared gradients for adagrad
-
-  float *db;	// gradient wrt bias vector
   float *db1;	// moving average of gradients for momentum
   float *db2;	// sum of squared gradients for adagrad
 
-  float *x;	// last input (wcols,xcols)
-  float *y;	// last output (wrows,xcols)
-  float *dx;	// gradient wrt input
-  float *dy;	// gradient wrt output
-  float *xmask;	// input mask for dropout
-  float *xones;	// vector of ones for bias calculation (xcols)
+  bool adagrad;
+  bool nesterov;
+  float learningRate;	// default=0, acts like 1.0
+  float momentum;
+  float dropout;
+  float maxnorm;	// default=0, acts like inf
+  float L1, L2;
 
-  int wrows;
-  int wcols;
-  int xcols;
 } *Layer;
 
 
@@ -41,6 +54,7 @@ float *forw(Layer l, float *x, int xcols);
 float *initforw(Layer l, float *x, int xcols);
 float *back(Layer l, float *dy, bool dx);
 float *initback(Layer l, float *dy, bool dx);
+void initupdate(Layer l);
 
 __global__ void _fill(float *y, int n, float val);
 __global__ void _reluforw(float *y, int n);
@@ -48,59 +62,57 @@ __global__ void _reluback(float *dy, float *y, int n);
 __global__ void _softback(float *dy, float *y, int nrows, int ncols);
 
 
-#define CUDA(_n,_s) {				\
-    cudaError_t err = (_s);\
-    if (err != cudaSuccess) {\
-      fprintf(stderr, "Error[%d]: %s\n", _n, cudaGetErrorString(err)); \
-      exit(EXIT_FAILURE);\
-    }\
-  }
-
-
-#define NOTNULL(_n,_s) {				\
-    if ((_s) == NULL) {\
-      fprintf(stderr, "Error[%d]: Unexpected null value\n", _n);	\
-      exit(EXIT_FAILURE);\
-    }\
-  }
+#define CUDA(_s) assert((_s) == cudaSuccess)
+#define NOTNULL(_s) assert((_s) != NULL)
 
 
 Layer layer(LayerType type, int wrows, int wcols, float *w, float *b) {
   Layer l;
-  NOTNULL(1,w);
-  NOTNULL(2,l = (Layer) calloc(1, sizeof(struct LayerS)));
+  NOTNULL(w);
+  NOTNULL(l = (Layer) calloc(1, sizeof(struct LayerS)));
   l = (Layer) calloc(1, sizeof(struct LayerS));
   l->type = type;
   l->wrows = wrows;
   l->wcols = wcols;
   int wsize = wrows*wcols*sizeof(float);
-  CUDA(3,cudaMalloc((void **) &l->w, wsize));
-  CUDA(4,cudaMemcpy(l->w, w, wsize, cudaMemcpyHostToDevice));
+  CUDA(cudaMalloc((void **) &l->w, wsize));
+  CUDA(cudaMemcpy(l->w, w, wsize, cudaMemcpyHostToDevice));
   if (b != NULL) {
     int bsize = wrows*sizeof(float);
-    CUDA(5,cudaMalloc((void **) &l->b, bsize));
-    CUDA(6,cudaMemcpy(l->b, b, bsize, cudaMemcpyHostToDevice));
+    CUDA(cudaMalloc((void **) &l->b, bsize));
+    CUDA(cudaMemcpy(l->b, b, bsize, cudaMemcpyHostToDevice));
   }
   return(l);
 }
 
 extern "C" void lfree(Layer l) {
-  CUDA(8,cudaFree(l->w));
-  CUDA(8,cudaFree(l->b));
-  CUDA(8,cudaFree(l->dw));
-  //CUDA(8,cudaFree(l->dw1));
-  //CUDA(8,cudaFree(l->dw2));
-  CUDA(8,cudaFree(l->db));
-  //CUDA(8,cudaFree(l->db1));
-  //CUDA(8,cudaFree(l->db2));
-  //CUDA(8,cudaFree(l->x));   // taken as input, not alloced
-  CUDA(8,cudaFree(l->y));   
-  CUDA(8,cudaFree(l->dx));
-  //CUDA(8,cudaFree(l->dy));  // taken as input, not alloced
-  //CUDA(8,cudaFree(l->xmask));
-  CUDA(8,cudaFree(l->xones));
+  CUDA(cudaFree(l->w));
+  CUDA(cudaFree(l->b));
+  //CUDA(cudaFree(l->x));   // taken as input, not alloced
+  CUDA(cudaFree(l->y));   
+  CUDA(cudaFree(l->xones));
+  //CUDA(cudaFree(l->xmask));
+
+  CUDA(cudaFree(l->dw));
+  CUDA(cudaFree(l->db));
+  CUDA(cudaFree(l->dx));
+  //CUDA(cudaFree(l->dy));  // taken as input, not alloced
+
+  CUDA(cudaFree(l->dw1));
+  CUDA(cudaFree(l->dw2));
+  CUDA(cudaFree(l->db1));
+  CUDA(cudaFree(l->db2));
   free(l);
 }
+
+extern "C" void adagrad(Layer l, int i) { l->adagrad = i; }
+extern "C" void nesterov(Layer l, int i) { l->nesterov = i; }
+extern "C" void learningRate(Layer l, float lr) { l->learningRate = lr; }
+extern "C" void momentum(Layer l, float m) { l->momentum = m; }
+extern "C" void dropout(Layer l, float d) { l->dropout = d; }
+extern "C" void maxnorm(Layer l, float m) { l->maxnorm = m; }
+extern "C" void L1(Layer l, float m) { l->L1 = m; }
+extern "C" void L2(Layer l, float m) { l->L2 = m; }
 
 extern "C" int lsize(Layer l, int i) { 
   switch(i) {
@@ -125,21 +137,21 @@ extern "C" void forward(Layer *net, float *x, float *y, int nlayer, int xcols, i
   int yrows = net[nlayer-1]->wrows;
   int xsize = xrows * batch * sizeof(float);
   int ysize = yrows * batch * sizeof(float);
-  CUDA(9,cudaMalloc((void **) &xgpu, xsize));
+  CUDA(cudaMalloc((void **) &xgpu, xsize));
   for (int b = 0; b < xcols; b += batch) {
     if (b + batch > xcols) {
       batch = xcols - b;
       xsize = xrows * batch * sizeof(float);
       ysize = yrows * batch * sizeof(float);
     }
-    CUDA(10,cudaMemcpy(xgpu, &x[b * xrows], xsize, cudaMemcpyHostToDevice));
+    CUDA(cudaMemcpy(xgpu, &x[b * xrows], xsize, cudaMemcpyHostToDevice));
     float *ygpu = xgpu;
     for (int l = 0; l < nlayer; l++) {
       ygpu = forw(net[l], ygpu, batch);
     }
-    CUDA(11,cudaMemcpy(&y[b * yrows], ygpu, ysize, cudaMemcpyDeviceToHost));
+    CUDA(cudaMemcpy(&y[b * yrows], ygpu, ysize, cudaMemcpyDeviceToHost));
   }
-  CUDA(12,cudaFree(xgpu));
+  CUDA(cudaFree(xgpu));
 }
 
 extern "C" void forwback(Layer *net, float *x, float *y, int nlayer, int xcols, int batch) {
@@ -148,27 +160,26 @@ extern "C" void forwback(Layer *net, float *x, float *y, int nlayer, int xcols, 
   int yrows = net[nlayer-1]->wrows;
   int xsize = xrows * batch * sizeof(float);
   int ysize = yrows * batch * sizeof(float);
-  CUDA(13,cudaMalloc((void **) &xgpu, xsize));
-  CUDA(14,cudaMalloc((void **) &ygpu, ysize));
+  CUDA(cudaMalloc((void **) &xgpu, xsize));
+  CUDA(cudaMalloc((void **) &ygpu, ysize));
   for (int b = 0; b < xcols; b += batch) {
     if (b + batch > xcols) {
       batch = xcols - b;
       xsize = xrows * batch * sizeof(float);
       ysize = yrows * batch * sizeof(float);
     }
-    CUDA(15,cudaMemcpy(xgpu, &x[b * xrows], xsize, cudaMemcpyHostToDevice));
+    CUDA(cudaMemcpy(xgpu, &x[b * xrows], xsize, cudaMemcpyHostToDevice));
     float *xptr = xgpu;
     for (int l = 0; l < nlayer; l++)
       xptr = forw(net[l], xptr, batch);
-    CUDA(16,cudaMemcpy(ygpu, &y[b * yrows], ysize, cudaMemcpyHostToDevice));
+    CUDA(cudaMemcpy(ygpu, &y[b * yrows], ysize, cudaMemcpyHostToDevice));
     float *yptr = ygpu;
     for (int l = nlayer - 1; l >= 0; l--)
       yptr = back(net[l], yptr, (l>0));
   }
-  CUDA(17,cudaFree(xgpu));
-  CUDA(18,cudaFree(ygpu));
+  CUDA(cudaFree(xgpu));
+  CUDA(cudaFree(ygpu));
 }
-
 
 
 float *forw(Layer l, float *x, int xcols) {
@@ -190,10 +201,33 @@ float *forw(Layer l, float *x, int xcols) {
   switch(l->type) {
   case RELU:
     _reluforw<<<BLK,THR>>>(l->y, l->wrows * l->xcols);
-    CUDA(19,cudaGetLastError());
+    CUDA(cudaGetLastError());
     break;
   }
   return(l->y);
+}
+
+float *initforw(Layer l, float *x, int xcols) {
+  // Alloc/realloc l->y and l->xones if necessary
+  // Update l->xcols
+  int yrows = l->wrows;
+  int ycols = xcols;
+  if ((l->y == NULL) || (l->xcols != xcols)) {
+    CUDA(cudaFree(l->y));
+    CUDA(cudaMalloc((void **) &l->y, yrows * ycols * sizeof(float)));
+  }
+  if ((l->b != NULL) && ((l->xones == NULL) || l->xcols != xcols)) {
+    CUDA(cudaFree(l->xones));
+    CUDA(cudaMalloc((void **) &l->xones, xcols * sizeof(float)));
+    _fill<<<BLK,THR>>>(l->xones, xcols, 1.0);
+    CUDA(cudaGetLastError());
+  }
+  if ((l->dx != NULL) && (l->xcols != xcols)) {
+    CUDA(cudaFree(l->dx));
+    l->dx = NULL;
+  }
+  l->xcols = xcols;
+  return x;
 }
 
 float *back(Layer l, float *dy, bool dx) {
@@ -206,11 +240,11 @@ float *back(Layer l, float *dy, bool dx) {
   switch(l->type) {
   case RELU:
     _reluback<<<BLK,THR>>>(l->dy, l->y, l->wrows * l->xcols);
-    CUDA(20,cudaGetLastError());
+    CUDA(cudaGetLastError());
     break;
   case SOFT:
     _softback<<<BLK,THR>>>(l->dy, l->y, l->wrows, l->xcols);
-    CUDA(21,cudaGetLastError());
+    CUDA(cudaGetLastError());
     break;
   }
 
@@ -233,37 +267,76 @@ float *back(Layer l, float *dy, bool dx) {
   return l->dx;
 }
 
-float *initforw(Layer l, float *x, int xcols) {
-  // Alloc/realloc l->y and l->xones if necessary
-  // Update l->xcols
-  int yrows = l->wrows;
-  int ycols = xcols;
-  if ((l->y == NULL) || (l->xcols != xcols)) {
-    CUDA(22,cudaFree(l->y));
-    CUDA(23,cudaMalloc((void **) &l->y, yrows * ycols * sizeof(float)));
-  }
-  if ((l->b != NULL) && ((l->xones == NULL) || l->xcols != xcols)) {
-    CUDA(24,cudaFree(l->xones));
-    CUDA(25,cudaMalloc((void **) &l->xones, xcols * sizeof(float)));
-    _fill<<<BLK,THR>>>(l->xones, xcols, 1.0);
-    CUDA(26,cudaGetLastError());
-  }
-  if ((l->dx != NULL) && (l->xcols != xcols)) {
-    CUDA(27,cudaFree(l->dx));
-    l->dx = NULL;
-  }
-  l->xcols = xcols;
-  return x;
-}
-
 float *initback(Layer l, float *dy, bool dx) {
   if (l->dw == NULL)
-    CUDA(28,cudaMalloc((void **) &l->dw, l->wrows * l->wcols * sizeof(float)));
+    CUDA(cudaMalloc((void **) &l->dw, l->wrows * l->wcols * sizeof(float)));
   if ((l->b != NULL) && (l->db == NULL))
-    CUDA(29,cudaMalloc((void **) &l->db, l->wrows * sizeof(float)));
+    CUDA(cudaMalloc((void **) &l->db, l->wrows * sizeof(float)));
   if (dx && (l->dx == NULL))
-    CUDA(30,cudaMalloc((void **) &l->dx, l->wcols * l->xcols * sizeof(float)));
+    CUDA(cudaMalloc((void **) &l->dx, l->wcols * l->xcols * sizeof(float)));
   return dy;
+}
+
+extern "C" void update(Layer l) {
+  initupdate(l);
+  int nw = l->wcols * l->wrows;
+  int nb = (l->b == NULL ? 0 : l->wrows);
+  if (l->L1 != 0) {
+    assert(1 == 0); // TBD
+  }
+  if (l->L2 != 0) { // dw += L2 * w
+    // axpy(n,α,x(n),incx=1,y(n),incy=1): y = α x + y
+    cublasSaxpy(nw, l->L2, l->w, 1, l->dw, 1);
+    // We do not apply L2 to l->b??
+  }
+  if (l->adagrad) { // dw2 += dw.*dw; dw /= (epsilon + sqrt(dw2))
+    assert(1 == 0); // TBD
+  }
+  if (l->learningRate != 0) {  // dw *= learningRate
+    // scal(n,α,x(n),incx=1): x = α x
+    cublasSscal(nw, l->learningRate, l->dw, 1);
+    if (nb) cublasSscal(nb, l->learningRate, l->db, 1);
+  }
+  if (l->momentum != 0) {  // dw1 = momentum * dw1 + dw
+    // do we apply momentum to db?
+    cublasSscal(nw, l->momentum, l->dw1, 1);
+    cublasSaxpy(nw, 1.0, l->dw, 1, l->dw1, 1);
+    if (l->nesterov) {	// TODO: check this!
+      cublasSaxpy(nw, l->momentum, l->dw1, 1, l->dw, 1);
+    } else {
+      cublasScopy(nw, l->dw1, 1, l->dw, 1);
+    }
+  }
+  cublasSaxpy(nw, -1.0, l->dw, 1, l->w, 1);  // w -= dw
+  if (nb) cublasSaxpy(nb, -1.0, l->db, 1, l->b, 1);  // b -= db
+  if (l->maxnorm != 0) {
+    assert(1 == 0);  // TBD
+  }
+}
+
+void initupdate(Layer l) {
+  if (l->adagrad && (l->dw2 == NULL)) {
+    CUDA(cudaMalloc((void **) &(l->dw2), l->wcols*l->wrows*sizeof(float)));
+    _fill<<<BLK,THR>>>(l->dw2, l->wcols*l->wrows, 0.0);
+    CUDA(cudaGetLastError());
+    if (l->b != NULL) {
+      assert(l->db2 == NULL);
+      CUDA(cudaMalloc((void **) &(l->db2), l->wrows*sizeof(float)));
+      _fill<<<BLK,THR>>>(l->db2, l->wrows, 0.0);
+      CUDA(cudaGetLastError());
+    }
+  }
+  if ((l->momentum != 0) && (l->dw1 == NULL)) {
+    CUDA(cudaMalloc((void **) &(l->dw1), l->wcols*l->wrows*sizeof(float)));
+    _fill<<<BLK,THR>>>(l->dw1, l->wcols*l->wrows, 0.0);
+    CUDA(cudaGetLastError());
+    if (l->b != NULL) {
+      assert(l->db1 == NULL);
+      CUDA(cudaMalloc((void **) &(l->db1), l->wrows*sizeof(float)));
+      _fill<<<BLK,THR>>>(l->db1, l->wrows, 0.0);
+      CUDA(cudaGetLastError());
+    }
+  }
 }
 
 
