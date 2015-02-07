60915501c74ad95f63c79aa2754cefb8a25bd921
Deniz Yuret <denizyuret@gmail.com>
2015-02-07 02:34:11 +0200

    *** empty log message ***

--- cuda/jnet.h
+++ cuda/jnet.h
@@ -11,17 +11,17 @@ int lsize(Layer l, int i);
 void forward(Layer *net, float *x, float *y, int nlayer, int xcols, int batch);
 void forwback(Layer *net, float *x, float *y, int nlayer, int xcols, int batch);
 void update(Layer l);
-float *forw(Layer l, float *x, int xcols);
-float *back(Layer l, float *dy, int dx);
+float *lforw(Layer l, float *x, int xcols);
+float *lback(Layer l, float *dy, int dx);
 
-void adagrad(Layer l, int i);
-void nesterov(Layer l, int i);
-void learningRate(Layer l, float lr);
-void momentum(Layer l, float m);
-void dropout(Layer l, float d);
-void maxnorm(Layer l, float m);
-void L1(Layer l, float m);
-void L2(Layer l, float m);
+void set_adagrad(Layer l, int i);
+void set_nesterov(Layer l, int i);
+void set_learningRate(Layer l, float lr);
+void set_momentum(Layer l, float m);
+void set_dropout(Layer l, float d);
+void set_maxnorm(Layer l, float m);
+void set_L1(Layer l, float m);
+void set_L2(Layer l, float m);
 
 #ifdef __cplusplus
 }
