ea969bab29093de3fa0c1c98afa5bff3e8ac130f
Deniz Yuret <denizyuret@gmail.com>
2015-02-08 09:54:49 +0200

    Moved struct LayerS to jnet.h for testing.

--- cuda/jnet.cu
+++ cuda/jnet.cu
@@ -15,40 +15,6 @@
 #define BLK 128
 #define THR 128
 
-typedef enum { NOOP=0, RELU=1, SOFT=2 } LayerType;
-
-struct LayerS {
-  int wrows, wcols, xcols; // size params
-
-  LayerType type;	// type of activation function	
-  float *w;		// weight matrix (wrows,wcols)
-  float *b;		// bias vector (wrows)
-  float *x;		// last input (wcols,xcols)
-  float *y;		// last output (wrows,xcols)
-  float *xones;		// vector of ones for bias calculation (xcols)
-  float *xmask;		// input mask for dropout
-
-  float *dw;		// gradient wrt weight matrix
-  float *db;		// gradient wrt bias vector
-  float *dx;		// gradient wrt input
-  float *dy;		// gradient wrt output
-
-  float *dw1;		// moving average of gradients for momentum
-  float *dw2;		// sum of squared gradients for adagrad
-  float *db1;		// moving average of gradients for momentum
-  float *db2;		// sum of squared gradients for adagrad
-
-  int adagrad;		// [0] adagrad during weight updates
-  int nesterov;		// [0] nesterov during weight updates
-  float learningRate;	// [0.01]
-  float momentum;	// [0]
-  float dropout;	// [0] probability of dropping inputs
-  float maxnorm;	// [0] default=0, acts like inf
-  float L1, L2;		// [0,0] L1,L2 regularization
-};
-
-
-Layer layer(LayerType type, int wrows, int wcols, float *w, float *b);
 static inline float *gpuArray(int nfloats);
 static inline float *gpuCopy(int nfloats, float *cpuArray);
 static inline float *gpuFill(int nfloats, float val);
