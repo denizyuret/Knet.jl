fe97d9f8c7147ff7262fdcbdda0db1898cb333cf
Deniz Yuret <denizyuret@gmail.com>
2015-02-13 21:30:29 +0200

    *** empty log message ***

--- test/predict.m
+++ test/predict.m
@@ -1,11 +1,15 @@
 function predict(varargin)
     batch = 128;
     assert(nargin >= 3);
+    fprintf(2, 'Reading... ');
     x = h5read(varargin{1}, '/data');
     for i=2:(numel(varargin)-1)
         net{i-1} = h5read_layer(varargin{i});
     end
-    net = copynet(net, 'gpu');
+    if gpuDeviceCount > 0
+        net = copynet(net, 'gpu');
+    end
+    fprintf(2, 'predicting... ');
     tic; y = gather(forward(net, x, batch)); 
     fprintf(2, '%g seconds\n', toc);
     try delete(varargin{end});
