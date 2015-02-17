98af1a15b31c3ca1647c1913d378be4886111e87
Deniz Yuret <denizyuret@gmail.com>
2015-02-17 14:05:30 +0200

    Passed cpu predict test

--- test/predict.jl
+++ test/predict.jl
@@ -1,21 +1,12 @@
 using HDF5
 include("../julia/kunet.jl")
+include("h5read_layer.jl")
+blas_set_num_threads(20)
 @time x = h5read(ARGS[1], "/data")
-# TODO: Replace this with h5read_layer:
-@time w1 = h5read(ARGS[2], "/w")
-@time b1 = h5read(ARGS[2], "/b")
-@time w2 = h5read(ARGS[3], "/w")
-@time b2 = h5read(ARGS[3], "/b")
-l1 = KUnet.Layer()
-l1.w = w1
-l1.b = b1
-l1.xforw! = KUnet.noop
-l1.yforw! = KUnet.reluforw!
-l2 = KUnet.Layer()
-l2.w = w2
-l2.b = b2
-l2.xforw! = KUnet.noop
-l2.yforw! = KUnet.noop
-@time y = KUnet.forw!(l1, x)
-@time y = KUnet.forw!(l2, y)
+@time l1 = h5read_layer(ARGS[2])
+@time l2 = h5read_layer(ARGS[3])
+l1.xforw = KUnet.noop
+l2.xforw = KUnet.noop
+net = [l1,l2]
+@time y = KUnet.predict(net, x, 937)
 @time h5write(ARGS[4], "data", y)
