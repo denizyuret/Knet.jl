5a7db8daef879ff7b1d0b3781f697ca7f3abfe69
Deniz Yuret <denizyuret@gmail.com>
2015-02-18 11:35:18 +0200

    CUDA extensions; the rest of the code is generic.

---
+++ julia/cuda.jl
@@ -0,0 +1,9 @@
+# CUDA extensions:
+using CUDArt
+using CUBLAS
+typealias Cmat Ptr{Float32}
+const libjnet = find_library(["libjnet"], ["."])
+import InplaceOps: mul!, badd!
+mul!(O::CudaVecOrMat, A::CudaVecOrMat, B::CudaVecOrMat) = CUBLAS.gemm!('N','N',one(eltype(A)),A,B,zero(eltype(A)),O)  # InplaceOps.jl:53
+badd!(::Type{InplaceOps.Inplace{1}}, A::CudaMatrix, B::CudaVecOrMat) = ccall((:badd,libjnet),Void,(Cint,Cint,Cmat,Cmat),size(A,1),size(A,2),A,B) # InplaceOps.jl:83
+reluforw(l,y::CudaArray)=ccall((:reluforw,libjnet),Void,(Cint,Cmat),length(y),y)
