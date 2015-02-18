62d7ca3665154a850839539fda58e20baa2410d6
Deniz Yuret <denizyuret@gmail.com>
2015-02-18 10:52:32 +0200

    Julia cpu version.

---
+++ test/predictcpu.jl
@@ -0,0 +1,15 @@
+using HDF5
+include("../julia/kunet.jl")
+include("h5read_layer.jl")
+blas_set_num_threads(20)
+@time x = h5read(ARGS[1], "/data")
+@time l1 = h5read_layer(ARGS[2])
+@time l2 = h5read_layer(ARGS[3])
+l1.xforw = KUnet.noop
+l2.xforw = KUnet.noop
+net = [l1,l2]
+@time gc()
+@time y = KUnet.predict(net, x, 937)
+@time y = KUnet.predict(net, x, 937)
+# Profile.print()
+@time h5write(ARGS[4], "data", y)
