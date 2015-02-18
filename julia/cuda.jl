234a70c16d3fb7eefc93745b4184675cddb48bad
Deniz Yuret <denizyuret@gmail.com>
2015-02-18 16:08:39 +0200

    Added gpumem.

--- julia/cuda.jl
+++ julia/cuda.jl
@@ -5,9 +5,6 @@ typealias Cmat Ptr{Float32}
 const libjnet = find_library(["libjnet"], ["."])
 import InplaceOps: mul!, badd!
 badd!(::Type{InplaceOps.Inplace{1}}, A::CudaMatrix, B::CudaVecOrMat) = ccall((:badd,libjnet),Void,(Cint,Cint,Cmat,Cmat),size(A,1),size(A,2),A,B) # InplaceOps.jl:83
-reluforw(y::CudaArray)=ccall((:reluforw,libjnet),Void,(Cint,Cmat),length(y),y)
-reluback(y::CudaArray,dy::CudaArray)=ccall((:reluback,libjnet),Void,(Cint,Cmat,Cmat),length(y),y,dy)
-softback(y::CudaArray,dy::CudaArray)=ccall((:softback,libjnet),Void,(Cint,Cint,Cmat,Cmat),size(y,1),size(y,2),y,dy)
 
 import Base: ctranspose         # TODO: these don't hang high enough in the type hierarchy
 import InplaceOps: Transpose, mul!
@@ -26,3 +23,11 @@ mul!(O::CudaVecOrMat, A::CudaVecOrMat, B::Transpose) = CUBLAS.gemm!('N','T',one(
 
 import Base: sum!  # TODO: add error checking here since this is not a full implementation of sum!
 sum!(r::CudaVecOrMat, A::CudaMatrix) = ccall((:bsum,libjnet),Void,(Cint,Cint,Cmat,Cmat),size(A,1),size(A,2),A,r) # reducedim.jl:226
+
+# For debugging
+function gpumem()
+    mfree=Csize_t[1]
+    mtotal=Csize_t[1]
+    ccall((:cudaMemGetInfo,"libcudart.so"),Cint,(Ptr{Csize_t},Ptr{Csize_t}),mfree,mtotal)
+    convert(Int,mfree[1])
+end
