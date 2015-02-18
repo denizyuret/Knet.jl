b1448e2283b740796b041db3ea2cf3f6a8921ff1
Deniz Yuret <denizyuret@gmail.com>
2015-02-18 16:08:57 +0200

    cleanup

--- julia/util.jl
+++
@@ -1,7 +0,0 @@
-function gpumem()
-    mfree=Csize_t[1]
-    mtotal=Csize_t[1]
-    ccall((:cudaMemGetInfo,"libcudart.so"),Cint,(Ptr{Csize_t},Ptr{Csize_t}),mfree,mtotal)
-    convert(Int,mfree[1])
-end
-
