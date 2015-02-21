e754aaf91c8250780365ea5b9b2307742bdd3bd3
Deniz Yuret <denizyuret@gmail.com>
2015-02-21 18:30:22 +0200

    New interface.

--- src/update.jl
+++ src/update.jl
@@ -1,100 +1,74 @@
 # TODO: express the math in readable and generic form 
+# TODO: use blas for cpu versions to take advantage of multicore.
 
-type TrainOpts adagrad; batch; dropout; iters; l1reg; l2reg; learningRate; maxnorm; momentum; nesterov; 
-TrainOpts()=new(0f0,    128,   0f0,     0,     0f0,   0f0,   0.01f0,       0f0,     0f0,      0f0) end
+using InplaceOps
 
-function update(l::Layer, o::TrainOpts)
-    initupdate(l, o)
-    if o.l1reg > 0f0
-        l1reg!(o.l1reg, l.w, l.dw)  # l.dw += o.l1reg * sign(l.w)
+function update(w, dw, o::UpdateParam)
+    initupdate(w, dw, o)
+    isdefined(o,:l1reg) && (o.l1reg > zero(o.l1reg)) && l1reg!(o.l1reg, w, dw)
+    isdefined(o,:l2reg) && (o.l2reg > zero(o.l2reg)) && l2reg!(o.l2reg, w, dw)
+    isdefined(o,:adagrad) && (o.adagrad > zero(o.adagrad)) && adagrad!(o.adagrad, o.ada, dw)
+    isdefined(o,:learningRate) && (o.learningRate != one(o.learningRate)) && (@in1! dw .* o.learningRate)
+    isdefined(o,:momentum) && (o.momentum > zero(o.momentum)) && momentum!(o.momentum, o.mom, dw)
+    isdefined(o,:nesterov) && (o.nesterov > zero(o.nesterov)) && nesterov!(o.nesterov, o.nes, dw)
+    @in1! w .- dw
+    isdefined(o,:maxnorm) && (o.maxnorm > zero(o.maxnorm)) && maxnorm!(o.maxnorm, w)
+end
+
+using Base.LinAlg
+
+l1reg!(l1, w, dw)=(for i=1:length(dw) (w[i]>zero(w[i])) ? (dw[i]+=l1) : (w[i]<zero(w[i])) ? (dw[i]-=l1) : 0 end)
+l1reg!(l1, w::CudaMatrix, dw::CudaMatrix)=ccall((:l1reg,libkunet),Void,(Cint,Cfloat,Cmat,Cmat),length(dw),l1,w,dw)
+
+l2reg!(l2, w, dw)=axpy!(length(dw), l2, w, 1, dw, 1)
+l2reg!(l2, w::CudaMatrix, dw::CudaMatrix) = CUBLAS.axpy!(length(dw), l2, w, 1, dw, 1)
+
+adagrad!(eps, dw2, dw)=(for i=1:length(dw) (dw2[i] += dw[i] * dw[i]; dw[i] /= (eps + sqrt(dw2[i]))) end)
+adagrad!(eps, dw2::CudaMatrix, dw::CudaMatrix)=ccall((:adagrad,libkunet),Void,(Cint,Cfloat,Cmat,Cmat),length(dw),eps,dw2,dw)
+
+momentum!(m, dw2, dw)=(axpy!(length(dw), m, dw2, 1, dw, 1);copy!(dw2,dw))
+momentum!(m, dw2::CudaMatrix, dw::CudaMatrix)=(CUBLAS.axpy!(length(dw), m, dw2, 1, dw, 1);copy!(dw2,dw))
+
+nesterov!(m, dw2, dw)=(nw=length(dw);scal!(nw, m, dw2, 1); axpy!(nw, one(eltype(dw)), dw, 1, dw2, 1); axpy!(nw, m, dw2, 1, dw, 1))
+nesterov!(m, dw2::CudaMatrix, dw::CudaMatrix)=(nw=length(dw);CUBLAS.scal!(nw, m, dw2, 1); CUBLAS.axpy!(nw, one(eltype(dw)), dw, 1, dw2, 1); CUBLAS.axpy!(nw, m, dw2, 1, dw, 1))
+
+function maxnorm!(maxnorm, w)
+    error("Did not debug maxnorm yet.")
+    norms = sqrt(sum(w.^2, 2))
+    if any(norms > o.maxnorm)
+        scale = min(o.maxnorm ./ norms, 1)
+        w *= scale
     end
-    if o.l2reg > 0f0
-        l2reg!(o.l2reg, l.w, l.dw)  # l.dw += o.l2reg * l.w
+end
+
+function initupdate(w, dw, o::UpdateParam)
+    isdefined(o,:adagrad) && (o.adagrad > zero(o.adagrad)) && chksize(o, :ada, dw)
+    isdefined(o,:momentum) && (o.momentum > zero(o.momentum)) && chksize(o, :mom, dw)
+    isdefined(o,:nesterov) && (o.nesterov > zero(o.nesterov)) && chksize(o, :nes, dw)
+end
+
+chksize(o, n, a)=(!isdefined(o,n) ? (o.(n) = zeros(a)) : assert(size(o.(n)) == size(a)))
+
+
+function setparam!(l::Layer,k,v)
+    if (k == :dropout)
+        l.dropout = v
+        (v > zero(v)) && (l.fx = drop)
+        return
     end
-    if o.adagrad > 0f0
-        adagrad!(o.adagrad, l.dw2, l.dw) # l.dw2 += l.dw .* l.dw; l.dw /= o.adagrad + sqrt(l.dw2)
-        adagrad!(o.adagrad, l.db2, l.db) # l.db2 += l.db .* l.db; l.db /= o.adagrad + sqrt(l.db2)
+    if isdefined(l, :w)
+        isdefined(l, :pw) || (l.pw = UpdateParam())
+        setparam!(l.pw, k, v)
     end
-    if o.learningRate != 1.0f0
-        @in1! l.dw .* o.learningRate
-        @in1! l.db .* o.learningRate
-    end
-    if o.momentum > 0f0
-        assert(o.nesterov == 0f0)
-        momentum!(o.momentum, l.dw1, l.dw)  # l.dw1 = o.momentum * l.dw1 + l.dw; l.dw = l.dw1
-        momentum!(o.momentum, l.db1, l.db)  # l.db1 = o.momentum * l.db1 + l.db; l.db = l.db1
-    end
-    if o.nesterov > 0f0
-        assert(o.momentum == 0f0)
-        nesterov!(o.nesterov, l.dw1, l.dw)  # l.dw1 = o.nesterov * l.dw1 + l.dw; l.dw = o.nesterov * l.dw1 + l.dw; 
-        nesterov!(o.nesterov, l.db1, l.db)  # l.db1 = o.nesterov * l.db1 + l.db; l.db = o.nesterov * l.db1 + l.db; 
-    end
-    @in1! l.w .- l.dw
-    @in1! l.b .- l.db
-    if o.maxnorm > 0f0
-        norms = sqrt(sum(w.^2, 2))
-        if any(norms > o.maxnorm)
-            scale = min(o.maxnorm ./ norms, 1)
-            l.w *= scale
+    if isdefined(l, :b)
+        isdefined(l, :pb) || (l.pb = UpdateParam())
+        if in(k, [:l1reg, :l2reg, :maxnorm]) && (v != zero(v))
+            warn("Skipping $k regularization for bias.")
+        else
+            setparam!(l.pb, k, v)
         end
     end
 end
 
-function initupdate(l, o)
-    if o.adagrad > 0f0
-        if (!isdefined(l,:dw2)) l.dw2 = zeros(l.dw) end
-        if (!isdefined(l,:db2)) l.db2 = zeros(l.db) end
-    end
-    if (o.momentum > 0f0 || o.nesterov > 0f0)
-        if (!isdefined(l,:dw1)) l.dw1 = zeros(l.dw) end
-        if (!isdefined(l,:db1)) l.db1 = zeros(l.db) end
-    end
-end
-
-l2reg!(l2::Float32, w::Matrix, dw::Matrix) =    Base.LinAlg.axpy!(length(dw), l2, w, 1, dw, 1)
-l2reg!(l2::Float32, w::CudaMatrix, dw::CudaMatrix) = CUBLAS.axpy!(length(dw), l2, w, 1, dw, 1)
-
-l1reg!(l1::Float32, w::CudaMatrix, dw::CudaMatrix)=ccall((:l1reg,libkunet),Void,(Cint,Cfloat,Cmat,Cmat),length(dw),l1,w,dw)
-
-function l1reg!(l1::Float32, w::Matrix, dw::Matrix)
-    for i=1:length(dw)
-        if (w[i] > 0f0) dw[i] += l1
-        elseif (w[i] < 0f0) dw[i] -= l1
-        end
-    end
-end
-
-adagrad!(eps::Float32, dw2::CudaMatrix, dw::CudaMatrix)=ccall((:adagrad,libkunet),Void,(Cint,Cfloat,Cmat,Cmat),length(dw),eps,dw2,dw)
-
-function adagrad!(eps::Float32, dw2::Matrix, dw::Matrix)
-    for i=1:length(dw)
-        dw2[i] += dw[i] * dw[i];
-        dw[i] /= (eps + sqrt(dw2[i]))
-    end
-end
-
-# TODO: use blas for gpu versions to take advantage of multicore.
-function momentum!(m::Float32, dw1::Matrix, dw::Matrix)
-    for i=1:length(dw)
-        dw1[i] = m * dw1[i] + dw[i]
-        dw[i]  = dw1[i]
-    end
-end
-
-function momentum!(m::Float32, dw1::CudaMatrix, dw::CudaMatrix)
-    CUBLAS.axpy!(length(dw), m, dw1, 1, dw, 1)
-    copy!(dw1, dw)
-end
-
-function nesterov!(m::Float32, dw1::Matrix, dw::Matrix)
-    for i=1:length(dw)
-        dw1[i] = m * dw1[i] + dw[i]
-        dw[i]  = m * dw1[i] + dw[i]
-    end
-end
-
-function nesterov!(m::Float32, dw1::CudaMatrix, dw::CudaMatrix)
-    CUBLAS.scal!(length(dw), m, dw1, 1)
-    CUBLAS.axpy!(length(dw), 1.0f0, dw, 1, dw1, 1)
-    CUBLAS.axpy!(length(dw), m, dw1, 1, dw, 1)
-end
+setparam!(p::UpdateParam,k,v)=(p.(k)=v)
+setparam!(net::Net,k,v)=for l=net setparam!(l,k,v) end
