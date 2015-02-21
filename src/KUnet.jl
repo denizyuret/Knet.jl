238461aae868a68c3ea9850dfe9b256500250ddd
Deniz Yuret <denizyuret@gmail.com>
2015-02-21 18:28:57 +0200

    Brought data types here.

--- src/KUnet.jl
+++ src/KUnet.jl
@@ -1,10 +1,23 @@
 module KUnet
 
+type Layer w; b; fx; fy; dw; db; pw; pb; y; x; dx; dropout; xdrop; Layer()=new() end
+typealias Net Array{Layer,1}
+type UpdateParam 
+    learningRate
+    l1reg
+    l2reg
+    maxnorm
+    adagrad;  ada
+    momentum; mom
+    nesterov; nes
+    UpdateParam()=new(0.01)
+end
+
 include("layer.jl")
-include("func.jl")
-include("cuda.jl")
-include("h5io.jl")
-include("update.jl")
 include("net.jl")
+include("cuda.jl")
+include("update.jl")
+include("func.jl")
+include("h5io.jl")
 
 end # module
