ab4b6b4251de61facc2d2dc465cf02726cb93b03
Deniz Yuret <denizyuret@gmail.com>
2015-02-21 17:57:35 +0200

    New function interface.

--- src/func.jl
+++ src/func.jl
@@ -1,81 +1,77 @@
-relu(w,b)=Layer(w,b,reluforw,reluback)
-soft(w,b)=Layer(w,b,softforw,softback)
+using CUDArt
+
+# ACTIVATION FUNCTION INTERFACE: We use the same name for the
+# activation function (relu, sigm, etc.) and its derivative.  That way
+# a layer can have a single function field and the user cannot pair a
+# function with the wrong derivative.  Let x be the layer input,
+# y1=wx+b the linear output, and y2=f(y1) the final output.  The
+# activation function takes y1 and overwrites it with y2.  The
+# derivative takes dy2, the gradient of the final output, and
+# overwrites it with dy1, the gradient of the linear output.  The
+# derivative needs to know y2, so it takes an extra argument and can
+# be distinguished from the activation function by its method
+# signature.  These functions modify their arguments in place and do
+# not return anything.  TODO: Implement sigmoid and maxout
+
+relu(l,y)=for i=1:length(y) y[i]<zero(y[i])&&(y[i]=zero(y[i])) end
+relu(l,y,dy)=for i=1:length(y) y[i]==zero(y[i])&&(dy[i]=zero(dy[i])) end
+relu(l,y::CudaArray)=ccall((:reluforw,libkunet),Void,(Cint,Cmat),length(y),y)
+relu(l,y::CudaArray,dy::CudaArray)=ccall((:reluback,libkunet),Void,(Cint,Cmat,Cmat),length(dy),y,dy)
+
+
+# PREPROCESSING FUNCTION INTERFACE: A preprocessing function
+# (e.g. dropout) modifies the input x before applying the layer.
+# Again, we use the same name for the function and its derivative and
+# the helpers.
+
+function drop(l, x)
+    if l.dropout > 0
+        resize(l, :xdrop, x)
+        rand!(l.xdrop)
+        drop(x, l.xdrop, l.dropout, 1/(1-l.dropout))
+    end
+end
+
+function drop(l, x, dx)
+    if l.dropout > 0
+        drop(dx, l.xdrop, l.dropout, 1/(1-l.dropout))
+    end
+end
+
+drop(x, xdrop, dropout, scale)=for i=1:length(x) x[i] = (xdrop[i] < dropout ? zero(x[i]) : scale * x[i]) end
+drop(x::CudaArray, xdrop::CudaArray, dropout, scale)=ccall((:drop,libkunet),Void,(Cint,Cmat,Cmat,Cfloat,Cfloat),length(x),x,xdrop,dropout,scale)
+
+
+# LOSS INTERFACE: A loss function takes y, the network output, and dy,
+# the desired output.  These should have the same dimensionality, so
+# use 1-of-k encoding for classification outputs.  It overwrites dy
+# with the gradient of the output y with respect to the loss function.
+
+function softmaxloss(y, dy)
+    yrows,ycols = size(y)
+    loss = zero(eltype(y))
+    prob = similar(y, yrows)
+    for j=1:ycols
+        ymax = y[1,j]
+        for i=2:yrows y[i,j] > ymax && (ymax = y[i,j]) end
+        psum = zero(ymax)
+        for i=1:yrows
+            yij = y[i,j] - ymax
+            prob[i] = exp(yij)
+            psum += prob[i]
+            dy[i,j] == 1 && (loss += yij)
+        end
+        loss -= log(psum)
+        for i=1:yrows
+            prob[i] /= psum
+            dy[i,j] = (prob[i] - dy[i,j]) / ycols
+        end
+    end
+    return loss
+end
+
+# TODO: This doesn't return the loss, just writes the gradient:
+softmaxloss(y::CudaArray,dy::CudaArray)=ccall((:softback,libkunet),Cfloat,(Cint,Cint,Cmat,Cmat),size(dy,1),size(dy,2),y,dy)
 
 noop(l,x)=x
-
-function reluforw(l,y)
-    for i=1:length(y)
-        if (y[i] < 0)
-            y[i] = 0
-        end
-    end
-    return y
-end
-
-function reluback(l, dy)
-    for i=1:length(dy)
-        if (l.y[i] <= 0)
-            dy[i] = 0
-        end
-    end
-    return dy
-end
-
-function softforw(l, y)
-    return y
-end
-
-function softback(l, dy)
-    # we do softmax here instead of in forw
-    # overwriting y from unnormalized log probabilities to normalized probabilities
-    # NumericExtensions.softmax!(y,y,1) allocates unnecessary memory
-    # dy is a 0-1 matrix of correct answers
-    # will overwrite it with the gradient
-    # TODO: is this a good interface?
-    # TODO: other types of final layers (sigmoid), losses (hinge, squared)?
-
-    y = l.y
-    for j=1:size(y,2)
-        ymax = y[1,j]
-        for i=2:size(y,1)
-            if (y[i,j] > ymax)
-                ymax = y[i,j]
-            end
-        end
-        ysum = zero(ymax)
-        for i=1:size(y,1)
-            y[i,j] = exp(y[i,j] - ymax)
-            ysum += y[i,j]
-        end
-        for i=1:size(y,1)
-            y[i,j] /= ysum
-            dy[i,j] = (y[i,j] - dy[i,j]) / size(y,2)
-        end
-    end
-    return dy
-end
-
-function dropforw(l, x, dropout)
-    resize(l, :xmask, x)
-    rand!(l.xmask)
-    drop(x, l.xmask, dropout, 1/(1-dropout))
-    return x
-end
-
-function dropback(l, x, dropout)
-    drop(x, l.xmask, dropout, 1/(1-dropout))
-    return x
-end
-
-function drop(x, xmask, dropout, scale)
-    for i=1:length(x)
-        x[i] = (xmask[i] < dropout ? zero(x[i]) : scale * x[i])
-    end
-end
-
-# CUDA extensions
-using CUDArt
-function reluforw(l,y::CudaArray)  ccall((:reluforw,libkunet),Void,(Cint,Cmat),length(y),y); y end
-function reluback(l,dy::CudaArray) ccall((:reluback,libkunet),Void,(Cint,Cmat,Cmat),length(dy),l.y,dy); dy end
-function softback(l,dy::CudaArray) ccall((:softback,libkunet),Void,(Cint,Cint,Cmat,Cmat),size(dy,1),size(dy,2),l.y,dy); dy end
-function drop(x::CudaArray, xmask::CudaArray, dropout, scale) ccall((:drop,libkunet),Void,(Cint,Cmat,Cmat,Cfloat,Cfloat),length(x),x,xmask,dropout,scale) end
+noop(l,x,dx)=dx
