4af3ae6e67ba0dd73745f8a609cf0c140d98a638
Deniz Yuret <denizyuret@gmail.com>
2015-02-21 18:28:21 +0200

    Hierarchical file structure for UpdateParams.

--- src/h5io.jl
+++ src/h5io.jl
@@ -1,39 +1,70 @@
 # File I/O
-using HDF5
 using CUDArt
+using HDF5
+import HDF5: h5write, h5read
+const dont_save = [:y, :x, :dx, :xdrop]
 
-function Layer(fname::String)
+function Layer(fname::String; gpu=true)
     f = h5open(fname, "r")
     l = Layer()
-    for n in names(f)
-        s = symbol(n)
-        if in(s, names(l))
-            l.(s) = read(f, "/$n")
-        end
-    end
-    for n in names(attrs(f))
-        s = symbol(n)
-        if in(s, names(l))
-            l.(s) = eval(parse(read(attrs(f)[n])))
-        end
-    end
+    h5read(f["/"], l, gpu)
     close(f)
     return l
 end
 
-function HDF5.h5write(fname::String, l::Layer)
-    f = h5open(fname, "w")
-    for n in names(l)
-        if (isdefined(l,n)) 
-            if (isa(l.(n), Array))
-                f["/$n"] = l.(n)
-            elseif (isa(l.(n), CudaArray))
-                f["/$n"] = to_host(l.(n))
-            elseif (isa(l.(n), Function))
-                attrs(f)[string(n)] = string(l.(n))
-            end
+function h5read(g::HDF5Group, l::Union(Layer,UpdateParam), gpu::Bool=true)
+    a = attrs(g)
+    for n in names(a)
+        s = symbol(n)
+        if in(s, names(l))
+            l.(s) = read(a[n])
+            isa(l.(s), String) && (l.(s) = eval(parse(l.(s))))
+        else
+            warn("$n is not a valid field")
         end
     end
+    for n in names(g)
+        s = symbol(n)
+        if in(s, names(l))
+            if isa(g[n], HDF5Dataset)
+                l.(s) = read(g[n])
+                gpu && (l.(s) = CudaArray(l.(s)))
+            elseif isa(g[n], HDF5Group)
+                assert(isa(l, Layer))
+                l.(s) = UpdateParam()
+                h5read(g[n], l.(s))
+            else
+                warn("Don't know how to read $n::$(typeof(g[n]))")
+            end
+        else
+            warn("$n is not a valid field")
+        end
+    end
+end
+
+function h5write(fname::String, l::Layer)
+    f = h5open(fname, "w")
+    h5write(f["/"], l)
     close(f)
 end
 
+function h5write(g::HDF5Group, l)
+    for n in names(l)
+        if (isdefined(l,n) && !in(n, dont_save))
+            if (isa(l.(n), Array))
+                g["$n"] = l.(n)
+            elseif (isa(l.(n), CudaArray))
+                g["$n"] = to_host(l.(n))
+            elseif (isa(l.(n), Function))
+                attrs(g)[string(n)] = string(l.(n))
+            elseif (isa(l.(n), Number))
+                attrs(g)[string(n)] = l.(n)
+            elseif (isa(l.(n), UpdateParam))
+                gg = g_create(g, string(n))
+                h5write(gg, l.(n))
+            else
+                error("Don't know how to save $(typeof(l.(n)))::$n")
+            end
+        end
+    end
+end
