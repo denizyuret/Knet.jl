65608697b22d7ad0d4e662ab74fbb790899f329b
Deniz Yuret <denizyuret@gmail.com>
2015-02-24 11:41:09 +0200

    New constructors.

--- src/types.jl
+++ src/types.jl
@@ -1,54 +1,38 @@
-# export Layer, Net, UpdateParam, setparam!
+# Layer: both f (activation function) and b (bias vector) are
+# optional, w (weight matrix) is the only required field
 
-type Layer f; w; b; fx; dw; db; pw; pb; y; x; dx; dropout; xdrop;  # can we use dx for xdrop?
-    function Layer(; args...)
-        o=new()
-        for (k,v)=args
-            in(k, names(o)) ? (o.(k) = v) : warn("Layer has no field $k")
-        end
-        return o
-    end
-end
+type Layer w; b; f; fx; dw; db; pw; pb; y; x; dx; dropout; xdrop; Layer()=new() end
+atype(w) = (usegpu ? CudaArray(w) : w)
+Layer(w; o...) = (l=Layer(); l.w=atype(w); setparam!(l; o...); l)
+Layer(w, b; o...) = (l=Layer(w; o...); l.b=atype(b); l)
+Layer(f::Function, w; o...) = (l=Layer(w; o...); l.f=f; l)
+Layer(f::Function, w, b; o...) = (l=Layer(w,b; o...); l.f=f; l)
+Layer(r::Integer, c::Integer; bias=true, o...) = (w=float32(randn(r,c)*0.01);bias ? Layer(w,zeros(Float32, r, 1);o...) : Layer(w; o...))
+Layer(f::Function, r::Integer, c::Integer; o...) = (l=Layer(r,c;o...); l.f=f; l)
 
-type UpdateParam learningRate; l1reg; l2reg; maxnorm; adagrad; ada; momentum; mom; nesterov; nes; 
-    function UpdateParam(; learningRate=0.01f0, args...)
-        o=new(learningRate)
-        for (k,v)=args
-            in(k, names(o)) ? (o.(k) = v) : warn("UpdateParam has no field $k")
-        end
-        return o
-    end
-end
+# Net: Convenience type and constructor for an array of layers
 
 typealias Net Array{Layer,1}
+Net(f::Function, d::Integer...; o...) = (n=Layer[]; for i=2:length(d); push!(n, (i<length(d)) ? Layer(f,d[i],d[i-1];o...) : Layer(d[i],d[i-1];o...)); end; n)
 
-function Net(fn::Function, dims::Integer...)
-    net = Layer[]
-    for i=2:length(dims)
-        nrows,ncols = dims[i],dims[i-1]
-        l = (i < length(dims)) ? Layer(nrows, ncols; f=fn) : Layer(nrows, ncols)
-        push!(net, l)
-    end
-    return net
-end
 
-function Layer(nrows::Integer, ncols::Integer; args...)
-    l = Layer(; args...)
-    arr = usegpu ? CudaArray : Array
-    l.w = arr(Float32, nrows, ncols)
-    l.b = arr(Float32, nrows, 1)
-    rand!(l.w); @in1! l.w .- 0.5f0; @in1! l.w .* 0.05f0;
-    fill!(l.b, 0f0)
-    return l
-end
-
-# Parameters can be set at the level of weights, layers, nets, or
+# UpdateParam: Parameters can be set at the level of weights, layers, nets, or
 # training sessions.  Here is the lowest level each parameter can be
 # set:
 # weight-specific: learningRate; l1reg; l2reg; maxnorm; adagrad; momentum; nesterov; 
 # layer-specific: dropout, apply_fx, return_dx
 # net-specific: ? 
 # train-specific: batch, iters, loss
+
+type UpdateParam learningRate; l1reg; l2reg; maxnorm; adagrad; ada; momentum; mom; nesterov; nes; UpdateParam(lr)=new(lr); end
+
+function UpdateParam(; learningRate=0.01f0, args...)
+    o=new(learningRate)
+    for (k,v)=args
+        in(k, names(o)) ? (o.(k) = v) : warn("UpdateParam has no field $k")
+    end
+    return o
+end
 
 function setparam!(l::Layer,k,v)
     if (k == :dropout)
@@ -74,3 +58,4 @@ setparam!(p::UpdateParam,k,v)=(p.(k)=v)
 
 setparam!(net::Net,k,v)=for l=net setparam!(l,k,v) end
 
+setparam!(x; args...)=(for (k,v)=args; setparam!(x,k,v); end)
