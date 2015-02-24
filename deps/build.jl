963d509e703aa05d0e544327036a4869250764e9
Deniz Yuret <denizyuret@gmail.com>
2015-02-24 10:08:09 +0200

    Check for cuda.

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,7 @@
-cd("../cuda") do
-    run(`make`)
+try success(`nvcc --version`)
+    cd("../cuda") do
+        run(`make libkunet.so`)
+    end
+catch
+    warn("CUDA not installed, GPU support will not be available.")
 end
