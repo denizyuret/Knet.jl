9612925d799757f87e89bb38e5df83f8acaaa37a
Deniz Yuret <denizyuret@gmail.com>
2015-02-28 13:04:29 +0200

    Added exports.

--- src/KUnet.jl
+++ src/KUnet.jl
@@ -1,4 +1,5 @@
 module KUnet
+const libkunet = find_library(["libkunet"], [Pkg.dir("KUnet/cuda")])
 
 using InplaceOps
 using Base.LinAlg.BLAS
@@ -7,6 +8,8 @@ using HDF5
 # Conditional module import
 installed(pkg)=isdir(Pkg.dir(string(pkg)))
 macro useif(pkg) if installed(pkg) Expr(:using,pkg) end end
+@useif CUDArt
+@useif CUBLAS
 
 # Turn gpu support on/off using KUnet.gpu(true/false)
 function gpu(b::Bool)
@@ -16,18 +19,17 @@ function gpu(b::Bool)
     !isdefined(:CUDArt) && (warn("CUDArt not installed."); usegpu=false)
     !isdefined(:CUBLAS) && (warn("CUBLAS not installed."); usegpu=false)
 end
-const libkunet = find_library(["libkunet"], [Pkg.dir("KUnet/cuda")])
-@useif CUDArt
-@useif CUBLAS
-gpu(libkunet != "" && isdefined(:CUDArt) && isdefined(:CUBLAS))
 
-###################
-include("types.jl")
-include("cuda.jl")
-include("net.jl")
-include("update.jl")
-include("func.jl")
-include("h5io.jl")
-###################
+global usegpu
+gpu((libkunet != "") && isdefined(:CUDArt) && isdefined(:CUBLAS))
+
+#########################
+include("types.jl");	export Layer, Net, UpdateParam, setparam!
+include("cuda.jl");	# extends copy!, mul!, badd!, bmul!, bsub!, sum!, zeros, rand!, fill!, free, to_host
+include("net.jl");	export train, predict
+include("update.jl");	# implements update: helper for train
+include("func.jl");     export relu, drop, softmaxloss
+include("h5io.jl");	# implements Layer(::String), h5write(::String, ::Layer)
+#########################
 
 end # module
