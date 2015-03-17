26d296dcbb9ea253192f46b0af6eec0c2f42c0b6
Deniz Yuret <denizyuret@gmail.com>
2015-03-17 11:52:04 +0200

    *** empty log message ***

---
+++ test/jldiotest.jl
@@ -0,0 +1,22 @@
+using KUnet
+using HDF5,JLD
+typealias LUP Union(Layer,UpdateParam)
+import Base.isequal
+
+function isequal(l1::LUP, l2::LUP)
+    for n in fieldnames(l1)
+        isdefined(l1,n) || continue
+        isdefined(l2,n) || return false
+    end
+    for n in fieldnames(l2)
+        isdefined(l2,n) || continue
+        isdefined(l1,n) || return false
+        isequal(l1.(n),l2.(n)) || return false
+    end
+    return true
+end
+
+net=newnet(relu, 1326, 20000,10)
+save("foo.jld", net)
+foo=newnet("foo.jld")
+isequal(copy(net,:cpu),copy(foo,:cpu))
