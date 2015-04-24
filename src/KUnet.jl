c3348488df8e9eca4f608c36a25482bff410980c
Deniz Yuret <denizyuret@gmail.com>
2015-04-24 15:39:31 +0300

    Fixed the error when loading in no-gpu machines.

--- src/KUnet.jl
+++ src/KUnet.jl
@@ -1,29 +1,36 @@
 module KUnet
-const libkunet = find_library(["libkunet"], [Pkg.dir("KUnet/cuda")])
-
 using Compat
 using InplaceOps
 using Base.LinAlg.BLAS
 using HDF5, JLD
 
-# Conditional module import
-installed(pkg)=isdir(Pkg.dir(string(pkg)))
-macro useif(pkg) if installed(pkg) Expr(:using,pkg) end end
-@useif CUDArt
-@useif CUBLAS
+# GPU support is on by default if the required libraries exist.
+# The user can turn gpu support on/off using KUnet.gpu(true/false)
 
-# Turn gpu support on/off using KUnet.gpu(true/false)
+global usegpu
+const libkunet = find_library(["libkunet"], [Pkg.dir("KUnet/cuda")])
+const libcuda = find_library(["libcuda"])
+const libcudart = find_library(["libcudart", "cudart"])
+installed(pkg)=isdir(Pkg.dir(string(pkg)))
+
 function gpu(b::Bool)
     global usegpu = b
     usegpu == false     && return
-    libkunet == ""      && (warn("libkunet.so not found."); usegpu=false)
-    !isdefined(:CUDArt) && (warn("CUDArt not installed."); usegpu=false)
-    !isdefined(:CUBLAS) && (warn("CUBLAS not installed."); usegpu=false)
+    isempty(libkunet)   && (warn("libkunet not found."); usegpu=false)
+    isempty(libcuda)    && (warn("libcuda not found."); usegpu=false)
+    isempty(libcudart)  && (warn("libcudart not found."); usegpu=false)
+    !installed(:CUDArt) && (warn("CUDArt not installed."); usegpu=false)
+    !installed(:CUBLAS) && (warn("CUBLAS not installed."); usegpu=false)
     usegpu
 end
 
-global usegpu
-gpu((libkunet != "") && isdefined(:CUDArt) && isdefined(:CUBLAS))
+gpu(true)
+
+# Conditional module import
+macro useifgpu(pkg) if usegpu Expr(:using,pkg) end end
+@useifgpu CUDArt
+@useifgpu CUBLAS
+
 
 #########################
 include("types.jl");	export Layer, Net, UpdateParam, setparam!, newnet
