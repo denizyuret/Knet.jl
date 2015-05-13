69c067b17d8d7d5200594af2df36406fe126f06b
Deniz Yuret <denizyuret@gmail.com>
2015-05-14 00:56:37 +0300

    Fixed initback to receive input from mmul layer.

--- src/pool.jl
+++ src/pool.jl
@@ -6,14 +6,27 @@ end
 # TODO: cpu implementation
 
 function forw(l::Pool, x::CudaArray; o...)
-    l.x = x
-    chksize(l, :y, l.x, cudnnGetPoolingNdForwardOutputDim(l.pd, l.x))
+    initforw(l, x)
     cudnnPoolingForward(l.pd, l.x, l.y)
 end
 
-function back(l::Pool, dy::CudaArray; o...)
-    l.dy = dy
-    @assert size(l.dy) == size(l.y)
-    chksize(l, :dx, l.x)
+function initforw(l::Pool, x::CudaArray)
+    l.x = x
+    chksize(l, :y, l.x, cudnnGetPoolingNdForwardOutputDim(l.pd, l.x))
+end
+
+function back(l::Pool, dy::CudaArray; dx=true, o...)
+    dx || return
+    initback(l, dy)
     cudnnPoolingBackward(l.pd, l.y, l.dy, l.x, l.dx)
+end
+
+function initback(l::Pool, dy::CudaArray)
+    if (size(dy) == size(l.y))
+        l.dy = dy
+    else
+        @assert length(dy) == length(l.y)
+        l.dy = reinterpret(eltype(dy), dy, size(l.y))
+    end
+    chksize(l, :dx, l.x)
 end
