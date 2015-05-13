d9c9c1ea461f07d2c2907ad17a99d304f458b967
Deniz Yuret <denizyuret@gmail.com>
2015-05-13 22:44:27 +0300

    *** empty log message ***

---
+++ src/pool.jl
@@ -0,0 +1,19 @@
+type Pool <: Layer; pd; x; y; dx; dy; 
+    Pool(d::Int)=new(PoolingDescriptor((d,d)))
+end
+
+# TODO: generalize to 3-D
+# TODO: cpu implementation
+
+function forw(l::Pool, x::CudaArray)
+    l.x = x
+    chksize(l, :y, l.x, cudnnGetPoolingNdForwardOutputDim(l.pd, l.x))
+    cudnnPoolingForward(l.pd, l.x, l.y)
+end
+
+function back(l::Pool, dy::CudaArray)
+    l.dy = dy
+    @assert size(l.dy) == size(l.y)
+    chksize(l, :dx, l.x)
+    cudnnPoolingBackward(l.pd, l.y, l.dy, l.x, l.dx)
+end
