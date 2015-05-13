1cec738bb8cbbd6563c31a59ae8151221c9a755f
Deniz Yuret <denizyuret@gmail.com>
2015-05-13 23:30:31 +0300

    *** empty log message ***

--- test/testmnist.jl
+++ test/testmnist.jl
@@ -2,15 +2,14 @@ require("mnist.jl")
 using KUnet
 using MNIST: xtrn, ytrn, xtst, ytst
 accuracy(y,z)=mean(findmax(y,1)[2] .== findmax(z,1)[2])
-KUnet.gpuseed(1)
-srand(1)
+KUnet.gpuseed(1); srand(1)
 
-net = [Drop(0.2), Mmul(4096, 784), Bias(4096), Relu(), 
-       Drop(0.5), Mmul(4096, 4096), Bias(4096), Relu(),
-       Drop(0.5), Mmul(10, 4096), Bias(10)
-       ]
+# net = [Drop(0.2), Mmul(4096, 784), Bias(4096), Relu(), 
+#        Drop(0.5), Mmul(4096, 4096), Bias(4096), Relu(),
+#        Drop(0.5), Mmul(10, 4096), Bias(10)]
+
+net = [Mmul(64, 784), Bias(64), Relu(), Mmul(10, 64), Bias(10)]
 setparam!(net, :lr, 0.5)
-
 for i=1:100
     train(net, xtrn, ytrn)
     println((i, accuracy(ytst, predict(net, xtst)), 
