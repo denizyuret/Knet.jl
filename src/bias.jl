22fbc5bbc7bae6370fbc4dc938280be299ec56b5
Deniz Yuret <denizyuret@gmail.com>
2015-05-14 18:14:31 +0300

    Fixed cpu version for N-D operation.

--- src/bias.jl
+++ src/bias.jl
@@ -1,24 +1,46 @@
-type Bias <: Layer; b::Param; end
-Bias(b::Array;a...)=Bias(Param(b;a...))
-Bias(b::CudaArray;a...)=Bias(Param(b;a...))
-# TODO: get rid of Float32
-Bias(d::Integer;a...)=Bias(Param(zeros(Float32,d);a...))
+type Bias <: Layer; b::Param; Bias()=new(); end
+Bias(b::Param)=(l=Bias();l.b=b;l)
+Bias(b; a...)=Bias(Param(b;a...))
+Bias(d::Integer...; a...)=Bias(Param(zeros(d); a...))
 
 update(l::Bias)=update(l.b)
 setparam!(l::Bias,k,v)=setparam!(l.b,k,v)
 
-forw(l::Bias, x::CudaArray; o...)=(cudnnAddTensor(l.b.data, x); x)
-back(l::Bias, dy::CudaArray; o...)=(chksize(l.b, :diff, l.b.data); cudnnConvolutionBackwardBias(dy, l.b.diff); dy)
+# We are implementing the CUDNN_ADD_SAME_C mode of cudnn:
+# In this mode if x has dimensions (X1,X2,...,C,N) then
+# bias has length=C.
 
-# TODO: make sure N-D works on cpu
 function forw(l::Bias, x; o...)
-    @in1! x .+ l.b.data
+    b = l.b.data
+    if ndims(x) == 1
+        @assert length(b) == length(x)
+        for i=1:length(x); x[i] += b[i]; end
+    else
+        c = ndims(x)-1
+        @assert length(b) == size(x, c)
+        for i=1:length(x); x[i] += b[ind2sub(size(x),i)[c]]; end
+    end
     return x
 end
 
 function back(l::Bias, dy; o...)
     chksize(l.b, :diff, l.b.data)
-    sum!(l.b.diff, dy)
+    db = l.b.diff
+    if ndims(dy) == 1
+        @assert length(db) == length(dy)
+        for i=1:length(dy); db[i]=dy[i]; end
+    else
+        c = ndims(dy)-1
+        @assert length(db) == size(dy, c)
+        fill!(db, zero(eltype(db)))
+        for i=1:length(dy); db[ind2sub(size(dy),i)[c]] += dy[i]; end
+    end
+    # sum!(l.b.diff, dy)
     return dy
+end
+
+if GPU
+forw(l::Bias, x::CudaArray; o...)=(cudnnAddTensor(l.b.data, x; mode=CUDNN_ADD_SAME_C); x)
+back(l::Bias, dy::CudaArray; o...)=(chksize(l.b, :diff, l.b.data); cudnnConvolutionBackwardBias(dy, l.b.diff); dy)
 end
 
