52e7e7afe59e1b411782bbd7729aef9ee373653a
Deniz Yuret <denizyuret@gmail.com>
2015-05-15 17:18:39 +0300

    *** empty log message ***

--- src/logp.cu
+++ src/logp.cu
@@ -1,9 +1,9 @@
 #include "kunet.h"
 
-__global__ void _logpforw(int nrows, int ncols, float *y) {
-  /* y is layer output, i.e. unnormalized log probabilities.
-     On output y will contain normalized probabilities.
-  */
+/* y is layer output, i.e. unnormalized log probabilities.
+   On output y will contain normalized probabilities. */
+
+__global__ void _slogpforw(int nrows, int ncols, float *y) {
   float ymax, z, logz;
   int i0, i1;
   int col = threadIdx.x + blockIdx.x * blockDim.x;
@@ -11,24 +11,33 @@ __global__ void _logpforw(int nrows, int ncols, float *y) {
     i0 = col * nrows;
     i1 = i0  + nrows;
     ymax = -INFINITY;
-    for (int i=i0; i<i1; i++) {
-      if (y[i] > ymax) {
-	ymax = y[i];
-      }
-    }
+    for (int i=i0; i<i1; i++) { if (y[i] > ymax) ymax = y[i]; }
     z = 0;
-    for (int i=i0; i<i1; i++) {
-      y[i] -= ymax;
-      z += exp(y[i]);
-    }
+    for (int i=i0; i<i1; i++) { y[i] -= ymax; z += exp(y[i]); }
     logz = log(z);
-    for (int i=i0; i<i1; i++) {
-      y[i] -= logz;
-    }
+    for (int i=i0; i<i1; i++) { y[i] -= logz; }
+    col += blockDim.x * gridDim.x;
+  }
+}
+
+__global__ void _dlogpforw(int nrows, int ncols, double *y) {
+  double ymax, z, logz;
+  int i0, i1;
+  int col = threadIdx.x + blockIdx.x * blockDim.x;
+  while (col < ncols) {
+    i0 = col * nrows;
+    i1 = i0  + nrows;
+    ymax = -INFINITY;
+    for (int i=i0; i<i1; i++) { if (y[i] > ymax) ymax = y[i]; }
+    z = 0;
+    for (int i=i0; i<i1; i++) { y[i] -= ymax; z += exp(y[i]); }
+    logz = log(z);
+    for (int i=i0; i<i1; i++) { y[i] -= logz; }
     col += blockDim.x * gridDim.x;
   }
 }
 
 extern "C" {
-void logpforw(int nrows, int ncols, float *y) KCALL(_logpforw,nrows,ncols,y);
+void slogpforw(int nrows, int ncols, float *y) KCALL(_slogpforw,nrows,ncols,y);
+void dlogpforw(int nrows, int ncols, double *y) KCALL(_dlogpforw,nrows,ncols,y);
 }
