5d0472607c40e165cee496b13a876582416ec20e
Deniz Yuret <denizyuret@gmail.com>
2015-05-15 16:32:22 +0300

    passed runtests.

--- src/drop.cu
+++ src/drop.cu
@@ -1,6 +1,15 @@
 #include "kunet.h"
 
-__global__ void _drop(int n, float *x, float *xmask, float dropout, float scale) {
+__global__ void _sdrop(int n, float *x, float *xmask, float dropout, float scale) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  while (i < n) {
+    if (xmask[i] < dropout) x[i] = 0;
+    else x[i] *= scale;
+    i += blockDim.x * gridDim.x;
+  }
+}
+
+__global__ void _ddrop(int n, double *x, double *xmask, double dropout, double scale) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
     if (xmask[i] < dropout) x[i] = 0;
@@ -10,5 +19,6 @@ __global__ void _drop(int n, float *x, float *xmask, float dropout, float scale)
 }
 
 extern "C" {
-void drop(int n, float *x, float *xmask, float dropout, float scale) KCALL(_drop,n,x,xmask,dropout,scale);
+void sdrop(int n, float *x, float *xmask, float dropout, float scale) KCALL(_sdrop,n,x,xmask,dropout,scale);
+void ddrop(int n, double *x, double *xmask, double dropout, double scale) KCALL(_ddrop,n,x,xmask,dropout,scale);
 }
