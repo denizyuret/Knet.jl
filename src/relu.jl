c6a3a10827e6c72bf7100212f8004eeec09b8035
Deniz Yuret <denizyuret@gmail.com>
2015-05-15 17:26:03 +0300

    passed runtests.

--- src/relu.jl
+++ src/relu.jl
@@ -4,11 +4,6 @@ forw(l::Relu,x; o...)=(x0=zero(eltype(x)); for i=1:length(x); (x[i]<x0)&&(x[i]=x
 back(l::Relu,dy; dx=true, o...)=(dx||return; y0=zero(eltype(dy)); for i=1:length(dy); (l.y[i]==y0)&&(dy[i]=y0) end; dy)
 
 if GPU
-
 forw(l::Relu,x::CudaArray; o...)=(l.y=cudnnActivationForward(x; mode=CUDNN_ACTIVATION_RELU))
 back(l::Relu,dy::CudaArray; dx=true, o...)=(dx && cudnnActivationBackward(l.y, dy; mode=CUDNN_ACTIVATION_RELU))
-
-#forw(l::Relu,x::CudaArray; o...)=(ccall((:reluforw,libkunet),Void,(Cint,Cmat),length(x),x); l.y=x)
-#back(l::Relu,dy::CudaArray; o...)=(ccall((:reluback,libkunet),Void,(Cint,Cmat,Cmat),length(dy),l.y,dy); dy)
-
 end # if GPU
