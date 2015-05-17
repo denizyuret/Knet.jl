41e0ddd70474a69db220feaac0218c81ab7c2341
Deniz Yuret <denizyuret@gmail.com>
2015-05-17 12:45:37 +0300

    Derivative unnecessary.

--- src/logp.jl
+++ src/logp.jl
@@ -21,30 +21,33 @@ function forw(l::Logp, y; o...)
     return (l.y=y)
 end
 
+# If the output is normalized logp connected to LogpLoss we should get
+# dy=dx since y=x+const.
+function back(l::Logp, dy; o...)=dy
+
+# For general loss functions:
 # z = Σj exp(xj)
 # yi = xi - logz
 # dJ/dxi = Σj dJ/dyj dyj/dxi
 # dyj/dxi = [i==j] - (1/z)exp(xi) = [i==j] - exp(yi)
 # dJ/dxi = dJ/dyi - exp(yi) Σj dJ/dyj
 
-function back(l::Logp, dy; dx=true, o...)
-    @assert issimilar(dy, l.y)
-    dx || return
-    (nd,nx) = size2(dy)
-    for j=1:nx
-        i1=(j-1)*nd+1
-        i2=j*nd
-        dysum = zero(Float64)
-        for i=i1:i2; dysum += dy[i]; end
-        for i=i1:i2; dy[i] = dy[i] - exp(l.y[i])*dysum; end
-    end
-    return dy
-end
+# function back(l::Logp, dy; returndx=true, o...)
+#     @assert issimilar(dy, l.y)
+#     returndx || return
+#     (nd,nx) = size2(dy)
+#     for j=1:nx
+#         i1=(j-1)*nd+1
+#         i2=j*nd
+#         dysum = zero(Float64)
+#         for i=i1:i2; dysum += dy[i]; end
+#         for i=i1:i2; dy[i] = dy[i] - exp(l.y[i])*dysum; end
+#     end
+#     return dy
+# end
 
 if GPU
 forw(l::Logp,y::CudaArray{Float32}; o...)=((nd,nx) = size2(y);ccall((:logpforw32,libkunet),Void,(Cint,Cint,Ptr{Float32}),nd,nx,y); l.y=y)
 forw(l::Logp,y::CudaArray{Float64}; o...)=((nd,nx) = size2(y);ccall((:logpforw64,libkunet),Void,(Cint,Cint,Ptr{Float64}),nd,nx,y); l.y=y)
-back(l::Logp,dy::CudaArray{Float32}; o...)=dy
-back(l::Logp,dy::CudaArray{Float64}; o...)=dy
 end # if GPU
 
