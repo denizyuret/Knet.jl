3c652a0e1b352bee31ef58549467db0e52f8381c
Deniz Yuret <denizyuret@gmail.com>
2015-05-17 08:48:35 +0300

    *** empty log message ***

--- src/drop.cu
+++ src/drop.cu
@@ -1,6 +1,6 @@
 #include "kunet.h"
 
-__global__ void _sdrop(int n, float *x, float *xmask, float dropout, float scale) {
+__global__ void _drop32(int n, float *x, float *xmask, float dropout, float scale) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
     if (xmask[i] < dropout) x[i] = 0;
@@ -9,7 +9,7 @@ __global__ void _sdrop(int n, float *x, float *xmask, float dropout, float scale
   }
 }
 
-__global__ void _ddrop(int n, double *x, double *xmask, double dropout, double scale) {
+__global__ void _drop64(int n, double *x, double *xmask, double dropout, double scale) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
     if (xmask[i] < dropout) x[i] = 0;
@@ -19,6 +19,6 @@ __global__ void _ddrop(int n, double *x, double *xmask, double dropout, double s
 }
 
 extern "C" {
-void sdrop(int n, float *x, float *xmask, float dropout, float scale) KCALL(_sdrop,n,x,xmask,dropout,scale);
-void ddrop(int n, double *x, double *xmask, double dropout, double scale) KCALL(_ddrop,n,x,xmask,dropout,scale);
+void drop32(int n, float *x, float *xmask, float dropout, float scale) KCALL(_drop32,n,x,xmask,dropout,scale);
+void drop64(int n, double *x, double *xmask, double dropout, double scale) KCALL(_drop64,n,x,xmask,dropout,scale);
 }
