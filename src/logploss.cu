e66094d56601b2329e331daecd76d2907e0df2ce
Deniz Yuret <denizyuret@gmail.com>
2015-05-17 08:50:47 +0300

    *** empty log message ***

--- src/logploss.cu
+++ src/logploss.cu
@@ -1,6 +1,6 @@
 #include "kunet.h"
 
-__global__ void _slogploss(int n, float scale, float *y, float *dy) {
+__global__ void _logploss32(int n, float scale, float *y, float *dy) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
     dy[i] = scale*(exp(y[i]) - dy[i]);
@@ -8,7 +8,7 @@ __global__ void _slogploss(int n, float scale, float *y, float *dy) {
   }
 }
 
-__global__ void _dlogploss(int n, double scale, double *y, double *dy) {
+__global__ void _logploss64(int n, double scale, double *y, double *dy) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
     dy[i] = scale*(exp(y[i]) - dy[i]);
@@ -17,6 +17,6 @@ __global__ void _dlogploss(int n, double scale, double *y, double *dy) {
 }
 
 extern "C" {
-  void slogploss(int n, float s, float *y, float *dy) KCALL(_slogploss,n,s,y,dy);
-  void dlogploss(int n, double s, double *y, double *dy) KCALL(_dlogploss,n,s,y,dy);
+  void logploss32(int n, float s, float *y, float *dy) KCALL(_logploss32,n,s,y,dy);
+  void logploss64(int n, double s, double *y, double *dy) KCALL(_logploss64,n,s,y,dy);
 }
