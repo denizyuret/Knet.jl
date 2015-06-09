4fc8e33146f6c61cddc48db86a810e481dc355b0
Deniz Yuret <denizyuret@gmail.com>
2015-06-09 21:52:44 +0300

    Failed the CudaArray test: mmul, hcat, xpose do not work.

--- test/polytest.jl
+++ test/polytest.jl
@@ -1,3 +1,4 @@
+using CUDArt
 using KUnet
 using KUnet: accuracy
 require(Pkg.dir("KUnet/test/mnist.jl"))
@@ -13,10 +14,10 @@ w0 = similar(ytst, size(ytst,1), 0)
 if true
 xnet = Layer[Poly(c=1f0,d=3f0,w=w0), PercLoss()]
 for i=1:1
-    @time train(xnet, xtrn, ytrn)
+    @time train(xnet, xtst, ytst)
     println((i, size(xnet[1].s), 
              accuracy(ytst, predict(xnet, xtst)),
-             accuracy(ytrn, predict(xnet, xtrn))))
+             )) # accuracy(ytrn, predict(xnet, xtrn))))
 end
 end # if false
 
@@ -25,9 +26,21 @@ strn = sparse(MNIST.xtrn)
 stst = sparse(MNIST.xtst)
 snet = Layer[Poly(c=1f0,d=3f0,w=w0), PercLoss()]
 for i=1:1
-    @time train(snet, strn, ytrn)
+    @time train(snet, stst, ytst)
     println((i, size(snet[1].s), 
              accuracy(ytst, predict(snet, stst)),
-             accuracy(ytrn, predict(snet, strn))))
+             )) # accuracy(ytrn, predict(snet, strn))))
 end
+end # if false
+
+if false # mmul, hcat, ctranspose do not work
+KUnet.atype(CudaArray)
+cnet = Layer[Poly(c=1f0,d=3f0,w=CudaArray(w0)), PercLoss()]
+for i=1:1
+    @time train(cnet, xtst, ytst; iters=1)
+    println((i, size(cnet[1].s), 
+             accuracy(ytst, predict(cnet, xtst)),
+             )) # accuracy(ytrn, predict(cnet, xtrn))))
+end
+KUnet.atype(Array)
 end # if false
