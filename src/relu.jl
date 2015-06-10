274ea3a53bce2f5ffc3b8bcac3e51b2a4d356ca3
Deniz Yuret <denizyuret@gmail.com>
2015-06-10 22:09:03 +0300

    reshape dy to match y going back.

--- src/relu.jl
+++ src/relu.jl
@@ -1,19 +1,13 @@
 type Relu <: Layer; y; Relu()=new() end
 copy(l::Relu; o...)=Relu()
 
-function forw(l::Relu,x; o...)
-    x0=zero(eltype(x)); for i=1:length(x); (x[i]<x0)&&(x[i]=x0) end; l.y=x
-end
+forw(l::Relu,x; o...)=(l.y = reluforw(x))
+back(l::Relu,dy; returndx=true, o...)=(returndx && reluback(l.y, reshape(dy, size(l.y))))
 
-function back(l::Relu,dy; returndx=true, o...)
-    @assert issimilar1(dy, l.y)
-    returndx||return
-    y0=zero(eltype(dy))
-    for i=1:length(dy); (l.y[i]==y0)&&(dy[i]=y0) end
-    return dy
-end
+reluforw(x)=(x0=zero(eltype(x)); for i=1:length(x); (x[i]<x0)&&(x[i]=x0) end; x)
+reluback(y, dy)=(y0=zero(eltype(dy)); for i=1:length(dy); (y[i]==y0)&&(dy[i]=y0) end; dy)
 
 if GPU
-forw(l::Relu,x::CudaArray; o...)=(l.y=cudnnActivationForward(x; mode=CUDNN_ACTIVATION_RELU))
-back(l::Relu,dy::CudaArray; returndx=true, o...)=(@assert issimilar1(dy, l.y); returndx && cudnnActivationBackward(l.y, dy; mode=CUDNN_ACTIVATION_RELU); dy)
+reluforw(x::CudaArray; o...)=(cudnnActivationForward(x; mode=CUDNN_ACTIVATION_RELU); x)
+reluback(y::CudaArray, dy::CudaArray)=(cudnnActivationBackward(y, dy; mode=CUDNN_ACTIVATION_RELU); dy)
 end # if GPU
--- src/tanh.jl
+++ src/tanh.jl
@@ -1,10 +1,13 @@
 type Tanh <: Layer; y; Tanh()=new() end
 copy(l::Tanh;o...)=Tanh()
 
-forw(l::Tanh,x; o...)=(for i=1:length(x); x[i]=tanh(x[i]); end; l.y=x)
-back(l::Tanh,dy; returndx=true, o...)=(@assert issimilar1(dy,l.y); returndx||return; y1=one(eltype(l.y)); for i=1:length(dy); dy[i]=dy[i]*(y1+l.y[i])*(y1-l.y[i]); end; dy)
+forw(l::Tanh,x; o...)=(l.y = tanhforw(x))
+back(l::Tanh,dy; returndx=true, o...)=(returndx && tanhback(l.y, reshape(dy, size(l.y))))
+
+tanhforw(x)=(for i=1:length(x); x[i]=tanh(x[i]); end; x)
+tanhback(y,dy)=(y1=one(eltype(y)); for i=1:length(dy); dy[i]=dy[i]*(y1+y[i])*(y1-y[i]); end; dy)
 
 if GPU
-forw(l::Tanh,x::CudaArray; o...)=(l.y=cudnnActivationForward(x; mode=CUDNN_ACTIVATION_TANH))
-back(l::Tanh,dy::CudaArray; returndx=true, o...)=(@assert issimilar1(dy,l.y); returndx && cudnnActivationBackward(l.y, dy; mode=CUDNN_ACTIVATION_TANH); dy)
+tanhforw(x::CudaArray)=(cudnnActivationForward(x; mode=CUDNN_ACTIVATION_TANH); x)
+tanhback(y::CudaArray,dy::CudaArray)=(cudnnActivationBackward(y, dy; mode=CUDNN_ACTIVATION_TANH); dy)
 end # if GPU
