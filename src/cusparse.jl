6ab67699abe6aa52f43ee1c8b96863c88f7da5fa
Deniz Yuret <dy_052@hpc-login3.hpcc.usc.edu>
2015-06-24 17:38:45 -0700

    *** empty log message ***

--- src/cusparse.jl
+++ src/cusparse.jl
@@ -1,7 +1,5 @@
 import Base: size, similar, transpose, nnz, full, sparse
 
-if GPU
-
 type CudaSparseMatrixCSC{Tv} <: AbstractCudaMatrix{Tv}
     m::Int                   # Number of rows
     n::Int                   # Number of columns
@@ -32,10 +30,18 @@ function At_mul_B!(k::CudaMatrix{Float32}, x::CudaSparseMatrixCSC{Float32}, s::C
     return k
 end
 
+function At_mul_B!(k::CudaMatrix{Float64}, x::CudaSparseMatrixCSC{Float64}, s::CudaSparseMatrixCSC{Float64})
+    @assert size(k)==(size(x,2),size(s,2))
+    ccall((:At_mul_B_64,libkunet),Void,
+          (Cint,Cint,Ptr{Cdouble},Ptr{Cint},Ptr{Cint},Ptr{Cdouble},Ptr{Cint},Ptr{Cint},Ptr{Cdouble}),
+          size(x,2),size(s,2),x.nzval,x.rowval,x.colptr,s.nzval,s.rowval,s.colptr,k)
+    device_synchronize()
+    return k
+end
 
 function A_mul_B!(k::CudaMatrix{Float32}, x::CudaSparseMatrixCSC{Float32}, s::CudaSparseMatrixCSC{Float32})
     @assert size(k)==(size(x,1),size(s,2))
-    ccall((:klinear32,libkunet),Void,
+    ccall((:A_mul_B_32,libkunet),Void,
           (Cint,Cint,Ptr{Cfloat},Ptr{Cint},Ptr{Cint},Ptr{Cfloat},Ptr{Cint},Ptr{Cint},Ptr{Cfloat}),
           size(x,1),size(s,2),x.nzval,x.rowval,x.colptr,s.nzval,s.rowval,s.colptr,k)
     device_synchronize()
@@ -44,53 +50,68 @@ end
 
 function A_mul_B!(k::CudaMatrix{Float64}, x::CudaSparseMatrixCSC{Float64}, s::CudaSparseMatrixCSC{Float64})
     @assert size(k)==(size(x,1),size(s,2))
-    ccall((:klinear64,libkunet),Void,
+    ccall((:A_mul_B_64,libkunet),Void,
           (Cint,Cint,Ptr{Cdouble},Ptr{Cint},Ptr{Cint},Ptr{Cdouble},Ptr{Cint},Ptr{Cint},Ptr{Cdouble}),
           size(x,1),size(s,2),x.nzval,x.rowval,x.colptr,s.nzval,s.rowval,s.colptr,k)
     device_synchronize()
     return k
 end
 
-using CUSPARSE: cusparseHandle, cusparseMatDescrDefault, CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ONE, cusparseDcsr2csc, cusparseScsr2csc
 
-transpose(x::CudaSparseMatrixCSC)=gpucopy(cpucopy(x).')
+transpose(x::CudaSparseMatrixCSC)=(t=gpucopy(cpucopy(x).');device_synchronize();t)
 
-function transpose1(x::CudaSparseMatrixCSC)  # this is buggy ???
-    xtype = eltype(x)
-    itype = eltype(x.colptr)
-    @assert itype == Int32 "transpose: CUSPARSE only handles Int32 indices"
-    (xrows,xcols) = size(x)
-    nz = nnz(x)
-    y = CudaSparseMatrixCSC(xcols, xrows, CudaArray(zeros(itype, xrows+1)), CudaArray(zeros(itype, nz)), CudaArray(zeros(xtype, nz)))
-    csr2csc = (xtype == Float64 ? cusparseDcsr2csc : 
-               xtype == Float32 ? cusparseScsr2csc : 
-               error("Cannot handle $xtype"))
-    csr2csc(cusparseHandle, xrows, xcols, nz, x.nzval, x.colptr, x.rowval, y.nzval, y.rowval, y.colptr, CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ONE)
-    return y
+# 100ksv: (128,128) and (512,512) work best for At_test (1.78)
+# 10ksv: (9,10), (10,10), (11,8+), (12,7+) (1.44)
+function At_test(blk,thr,k::CudaMatrix{Float32}, x::CudaSparseMatrixCSC{Float32}, s::CudaSparseMatrixCSC{Float32})
+    @assert size(k)==(size(x,2),size(s,2))
+    ccall((:At_test,libkunet),Void,
+          (Cint,Cint,Cint,Cint,Ptr{Cfloat},Ptr{Cint},Ptr{Cint},Ptr{Cfloat},Ptr{Cint},Ptr{Cint},Ptr{Cfloat}),
+          blk,thr,size(x,2),size(s,2),x.nzval,x.rowval,x.colptr,s.nzval,s.rowval,s.colptr,k)
+    device_synchronize()
+    return k
 end
 
-using CUSPARSE: cusparseSnnz, cusparseDnnz, cusparseSdense2csc, cusparseDdense2csc, CUSPARSE_DIRECTION_COLUMN, cusparseMatDescrDefault
-
-function sparse(x::CudaMatrix{Float32})
-    (xrows, xcols) = size(x)
-    nzarray = CudaArray(Int32, xcols)
-    nzcount = Int32[0]
-    cusparseSnnz(cusparseHandle, CUSPARSE_DIRECTION_COLUMN, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, nzcount)
-    nz = int(nzcount[1])
-    y = CudaSparseMatrixCSC(xrows, xcols, CudaArray(Cint, xcols+1), CudaArray(Cint, nz), CudaArray(Float32, nz))
-    cusparseSdense2csc(cusparseHandle, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, y.nzval, y.rowval, y.colptr)
-    return y
+# 100ksv: (64,128) and *(128,64)* and (512,32) work best for A_test (1.25)
+# 10ksv: 1<<(6,8), (7,8), (8,8), (9,8), (10,8) work best for A_test (1.28)
+function A_test(blk,thr,k::CudaMatrix{Float32}, x::CudaSparseMatrixCSC{Float32}, s::CudaSparseMatrixCSC{Float32})
+    @assert size(k)==(size(x,1),size(s,2))
+    ccall((:A_test,libkunet),Void,
+          (Cint,Cint,Cint,Cint,Ptr{Cfloat},Ptr{Cint},Ptr{Cint},Ptr{Cfloat},Ptr{Cint},Ptr{Cint},Ptr{Cfloat}),
+          blk,thr,size(x,1),size(s,2),x.nzval,x.rowval,x.colptr,s.nzval,s.rowval,s.colptr,k)
+    device_synchronize()
+    return k
 end
 
-function sparse(x::CudaMatrix{Float64})
-    (xrows, xcols) = size(x)
-    nzarray = CudaArray(Int32, xcols)
-    nzcount = Int32[0]
-    cusparseDnnz(cusparseHandle, CUSPARSE_DIRECTION_COLUMN, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, nzcount)
-    nz = int(nzcount[1])
-    y = CudaSparseMatrixCSC(xrows, xcols, CudaArray(Cint, xcols+1), CudaArray(Cint, nz), CudaArray(Float64, nz))
-    cusparseDdense2csc(cusparseHandle, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, y.nzval, y.rowval, y.colptr)
-    return y
-end
+# using CUSPARSE: cusparseHandle, cusparseMatDescrDefault, CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ONE, cusparseDcsr2csc, cusparseScsr2csc
 
-end # if GPU
+# function transpose1(x::CudaSparseMatrixCSC{Float32})  # this is buggy ???
+#     (xrows,xcols) = size(x); nz = nnz(x)
+#     y = CudaSparseMatrixCSC(xcols, xrows, CudaArray(zeros(Int32, xrows+1)), CudaArray(zeros(Int32, nz)), CudaArray(zeros(Float32, nz)))
+#     cusparseScsr2csc(cusparseHandle, xrows, xcols, nz, x.nzval, x.colptr, x.rowval, y.nzval, y.rowval, y.colptr, CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ONE)
+#     return y
+# end
+
+# using CUSPARSE: cusparseSnnz, cusparseDnnz, cusparseSdense2csc, cusparseDdense2csc, CUSPARSE_DIRECTION_COLUMN, cusparseMatDescrDefault
+
+# function sparse(x::CudaMatrix{Float32})
+#     (xrows, xcols) = size(x)
+#     nzarray = CudaArray(Int32, xcols)
+#     nzcount = Int32[0]
+#     cusparseSnnz(cusparseHandle, CUSPARSE_DIRECTION_COLUMN, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, nzcount)
+#     nz = int(nzcount[1])
+#     y = CudaSparseMatrixCSC(xrows, xcols, CudaArray(Cint, xcols+1), CudaArray(Cint, nz), CudaArray(Float32, nz))
+#     cusparseSdense2csc(cusparseHandle, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, y.nzval, y.rowval, y.colptr)
+#     return y
+# end
+
+# function sparse(x::CudaMatrix{Float64})
+#     (xrows, xcols) = size(x)
+#     nzarray = CudaArray(Int32, xcols)
+#     nzcount = Int32[0]
+#     cusparseDnnz(cusparseHandle, CUSPARSE_DIRECTION_COLUMN, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, nzcount)
+#     nz = int(nzcount[1])
+#     y = CudaSparseMatrixCSC(xrows, xcols, CudaArray(Cint, xcols+1), CudaArray(Cint, nz), CudaArray(Float64, nz))
+#     cusparseDdense2csc(cusparseHandle, xrows, xcols, cusparseMatDescrDefault, x, xrows, nzarray, y.nzval, y.rowval, y.colptr)
+#     return y
+# end
+
