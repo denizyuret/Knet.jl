16622e814cace2f87643397f84dd6f0157d2cdf0
Deniz Yuret <dy_052@hpc-login3.hpcc.usc.edu>
2015-06-24 18:02:59 -0700

    *** empty log message ***

--- src/gpu.jl
+++ src/gpu.jl
@@ -27,3 +27,19 @@ gpu(b::Bool)=(b && !GPU && error("No GPU"); global USEGPU=b)
 # Conditionally import gpulibs
 macro useifgpu(pkg) if GPU Expr(:using,pkg) end end
 
+# Additional cuda code
+const libkunet = find_library(["libkunet"], [Pkg.dir("KUnet/src")])
+
+# For debugging
+function gpumem()
+    mfree=Csize_t[1]
+    mtotal=Csize_t[1]
+    ccall((:cudaMemGetInfo,"libcudart.so"),Cint,(Ptr{Csize_t},Ptr{Csize_t}),mfree,mtotal)
+    convert(Int,mfree[1])
+end
+
+# our version of srand sets both gpu and cpu
+function gpuseed(n::Integer)
+    srand(n)
+    GPU && ccall((:gpuseed,libkunet),Void,(Culonglong,),convert(Culonglong, n))
+end
