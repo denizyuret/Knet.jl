52d5140fe257f77a3a6e6987d81c92151f750ba8
Deniz Yuret <denizyuret@gmail.com>
2015-06-30 12:09:48 -0700

    Removed curand stuff.

--- src/kunet.h
+++ src/kunet.h
@@ -6,10 +6,4 @@
 #define BLK 128
 #define THR 128
 #define KCALL(f,...) {f<<<BLK,THR>>>(__VA_ARGS__); CUDA(cudaGetLastError()); }
-#include <curand.h>
-extern curandGenerator_t RNG;
-#define CURAND(_s) {							\
-    if (RNG==NULL) assert(curandCreateGenerator(&RNG, CURAND_RNG_PSEUDO_DEFAULT)==CURAND_STATUS_SUCCESS); \
-    assert((_s) == CURAND_STATUS_SUCCESS);				\
-  }
 #endif
