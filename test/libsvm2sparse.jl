f6d4bcb80c8f2603b9ac47f24c1e693af984b983
Deniz Yuret <denizyuret@gmail.com>
2015-08-19 19:14:33 -0700

    Sparse array should use 32 bit indices for gpu. Do two passes over the file to get the sizes.

--- test/libsvm2sparse.jl
+++ test/libsvm2sparse.jl
@@ -1,28 +1,46 @@
-using KUnet: hcat!
-
-SMAX = (1<<30)
-
 function libsvm2sparse(fname)
+    ncols = xrows = yrows = 0
+    ydict = Dict{Int32,Int32}()
+    info("Sizing $fname...")
     f = open(fname)
-    x = spzeros(Float32,SMAX,0)
-    y = zeros(Float32,2,0)
     for l in eachline(f)
+        ncols += 1
         a = split(l)
-        y1 = zeros(Float32,2,1)
-        y1[a[1]=="1" ? 1 : 2] = 1
-        x1 = spzeros(Float32,SMAX,1)
+        class = parse(Int32,a[1])
+        ydict[class] = 1
         for i=2:length(a)
-            (idx,val) = map(int,split(a[i],':'))
-            @assert val==1 (idx,val)
-            @assert idx<=SMAX
-            x1[idx,1] = 1
+            aa = split(a[i],':')
+            idx = parse(Int32,aa[1])
+            idx > xrows && (xrows = idx)
         end
-        x = hcat!(x, x1, [1], 1)
-        y = hcat!(y, y1, [1], 1)
-        # x = [x x1]
-        # y = [y y1]
     end
-    x.m = maximum(x.rowval)
     close(f)
+    ykeys = sort(collect(keys(ydict)))
+    yrows = length(ykeys)
+    for i in 1:yrows
+        ydict[ykeys[i]] = i
+    end
+    info("Found $ncols instances with xrows=$xrows yrows=$yrows")
+
+    x = spzeros(Float32,Int32,xrows,ncols)
+    y = zeros(Float32,yrows,ncols)
+
+    info("Reading $fname...")
+    f = open(fname)
+    ncols = 0
+    for l in eachline(f)
+        ncols += 1
+        a = split(l)
+        class = parse(Int32,a[1])
+        y[ydict[class], ncols] = 1
+        for i=2:length(a)
+            aa = split(a[i],':')
+            idx = parse(Int32,aa[1])
+            val = parse(Float32,aa[2])
+            x[idx, ncols] = 1
+        end
+    end
+    close(f)
+    info("done.")
     return (x,y)
 end
