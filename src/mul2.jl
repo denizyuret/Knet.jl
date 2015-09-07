c8bc493e357b14c046a1023cb44623a2b30e1120
Deniz Yuret <denizyuret@gmail.com>
2015-09-07 14:23:58 -0700

    Added back dx option.

--- src/mul2.jl
+++ src/mul2.jl
@@ -5,15 +5,18 @@ overwrites(l::Mul2)=false
 back_reads_x(l::Mul2)=true
 back_reads_y(l::Mul2)=false
 
-# x is a pair (Tuple{2}) of similarly sized input matrices
-# we'll overwrite the second matrix for the result for now
-# if any of the matrices is nothing, that represents zero, we return nothing (or mult the other by zero?)
-# TODO: We should probably implement dropout (and gaussian noise etc.) using mul2 and random generators.
+# x1,x2 is a pair of similarly sized input matrices
 
 function forw(l::Mul2, x1, x2; y=nothing, o...)
-    return ((x1 == nothing || x2 == nothing) ? nothing :
-            mul2!(initforw(l,x1,x2,y), x1, x2))
+    initforw(l,x1,x2,y)
+    mul2!(l.y, x1, x2)
 end
+
+# if any of the matrices is nothing, that represents zero, we return nothing
+
+forw(l::Mul2, ::Void, ::Void; o...)=nothing
+forw(l::Mul2, x1, ::Void; o...)=nothing
+forw(l::Mul2, ::Void, x2; o...)=nothing
 
 function initforw(l::Mul2, x1, x2, y)
     issimilar(x1,x2) || error("Input mismatch")
@@ -23,9 +26,10 @@ function initforw(l::Mul2, x1, x2, y)
     return l.y
 end
 
-function back(l::Mul2, dy; x=(l.x1,l.x2), returndx=true, o...)
+function back(l::Mul2, dy; dx=nothing, x=(l.x1,l.x2), returndx=true, o...)
     (length(x)==2 && issimilar(dy,x[1]) && issimilar(dy,x[2])) || error("Input mismatch")
     returndx || return
+    dx != nothing && ((l.dx1,l.dx2) = dx)
     similar!(l,:dx1,x[1])
     similar!(l,:dx2,x[2])
     mul2(l.dx1,dy,x[2])
