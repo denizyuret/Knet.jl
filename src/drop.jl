9601c13e19353488178afc9bfa4bb58f0c0f871a
Deniz Yuret <denizyuret@gmail.com>
2015-09-07 14:12:32 -0700

    Fixed the forw y parameter.

--- src/drop.jl
+++ src/drop.jl
@@ -1,15 +1,16 @@
-type Drop <: Layer; dropout; xdrop; Drop(d)=new(d); end
+type Drop <: Layer; dropout; xdrop; y; Drop(d)=new(d); end
 
-# TODO: implement Dropout using Mul2
+# TODO: implement Dropout using Mul2: however forw conditional on predict?
+## maybe we can make rand conditional and mul2 treat nothing as identity?
 # TODO: be careful about corrupting the xdrop matrix in RNN
 
 overwrites(l::Drop)=true
 back_reads_x(l::Drop)=false
 back_reads_y(l::Drop)=false
 
-function forw(l::Drop, x; predict=false, xdrop=nothing, seed=nothing, o...)
+function forw(l::Drop, x; y=x, predict=false, xdrop=nothing, seed=nothing, o...)
     if !predict && (l.dropout > 0)
-        similar!(l, :xdrop, x)
+        similar!(l, :xdrop, y)
         if xdrop != nothing
             copy!(l.xdrop, xdrop)
         elseif seed != nothing
@@ -21,9 +22,11 @@ function forw(l::Drop, x; predict=false, xdrop=nothing, seed=nothing, o...)
         else
             rand!(l.xdrop)
         end
-        drop(x, l.xdrop, l.dropout, 1/(1-l.dropout))
+        drop(x, y, l.xdrop, l.dropout, 1/(1-l.dropout))
+    else
+        y===x || copy!(y,x)
     end
-    return x
+    return y
 end
 
 function back(l::Drop, dy; returndx=true, o...)
@@ -33,8 +36,8 @@ function back(l::Drop, dy; returndx=true, o...)
     return dy
 end
 
-drop(x::Array, xdrop::Array, dropout::Number, scale::Number)=(for i=1:length(x); x[i] = (xdrop[i] < dropout ? zero(x[i]) : scale * x[i]); end)
-drop(x::KUdense, xdrop::KUdense, dropout::Number, scale::Number)=drop(x.arr, xdrop.arr, dropout, scale)
+drop(x::Array, y::Array, xdrop::Array, dropout::Number, scale::Number)=(for i=1:length(y); y[i] = (xdrop[i] < dropout ? zero(x[i]) : scale * x[i]); end; y)
+drop(x::KUdense, y::KUdense, xdrop::KUdense, dropout::Number, scale::Number)=(drop(x.arr, y.arr, xdrop.arr, dropout, scale); y)
 
-GPU && (drop(x::CudaArray{Float32}, xdrop::CudaArray{Float32}, dropout, scale)=ccall((:drop32,libkunet),Void,(Cint,Ptr{Float32},Ptr{Float32},Cdouble,Cdouble),length(x),x,xdrop,dropout,scale))
-GPU && (drop(x::CudaArray{Float64}, xdrop::CudaArray{Float64}, dropout, scale)=ccall((:drop64,libkunet),Void,(Cint,Ptr{Float64},Ptr{Float64},Cdouble,Cdouble),length(x),x,xdrop,dropout,scale))
+GPU && (drop(x::CudaArray{Float32}, y::CudaArray{Float32}, xdrop::CudaArray{Float32}, dropout, scale)=ccall((:drop32,libkunet),Void,(Cint,Ptr{Float32},Ptr{Float32},Ptr{Float32},Cdouble,Cdouble),length(x),x,xdrop,dropout,scale))
+GPU && (drop(x::CudaArray{Float64}, y::CudaArray{Float64}, xdrop::CudaArray{Float64}, dropout, scale)=ccall((:drop64,libkunet),Void,(Cint,Ptr{Float64},Ptr{Float64},Ptr{Float64},Cdouble,Cdouble),length(x),x,xdrop,dropout,scale))
