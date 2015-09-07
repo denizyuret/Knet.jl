37ed35526b90a671c82cac936f8cf8d0343478a4
Deniz Yuret <denizyuret@gmail.com>
2015-09-07 15:15:29 -0700

    Added back dx option.

--- src/loss.cu
+++ src/loss.cu
@@ -1,50 +1,50 @@
 #include "kunet.h"
 
-__global__ void _softloss32(int n, double scale, float *y, float *dy) {
+__global__ void _softloss32(int n, double scale, float *y, float *dy, float *dx) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
-    dy[i] = scale*(y[i] - dy[i])/y[i];
+    dx[i] = scale*(y[i] - dy[i])/y[i];
     i += blockDim.x * gridDim.x;
   }
 }
 
-__global__ void _softloss64(int n, double scale, double *y, double *dy) {
+__global__ void _softloss64(int n, double scale, double *y, double *dy, double *dx) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
-    dy[i] = scale*(y[i] - dy[i])/y[i];
-    i += blockDim.x * gridDim.x;
-  }
-}
-
-extern "C" {
-  void softloss32(int n, double s, float *y, float *dy) KCALL(_softloss32,n,s,y,dy);
-  void softloss64(int n, double s, double *y, double *dy) KCALL(_softloss64,n,s,y,dy);
-}
-
-__global__ void _logploss32(int n, double scale, float *y, float *dy) {
-  int i = threadIdx.x + blockIdx.x * blockDim.x;
-  while (i < n) {
-    dy[i] = scale*(exp(y[i]) - dy[i]);
-    i += blockDim.x * gridDim.x;
-  }
-}
-
-__global__ void _logploss64(int n, double scale, double *y, double *dy) {
-  int i = threadIdx.x + blockIdx.x * blockDim.x;
-  while (i < n) {
-    dy[i] = scale*(exp(y[i]) - dy[i]);
+    dx[i] = scale*(y[i] - dy[i])/y[i];
     i += blockDim.x * gridDim.x;
   }
 }
 
 extern "C" {
-  void logploss32(int n, double s, float *y, float *dy) KCALL(_logploss32,n,s,y,dy);
-  void logploss64(int n, double s, double *y, double *dy) KCALL(_logploss64,n,s,y,dy);
+  void softloss32(int n, double s, float *y, float *dy, float *dx) KCALL(_softloss32,n,s,y,dy,dx);
+  void softloss64(int n, double s, double *y, double *dy, double *dx) KCALL(_softloss64,n,s,y,dy,dx);
 }
 
-__global__ void _xentloss32(int nd, int nx, float *y, float *dy) {
-  double z, ymax;
-  // double *qz = (double *) malloc(nd * sizeof(double));
+__global__ void _logploss32(int n, double scale, float *y, float *dy, float *dx) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  while (i < n) {
+    dx[i] = scale*(exp(y[i]) - dy[i]);
+    i += blockDim.x * gridDim.x;
+  }
+}
+
+__global__ void _logploss64(int n, double scale, double *y, double *dy, double *dx) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  while (i < n) {
+    dx[i] = scale*(exp(y[i]) - dy[i]);
+    i += blockDim.x * gridDim.x;
+  }
+}
+
+extern "C" {
+  void logploss32(int n, double s, float *y, float *dy, float *dx) KCALL(_logploss32,n,s,y,dy,dx);
+  void logploss64(int n, double s, double *y, double *dy, float *dx) KCALL(_logploss64,n,s,y,dy,dx);
+}
+
+__global__ void _xentloss32(int nd, int nx, float *y, float *p, float *dx) {
+  double z;
+  float qi, ymax;
   int i0, i1;
   int ix = threadIdx.x + blockIdx.x * blockDim.x;
   while (ix < nx) {
@@ -53,18 +53,16 @@ __global__ void _xentloss32(int nd, int nx, float *y, float *dy) {
     z = 0;
     ymax = -INFINITY;
     for (int i=i0; i<i1; i++) { if (y[i] > ymax) ymax = y[i]; }
-    for (int i=i0; i<i1; i++) { y[i] = exp(y[i] - ymax); z+=y[i]; }
-    for (int i=i0; i<i1; i++) { y[i] /= z; dy[i] = (y[i] - dy[i])/nx; }
-    //for (int i=i0; i<i1; i++) { z += (qz[i-i0] = exp(y[i] - ymax)); }
-    //for (int i=i0; i<i1; i++) { dy[i] = (qz[i-i0]/z - dy[i])/nx; }
+    for (int i=i0; i<i1; i++) { z += exp(y[i] - ymax); }
+    for (int i=i0; i<i1; i++) { qi = exp(y[i] - ymax)/z; dx[i] = (qi - p[i])/nx; }
     ix += blockDim.x * gridDim.x;
   }
   // free(qz);
 }
 
-__global__ void _xentloss64(int nd, int nx, double *y, double *dy) {
-  double z, ymax;
-  // double *qz = (double *) malloc(nd * sizeof(double));
+__global__ void _xentloss64(int nd, int nx, double *y, double *p, double *dx) {
+  double z;
+  double qi, ymax;
   int i0, i1;
   int ix = threadIdx.x + blockIdx.x * blockDim.x;
   while (ix < nx) {
@@ -73,21 +71,20 @@ __global__ void _xentloss64(int nd, int nx, double *y, double *dy) {
     z = 0;
     ymax = -INFINITY;
     for (int i=i0; i<i1; i++) { if (y[i] > ymax) ymax = y[i]; }
-    for (int i=i0; i<i1; i++) { y[i] = exp(y[i] - ymax); z+=y[i]; }
-    for (int i=i0; i<i1; i++) { y[i] /= z; dy[i] = (y[i] - dy[i])/nx; }
-    // for (int i=i0; i<i1; i++) { z += (qz[i-i0] = exp(y[i] - ymax)); }
-    // for (int i=i0; i<i1; i++) { dy[i] = (qz[i-i0]/z - dy[i])/nx; }
+    for (int i=i0; i<i1; i++) { z += exp(y[i] - ymax); }
+    for (int i=i0; i<i1; i++) { qi = exp(y[i] - ymax)/z; dx[i] = (qi - p[i])/nx; }
     ix += blockDim.x * gridDim.x;
   }
   // free(qz);
 }
 
+
 extern "C" {
-  void xentloss32(int nd, int nx, float *y, float *dy) KCALL(_xentloss32,nd,nx,y,dy);
-  void xentloss64(int nd, int nx, double *y, double *dy) KCALL(_xentloss64,nd,nx,y,dy);
+  void xentloss32(int nd, int nx, float *y, float *p, float *dx) KCALL(_xentloss32,nd,nx,y,p,dx);
+  void xentloss64(int nd, int nx, double *y, double *p, double *dx) KCALL(_xentloss64,nd,nx,y,p,dx);
 }
 
-__global__ void _percloss32(int nd, int nx, float *y, float *z) {
+__global__ void _percloss32(int nd, int nx, float *y, float *z, float *dx) {
   float ymax, zmax;
   int i0, i1, cy, cz;
   int ix = threadIdx.x + blockIdx.x * blockDim.x;
@@ -98,17 +95,17 @@ __global__ void _percloss32(int nd, int nx, float *y, float *z) {
     for (int i=i0; i<i1; i++) { 
       if (y[i] > ymax) { ymax = y[i]; cy = i; } 
       if (z[i] > zmax) { zmax = z[i]; cz = i; } 
-      z[i] = 0;
+      dx[i] = 0;
     }
     if (cz != cy) {
-      z[cz] = -1;
-      z[cy] = 1;
+      dx[cz] = -1;
+      dx[cy] = 1;
     }
     ix += blockDim.x * gridDim.x;
   }
 }
 
-__global__ void _percloss64(int nd, int nx, double *y, double *z) {
+__global__ void _percloss64(int nd, int nx, double *y, double *z, double *dx) {
   double ymax, zmax;
   int i0, i1, cy, cz;
   int ix = threadIdx.x + blockIdx.x * blockDim.x;
@@ -119,17 +116,17 @@ __global__ void _percloss64(int nd, int nx, double *y, double *z) {
     for (int i=i0; i<i1; i++) { 
       if (y[i] > ymax) { ymax = y[i]; cy = i; } 
       if (z[i] > zmax) { zmax = z[i]; cz = i; } 
-      z[i] = 0;
+      dx[i] = 0;
     }
     if (cz != cy) {
-      z[cz] = -1;
-      z[cy] = 1;
+      dx[cz] = -1;
+      dx[cy] = 1;
     }
     ix += blockDim.x * gridDim.x;
   }
 }
 
 extern "C" {
-  void percloss32(int nd, int nx, float *y, float *z) KCALL(_percloss32,nd,nx,y,z);
-  void percloss64(int nd, int nx, double *y, double *z) KCALL(_percloss64,nd,nx,y,z);
+  void percloss32(int nd, int nx, float *y, float *z, float *dx) KCALL(_percloss32,nd,nx,y,z,dx);
+  void percloss64(int nd, int nx, double *y, double *z, double *dx) KCALL(_percloss64,nd,nx,y,z,dx);
 }
