8d9fd0a786f8358fdaccb91686c2f891ac3e41c8
Deniz Yuret <denizyuret@gmail.com>
2015-09-07 14:10:58 -0700

    Added y parameter.

--- src/actf.cu
+++ src/actf.cu
@@ -1,44 +1,45 @@
 #include "kunet.h"
 
-/* y is layer output, i.e. unnormalized log probabilities.
-   On output y will contain normalized probabilities. */
+/* x is layer output, i.e. unnormalized log probabilities.
+   On output y will contain normalized log probabilities. 
+   x and y can point to the same array. */
 
-__global__ void _logpforw32(int nrows, int ncols, float *y) {
-  double z;
-  float ymax, logz;
+__global__ void _logpforw32(int nrows, int ncols, float *x, float *y) {
+  double expy;
+  float xmax, logz;
   int i0, i1;
   int col = threadIdx.x + blockIdx.x * blockDim.x;
   while (col < ncols) {
     i0 = col * nrows;
     i1 = i0  + nrows;
-    ymax = -INFINITY;
-    for (int i=i0; i<i1; i++) { if (y[i] > ymax) ymax = y[i]; }
-    z = 0;
-    for (int i=i0; i<i1; i++) { y[i] -= ymax; z += exp(y[i]); }
-    logz = log(z);
+    xmax = -INFINITY;
+    for (int i=i0; i<i1; i++) { if (x[i] > xmax) xmax = x[i]; }
+    expy = 0;
+    for (int i=i0; i<i1; i++) { y[i] = x[i]-xmax; expy += exp(y[i]); }
+    logz = log(expy);
     for (int i=i0; i<i1; i++) { y[i] -= logz; }
     col += blockDim.x * gridDim.x;
   }
 }
 
-__global__ void _logpforw64(int nrows, int ncols, double *y) {
-  double ymax, z, logz;
+__global__ void _logpforw64(int nrows, int ncols, double *x, double *y) {
+  double xmax, expy, logz;
   int i0, i1;
   int col = threadIdx.x + blockIdx.x * blockDim.x;
   while (col < ncols) {
     i0 = col * nrows;
     i1 = i0  + nrows;
-    ymax = -INFINITY;
-    for (int i=i0; i<i1; i++) { if (y[i] > ymax) ymax = y[i]; }
-    z = 0;
-    for (int i=i0; i<i1; i++) { y[i] -= ymax; z += exp(y[i]); }
-    logz = log(z);
+    xmax = -INFINITY;
+    for (int i=i0; i<i1; i++) { if (x[i] > xmax) xmax = x[i]; }
+    expy = 0;
+    for (int i=i0; i<i1; i++) { y[i] = x[i]-xmax; expy += exp(y[i]); }
+    logz = log(expy);
     for (int i=i0; i<i1; i++) { y[i] -= logz; }
     col += blockDim.x * gridDim.x;
   }
 }
 
 extern "C" {
-void logpforw32(int nrows, int ncols, float *y) KCALL(_logpforw32,nrows,ncols,y);
-void logpforw64(int nrows, int ncols, double *y) KCALL(_logpforw64,nrows,ncols,y);
+void logpforw32(int nrows, int ncols, float *x, float *y) KCALL(_logpforw32,nrows,ncols,x,y);
+void logpforw64(int nrows, int ncols, double *x, double *y) KCALL(_logpforw64,nrows,ncols,x,y);
 }
