1efe33c4810899c241a3c982248875b60a17b9ac
Deniz Yuret <denizyuret@gmail.com>
2015-09-08 16:54:05 -0700

    Fixed allocation.

--- src/add2.jl
+++ src/add2.jl
@@ -18,26 +18,21 @@ end
 # if any of the input matrices is nothing, it represents the zero matrix, and we return the other matrix
 
 forw(l::Add2, ::Void, ::Void; o...)=nothing
-forw(l::Add2, x1, ::Void; y=nothing, o...)=(y==nothing ? x1 : copy!(y,x1))
-forw(l::Add2, ::Void, x2; y=nothing, o...)=(y==nothing ? x2 : copy!(y,x2))
+forw(l::Add2, x1, ::Void; y=nothing, o...)=(y==nothing ? x1 : y===x1 ? x1 : copy!(y,x1))
+forw(l::Add2, ::Void, x2; y=nothing, o...)=(y==nothing ? x2 : y===x2 ? x2 : copy!(y,x2))
 
 
 function back(l::Add2, dy; dx=nothing, returndx=true, o...)
     returndx || return
-    dx == nothing || ((l.dx1,l.dx2) = dx)
-    l.dx1 = (!isdefined(l,:dx1) ? copy(dy) :
-             l.dx1 === dy ? dy :
-             copy!(l.dx1, dy))
-    l.dx2 = (!isdefined(l,:dx2) ? dy :
-             l.dx2 === dy ? dy :
-             copy!(l.dx2, dy))
-    return (l.dx1,l.dx2)
+    dx == nothing && (dx = (similar!(l,:dx1,dy), similar!(l,:dx2,dy)))
+    dx[1] === dy || copy!(dx[1], dy)
+    dx[2] === dy || copy!(dx[2], dy)
+    return dx
 end
 
-# We want to pass back two copies of dy, but if we pass the same
-# copy and one gets overwritten the other will be corrupted.  This
-# may happen, for example, if one of our inputs has multiple
-# outputs, so its dy is updated incrementally.  So we need
-# separate copies.  In other words return (dy,dy) won't work.  We
-# can pass back dy as one of the copies, we will allocate l.dx for
-# the other.
+# We want to pass back two copies of dy, but if we pass the same copy
+# and one gets overwritten the other will be corrupted.  This may
+# happen, for example, if one of our inputs has multiple outputs, so
+# its dy is updated incrementally.  Or if one input is an overwriting
+# layer it will overwrite the one copy with its dx.  So we may need
+# separate copies.  In other words return (dy,dy) won't always work.
