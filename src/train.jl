e9c23bf2953df47aa58a000e7beb1fbe84ba667a
Deniz Yuret <denizyuret@gmail.com>
2015-09-13 00:35:55 -0700

    Deprecated for now.

--- src/train.jl
+++ src/train.jl
@@ -26,7 +26,7 @@ function test(r::RNN, x, y)
 end
 
 function backprop(r::RNN, xi, yi) # t:5680
-    err = tforw(r, xi, yi) # t:2952
+    err = tforw(r, xi, yi; train=true) # t:2952
     tback(r, yi) # t:2648
     return err
 end
@@ -70,43 +70,13 @@ function gradcheck(r::RNN, xi, yi; delta=1e-4, rtol=eps(Float64)^(1/5), atol=eps
             (wi0<0) && (wi2>0) && (wi2=0)
             w[i] = wi1; copy!(p.arr, w); loss1 = tforw(r, xi, yi; train=false)
             w[i] = wi2; copy!(p.arr, w); loss2 = tforw(r, xi, yi; train=false)
-            w[i] = wi0
+            w[i] = wi0; copy!(p.arr, w)
             dwi = (loss2 - loss1) / (wi2 - wi1)
             if !isapprox(dw[n][i], dwi; rtol=rtol, atol=atol)
                 return (n, i, dw[n][i], dwi)
             end
         end
     end
-end
-
-function batch(x, y, nb)
-    isempty(x) && return (x,y)
-    xx = Any[]
-    yy = Any[]
-    ni = length(x)    # assume x and y are same length
-    nt = length(x[1]) # assume all x[i] are same length
-    for i1=1:nb:ni
-        i2=min(ni,i1+nb-1)
-        xi = Any[]
-        yi = Any[]
-        for t=1:nt
-            xit = x[i1][t]
-            xt = similar(xit, tuple(size(xit)..., i2-i1+1))
-            for i=i1:i2; xt[:,i-i1+1] = x[i][t]; end
-            push!(xi, xt)
-            yit = y[i1][t]
-            if yit == nothing
-                yt = nothing # assumes yit=nothing for all i
-            else
-                yt = similar(yit, tuple(size(yit)..., i2-i1+1))
-                for i=i1:i2; yt[:,i-i1+1] = y[i][t]; end
-            end
-            push!(yi, yt)
-        end
-        push!(xx, xi)
-        push!(yy, yi)
-    end
-    return(xx, yy)
 end
 
 function train2(r::RNN, x, y)
@@ -127,23 +97,36 @@ function train2(r::RNN, x, y)
 end
     
 # More efficient training for rnn's that have a single output with a final output net
-# xi is a sequence of tokens, yi is the desired final output
+# x is a sequence of tokens, y is the desired final output
 # TODO: make this into a model type and have a common model interface
-function forwback2(r::RNN, o::RNN, xi, yi)
-    forw2(r, o, xi, yi; train=true)
-    err = loss(o, yi)
-    back2(r, o, xi, yi)
+function forwback2(r::RNN, o::RNN, x, y)
+    forw2(r, o, x; train=true)
+    err = loss(o, y)
+    back2(r, o, y, length(x))
     return err
 end
 
-function back2(r::RNN, o::RNN, xi, yi)
+function back2(r::RNN, o::RNN, y, nt)
+    back(o, y)
+    back(r, o.dif[nops(o)+1])
+    for t=1:nt-1; back(r, nothing); end
+end
+
+function forw2(r::RNN, o::RNN, x; a...)
+    n = nops(r)
+    forw(r, x; a...)
+    forw(o, r.out[n:n]; a...)   # passing a vector for x forces init
+end
+
+function back2a(r::RNN, o::RNN, xi, yi)
+    nt = length(xi)
     dy = back(o, yi) # TODO: this doesn't work because back does not return anything yet
     dy = o.dif[nops(o)+1]
     back(r, dy)
     for t=nt-1:-1:1; back(r, nothing); end
 end
 
-function forw2(r::RNN, o::RNN, xi, yi; a...)
+function forw2a(r::RNN, o::RNN, xi, yi; a...)
     init(r, xi[1])
     nt = length(xi)
     for t=1:nt; forw(r, xi[t]; a...); end
@@ -152,12 +135,48 @@ function forw2(r::RNN, o::RNN, xi, yi; a...)
     forw(o, ry; a...)
 end
 
-function forwback1(r::RNN, xi, yi)
+function forwback1(r::RNN, xi, yi; a...)
     init(r, xi[1])
     nt = length(xi)
-    for t=1:nt; forw(r, xi[t]); end
+    for t=1:nt; forw(r, xi[t]; a...); end
     er = loss(r, yi)
     back(r, yi)
-    for t=nt-1:-1:1; back(r, nothing); end
+    for t=nt-1:-1:1; back(r, nothing; a...); end
     return er
+end
+
+function loss2(r::RNN, o::RNN, x, y)
+    forw2(r, o, x; train=false)
+    loss(o, y)
+end
+
+function grad2(r::RNN, o::RNN, x, y; delta=1e-4, rtol=eps(Float64)^(1/5), atol=eps(Float64)^(1/5), ncheck=10)
+    forwback2(r, o, x, y)
+    dw = cell(nops(r)+nops(o))
+    for n=1:length(dw)
+        p = (n<=nops(r) ? param(r.op[n]) : param(o.op[n-nops(r)]))
+        dw[n] = (p == nothing ? nothing : convert(Array, p.diff))
+    end
+    for n=1:length(dw)
+        dw[n] == nothing && continue
+        p = (n<=nops(r) ? param(r.op[n]) : param(o.op[n-nops(r)]))
+        w = convert(Array, p.arr)
+        wlen = length(w)
+        irange = (wlen <= ncheck ? (1:wlen) : rand(1:wlen, ncheck))
+        for i in irange
+            wi0 = w[i]
+            wi1 = wi0 - delta
+            wi2 = wi0 + delta
+            # Do not cross 0 for softloss
+            (wi0>=0) && (wi1<0) && (wi1=0)
+            (wi0<0) && (wi2>0) && (wi2=0)
+            w[i] = wi1; copy!(p.arr, w); l1 = loss2(r, o, x, y)
+            w[i] = wi2; copy!(p.arr, w); l2 = loss2(r, o, x, y)
+            w[i] = wi0; copy!(p.arr, w)
+            dwi = (l2 - l1) / (wi2 - wi1)
+            if !isapprox(dw[n][i], dwi; rtol=rtol, atol=atol)
+                return (n, i, dw[n][i], dwi)
+            end
+        end
+    end
 end
