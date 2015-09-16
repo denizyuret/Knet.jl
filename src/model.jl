6ec044b13c03919d25dfffe3978678830fbf3317
Deniz Yuret <denizyuret@gmail.com>
2015-09-15 21:38:25 -0700

    Data interface.

--- src/model.jl
+++ src/model.jl
@@ -8,24 +8,31 @@ Model is an abstract type whose subtypes should provide the following:
 
 Using these low level methods, Model defines the following:
 
-* `train(model,data)`
-* `test(model,data)`
-* `predict(model,data)`
-* `setparam!(model; o...)`
+* `train(model, data)`
+* `test(model, data)`
+* `predict(model, data)`
+* `setparam!(model; param...)`
 """
 abstract Model
 
 setparam!(m::Model; o...)=(for p in params(m); setparam!(p; o...); end)
-update!(m::Model; o...)=(for p in params(m); update!(p; o...); end) # TODO: rename to update!
-gscale!(m::Model, s)=(for p in params(m); scale!(s, p.diff); end)
+update!(m::Model; o...)=(for p in params(m); update!(p; o...); end)
 wnorm(m::Model,w=0)=(for p in params(m); w += vecnorm(p.arr); end; w)
 gnorm(m::Model,g=0)=(for p in params(m); g += vecnorm(p.diff); end; g)
 
-function backprop(m::Model, x, y; getloss=true)
-    forw(m, x; train=true)
-    loss1 = getloss ? loss(m, y) : nothing
-    back(m, y)
-    return loss1
+function predict(m::Model, d::Data)
+    for (x,y) in d
+        forw(m, x; y=y, train=false)
+    end
+end
+
+function test(m::Model, d::Data)
+    sumloss = 0
+    for (x,y) in d
+        forw(m, x; train=false)
+        sumloss += loss(m, y)
+    end
+    return sumloss
 end
 
 function train(m::Model, d::Data; gclip=0, gcheck=0, getloss=true, getnorm=true)
@@ -41,20 +48,14 @@ function train(m::Model, d::Data; gclip=0, gcheck=0, getloss=true, getnorm=true)
     return (sumloss, maxwnorm, maxgnorm)
 end
 
-function test(m::Model, d::Data)
-    sumloss = 0
-    for (x,y) in d
-        forw(m, x; train=false)
-        sumloss += loss(m, y)
-    end
-    return sumloss
+function backprop(m::Model, x, y; getloss=true)
+    forw(m, x; train=true)
+    loss1 = getloss ? loss(m, y) : nothing
+    back(m, y)
+    return loss1
 end
 
-function predict(m::Model, d::Data)
-    for (x,y) in d
-        forw(m, x; y=y, train=false)
-    end
-end
+const gradcheck_rng = MersenneTwister()
 
 function gradcheck(m::Model, x, y; delta=1e-4, rtol=eps(Float64)^(1/5), atol=eps(Float64)^(1/5), gcheck=10)
     l0 = backprop(m, x, y; getloss=true)
@@ -65,11 +66,12 @@ function gradcheck(m::Model, x, y; delta=1e-4, rtol=eps(Float64)^(1/5), atol=eps
         pcopy = copy(p.arr)     # TODO: do we need pcopy?
         w = convert(Array, p.arr)
         wlen = length(w)
-        irange = (wlen <= gcheck ? (1:wlen) : rand(1:wlen, gcheck))
+        irange = (wlen <= gcheck ? (1:wlen) : rand(gradcheck_rng, 1:wlen, gcheck))
         for i in irange
             wi0 = w[i]; wi1 = (wi0 >= 0 ? wi0 + delta : wi0 - delta)
             w[i] = wi1; copy!(p.arr, w); w[i] = wi0
-            l1 = test(m,x,y)
+            forw(m, x; train=false)
+            l1 = loss(m, y)
             dwi = (l1 - l0) / (wi1 - wi0)
             if !isapprox(dw[n][i], dwi; rtol=rtol, atol=atol)
                 println(tuple(:gc, n, i, dw[n][i], dwi))
@@ -81,3 +83,10 @@ end
 
 # This will not work for MLP!  extra parameterless ops do not effect equality.
 # Base.isequal(a::Model,b::Model)=(typeof(a)==typeof(b) && isequal(params(a),params(b)))
+
+# function inittrain(m::Model, d::Data)
+#     isempty(params(m)[1]) || return
+#     (x,n) = next(d,start(d))
+#     init(m, x[1]; train=true)
+# end
+
