fd5ec6d39f0928f902c7339ed9db236d1c6868f9
Deniz Yuret <denizyuret@gmail.com>
2015-09-16 19:02:22 -0700

    ItemTensor shuffle and bootstrap implemented.

--- src/data.jl
+++ src/data.jl
@@ -11,40 +11,40 @@ abstract Data
 import Base: start, next, done
 
 type ItemTensor <: Data; x; rng; epochsize; batchsize; bootstrap; shuffle; batch;
-    function ItemTensor(x...; rng=MersenneTwister(), epochsize=ccount(x[1]), batchsize=128, bootstrap=false, shuffle=false)
+    function ItemTensor(x...; rng=MersenneTwister(), epoch=ccount(x[1]), batch=128, bootstrap=false, shuffle=false)
         nx = ccount(x[1])
         all(xi->ccount(xi)==nx, x) || error("Item count mismatch")
-        shuffle = (shuffle ? (1:nx) : nothing)
-        batch = map(xi->KUdense(cget(xi,1:batchsize)), x)
-        new(x, rng, epochsize, batchsize, bootstrap, shuffle, batch)
+        idx = (shuffle ? shuffle!(rng,[1:nx;]) : nothing)
+        buf = map(xi->KUdense(similar(xi, csize(xi,batch))), x)
+        new(x, rng, epoch, batch, bootstrap, idx, buf)
     end
 end
 
-function start(d::ItemTensor)
-    if d.shuffle != nothing
-        d.shuffle=randperm(d.rng,length(d.shuffle))
-    end
-    return 0
-end
+start(d::ItemTensor)=(d.shuffle != nothing && shuffle!(d.rng, d.shuffle); 0)
 
-function done(d::ItemTensor, n)
-    n >= d.epochsize
-end
+done(d::ItemTensor, n)=(n >= d.epochsize)
 
 function next(d::ItemTensor, n)
     nx = ccount(d.x[1])
     nb = min(d.batchsize, d.epochsize-n)
     if d.bootstrap
         ix = rand(d.rng, 1:nx, nb)
+    elseif d.shuffle != nothing
+        i1 = mod1(n+1, nx)
+        i2 = min(i1+nb-1, nx)
+        ix = d.shuffle[i1:i2]
+        while length(ix) < nb
+            shuffle!(d.rng, d.shuffle)
+            i2 = min(nb - length(ix), nx)
+            ix = [ix; d.shuffle[1:i2]]
+        end
     else
         i1 = mod1(n+1, nx)
-        i2 = mod1(n+nb, nx)
-        if d.shuffle == nothing
-            ix = (i1 < i2 ? (i1:i2) : [i1:nx; 1:i2])
-            # nb>nx?
-        else
-            error("This is not working yet")
-            # i1==1?
+        i2 = min(i1+nb-1, nx)
+        ix = (i1:i2)
+        while length(ix) < nb
+            i2 = min(nb - length(ix), nx)
+            ix = [ix; 1:i2]
         end
     end
     length(ix) == nb || error()
