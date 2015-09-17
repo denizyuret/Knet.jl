231ce2f04566a9c637781c8965d57be3ea81d459
Deniz Yuret <denizyuret@gmail.com>
2015-09-16 20:21:00 -0700

    Tested new MNIST data generator.

--- test/testmnist.jl
+++ test/testmnist.jl
@@ -1,20 +1,20 @@
 using Base.Test
 using KUnet
+using KUnet: MNIST
 include("isapprox.jl")
-isdefined(:MNIST) || include("mnist.jl")
 setseed(42)
 nbatch=100
+
+dtrn = TrainMNIST(batch=nbatch)
+dtst = TestMNIST(batch=nbatch)
 
 x0 = copy(MNIST.xtrn)
 y0 = copy(MNIST.ytrn)
 
-info("Testing simple ffnn")
+info("Testing simple mlp")
 net = Net(Mmul(64), Bias(), Relu(), 
           Mmul(10), Bias(), XentLoss())
 setparam!(net, lr=0.5)
-
-dtrn = ItemTensor(MNIST.xtrn, MNIST.ytrn; batchsize=nbatch) # TODO: try other batch sizes
-dtst = ItemTensor(MNIST.xtst, MNIST.ytst; batchsize=nbatch)
 
 test(net, dtst)                 # to init weights
 mlp = deepcopy(net.op)
@@ -38,15 +38,11 @@ lenet = Net(Conv(20,5), Bias(), Relu(), Pool(2),
             Mmul(500), Bias(), Relu(),
             Mmul(10), Bias(), XentLoss())
 setparam!(lenet; lr=0.1)
-xtrn2 = reshape(MNIST.xtrn, 28, 28, 1, size(MNIST.xtrn, 2))
-xtst2 = reshape(MNIST.xtst, 28, 28, 1, size(MNIST.xtst, 2))
-ytrn2 = MNIST.ytrn
-ytst2 = MNIST.ytst
 
-for a in (:xtrn2,:xtst2,:ytrn2,:ytst2) @eval $a=KUnet.cget($a,1:100); end
+# single epoch training
+dtrn1 = TrainMNIST(batch=nbatch,epoch=nbatch)
 
-dtrn2 = ItemTensor(xtrn2,ytrn2; batchsize=nbatch)
-test(lenet, dtrn2)
+test(lenet, dtst) # to initialize weights
 lenet0 = deepcopy(lenet)
 lemlp = deepcopy(lenet.op)
 
@@ -55,16 +51,16 @@ lemlp = deepcopy(lenet.op)
 @show map(isequal, params(lenet), params(lemlp))
 
 @time for i=1:1
-    println(train(lenet, dtrn2))
-    train(lemlp, xtrn2, ytrn2; batch=nbatch)
+    println(train(lenet, dtrn1))
+    train(lemlp, csub(MNIST.xtrn,1:100), csub(MNIST.ytrn,1:100); batch=nbatch)
 
     # @show (i,1,map(vecnorm,params(lenet)),map(difnorm,params(lenet)))
     # @show (i,1,map(vecnorm,params(lemlp)),map(difnorm,params(lemlp)))
     @show map(isequal, params(lenet), params(lemlp))
     @show map(isapprox, params(lenet), params(lemlp))
     # @test all(map(isequal, params(lenet), params(lemlp)))
-    println((i, accuracy(ytst2, predict(lemlp, xtst2)), 
-                accuracy(ytrn2, predict(lemlp, xtrn2))))
+    println((i, accuracy(MNIST.ytst, predict(lemlp, MNIST.xtst)),
+                accuracy(MNIST.ytrn, predict(lemlp, MNIST.xtrn))))
 end
 
 @test isequal(x0,MNIST.xtrn)
