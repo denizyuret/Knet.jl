40f4e8741fcb1cfec1c2df280b64c3f2b476d4db
Deniz Yuret <denizyuret@gmail.com>
2015-09-16 18:13:58 -0700

    back in s2c needs seq=true.

--- src/arch.jl
+++ src/arch.jl
@@ -45,7 +45,7 @@ loss(r::S2C,y)=loss(r.net2,y)
 function forw(r::S2C, x::Vector; y=nothing, a...)
     n = nops(r.net1)            
     forw(r.net1, x; a...)
-    forw(r.net2, r.net1.out[n:n]; y=y, a...)   # passing a sequence [n:n] to net2 forces init
+    forw(r.net2, r.net1.out[n]; y=y, a...)   # passing a sequence [n:n] to net2 forces init
     # TODO: implement lastout or direct write from net1 out to net2 input
     # You can do the latter if net2 is initialized first or just overwrite its buf0
     # and check for === before copy
@@ -53,7 +53,7 @@ end
 
 function back(r::S2C, y)
     back(r.net2, y)
-    back(r.net1, r.net2.dif[nops(r.net2)+1])
-    while r.net1.sp > 0; back(r.net1, nothing); end
+    back(r.net1, r.net2.dif[nops(r.net2)+1]; seq=true)
+    while r.net1.sp > 0; back(r.net1, nothing; seq=true); end
 end
 
