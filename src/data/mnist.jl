af7979c99b2d0d20ca9ad8a7a0a0e6962f570f3b
Deniz Yuret <denizyuret@gmail.com>
2015-09-19 22:52:50 -0700

    work in progress.

--- src/data/mnist.jl
+++ src/data/mnist.jl
@@ -7,6 +7,8 @@ const ytrn_file = "train-labels-idx1-ubyte.gz"
 const xtst_file = "t10k-images-idx3-ubyte.gz"
 const ytst_file = "t10k-labels-idx1-ubyte.gz"
 xtrn = ytrn = xtst = ytst = nothing
+xtrnS = ytrnS = xtstS = ytstS = nothing
+xtrn4 = ytrn4 = xtst4 = ytst4 = nothing
 
 function wgetzcat(gz)
     isfile(gz) || run(`wget $mnisturl/$gz`)
@@ -29,10 +31,29 @@ end
 
 function loadmnist()
     global xtrn, ytrn, xtst, ytst
+    xtrn == nothing || return
     xtrn = readimages(xtrn_file)
     ytrn = readlabels(ytrn_file)
     xtst = readimages(xtst_file)
     ytst = readlabels(ytst_file)
+end
+
+function conv4dmnist()
+    global xtrn4, ytrn4, xtst4, ytst4
+    xtrn == nothing && loadmnist()
+    xtrnS = sparse(xtrn)
+    ytrnS = sparse(ytrn)
+    xtstS = sparse(xtst)
+    ytstS = sparse(ytst)
+end
+
+function sparsemnist()
+    global xtrnS, ytrnS, xtstS, ytstS
+    xtrn == nothing && loadmnist()
+    xtrnS = sparse(xtrn)
+    ytrnS = sparse(ytrn)
+    xtstS = sparse(xtst)
+    ytstS = sparse(ytst)
 end
 
 end # module
@@ -43,8 +64,14 @@ handwritten digit recognition problem from
 http://yann.lecun.com/exdb/mnist.  It takes the same keyword arguments
 as ItemTensor.  The data is downloaded if necessary.
 """
-function MNIST(;o...)
+function MNIST(; sparse=false, conv4d=true, o...)
     Mnist.xtrn == nothing && (@date Mnist.loadmnist())
-    (ItemTensor(Mnist.xtrn, Mnist.ytrn; o...),
-     ItemTensor(Mnist.xtst, Mnist.ytst; o...))
+    if !sparse
+        (ItemTensor(Mnist.xtrn, Mnist.ytrn; o...),
+         ItemTensor(Mnist.xtst, Mnist.ytst; o...))
+    else
+        Mnist.xtrnS == nothing && (@date Mnist.sparsemnist())
+        (ItemTensor(Mnist.xtrnS, Mnist.ytrnS; o...),
+         ItemTensor(Mnist.xtstS, Mnist.ytstS; o...))
+    end
 end
