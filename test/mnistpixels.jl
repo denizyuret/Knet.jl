54625fc741f7a019b1b85f1fb785908b483370c4
Deniz Yuret <denizyuret@gmail.com>
2015-09-20 01:05:48 -0700

    reorganizing mnist examples

--- test/mnistpixels.jl
+++ test/mnistpixels.jl
@@ -4,7 +4,9 @@
 # preprint arXiv:1504.00941.
 
 using KUnet
+using KUnet: nextidx
 using ArgParse
+isdefined(:MNIST) || include("mnist.jl")
 
 function parse_commandline(a=ARGS)
     s = ArgParseSettings()
@@ -68,20 +70,51 @@ function parse_commandline(a=ARGS)
     parse_args(a,s)
 end
 
+import Base: start, next, done
+
+"""
+This is the data generator for the pixel-by-pixel MNIST problem from:
+Le, Q. V., Jaitly, N., & Hinton, G. E. (2015). A Simple Way to
+Initialize Recurrent Networks of Rectified Linear Units. arXiv
+preprint arXiv:1504.00941.
+"""
+type Pixels; x; rng; datasize; epochsize; batchsize; bootstrap; shuffle; batch;
+    function Pixels(x...; rng=MersenneTwister(), epoch=ccount(x[1]), batch=16, bootstrap=false, shuffle=false)
+        nx = ccount(x[1])
+        all(xi->ccount(xi)==nx, x) || error("Item count mismatch")
+        idx = (shuffle ? shuffle!(rng,[1:nx;]) : nothing)
+        xbatch = [ similar(x[1], (1,batch)) for i=(1:clength(x[1])) ]
+        ybatch = similar(x[2], (clength(x[2]),batch))
+        new(x, rng, nx, epoch, batch, bootstrap, idx, (xbatch,ybatch))
+    end
+end
+
+start(d::Pixels)=(d.shuffle != nothing && shuffle!(d.rng, d.shuffle); 0)
+
+done(d::Pixels, n)=(n >= d.epochsize)
+
+function next(d::Pixels, n)
+    idx = nextidx(d,n)
+    nb = length(idx)
+    nt = clength(d.x[1])
+    for b=1:nb
+        i=idx[b]
+        t0 = (i-1)*nt
+        @inbounds for t=1:nt
+            d.batch[1][t][b] = d.x[1][t0 + t]
+        end
+        d.batch[2][:,b] = d.x[2][:,i]
+    end
+    (d.batch, n+nb)
+end
+
+
 args = parse_commandline(isdefined(:myargs) ? split(myargs) : ARGS)
 println(args)
 args["seed"] > 0 && setseed(args["seed"])
 
-(dtrn,dtst)=MNIST()
-trn = Pixels(dtrn.data[1], dtrn.data[2]; batch=args["batchsize"], epoch=args["train"], bootstrap=true)
-tst = Pixels(dtst.data[1], dtst.data[2]; batch=args["batchsize"])
-
-# TODO: reconsider the data interface.
-# we should make the model interface not insist on Data, any iterable should do, in fact remove the Data type.
-# x field should probably be named something else like data.
-# if we are ever going to need zero or more than one input, then we need to decide how to represent:
-# (x1,x2,...,y) or (y,x1,x2,...): probably the first
-# (y,) or y for no input tuples: probably the first
+trn = Pixels(MNIST.xtrn, MNIST.ytrn; batch=args["batchsize"], epoch=args["train"], bootstrap=true)
+tst = Pixels(MNIST.xtst, MNIST.ytst; batch=args["batchsize"])
 
 nx = 1
 ny = 10
@@ -105,8 +138,17 @@ setparam!(net; lr=args["lrate"])
     flush(STDOUT)
 end
 
+# NOTES:
+
 # batchsize=16 is too slow.  I got good progress with the following:
 # Dict{AbstractString,Any}("hidden"=>100,"rtol"=>0.01,"batchsize"=>200,"lrate"=>0.001,"train"=>10000,"gclip"=>1.0,"acc"=>10,"gcheck"=>0,"fbias"=>1.0,"epochs"=>100,"atol"=>0.01,"seed"=>1003,"type"=>"irnn")
+
+# DONE: reconsider the data interface.
+# we should make the model interface not insist on Data, any iterable should do, in fact remove the Data type.
+# x field should probably be named something else like data.
+# if we are ever going to need zero or more than one input, then we need to decide how to represent:
+# (x1,x2,...,y) or (y,x1,x2,...): probably the first
+# (y,) or y for no input tuples: probably the first
 
 # Sample output for debugging:
 # julia> include("mnistpixels.jl")
