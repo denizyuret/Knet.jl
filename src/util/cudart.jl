4b435ad0d8f66b023150a9236ce55c158088610a
Deniz Yuret <denizyuret@gmail.com>
2015-09-22 16:16:03 -0700

    Added getbytes.

--- src/util/cudart.jl
+++ src/util/cudart.jl
@@ -71,3 +71,46 @@ gpucopy_internal{T<:Number}(x::Array{T}, s::ObjectIdDict)=(haskey(s,x)||(s[x]=Cu
 Base.getindex{T}(x::CudaArray{T}, i::Integer)=copy!(T[0], 1, x, i, 1)[1]
 Base.setindex!{T}(x::CudaArray{T}, v, i::Integer)=copy!(x, i, T[convert(T,v)], 1, 1)
 Base.endof(x::CudaArray)=length(x)
+
+# Finding memory usage:
+
+_getbytes(x::DataType,d) = sizeof(x)
+
+_getbytes(x::NTuple,d)=sum(map(y->_getbytes(y,d), x))
+
+_getbytes(x::AbstractCudaArray,d)=(haskey(d,x) ? 0 : (d[x]=1; length(x) * sizeof(eltype(x))))
+
+function _getbytes(x::DenseArray,d) 
+    haskey(d,x) && return 0; d[x]=1
+    total = sizeof(x)
+    if !isbits(eltype(x))
+        for i = 1:length(x)
+            isize = _getbytes(x[i],d)
+            # @show (typeof(x), i, isize)
+            total += isize
+        end
+    end
+    # @show (typeof(x), total)
+    # @show (eltype(x), size(x), total)
+    return total
+end
+
+function _getbytes(x,d)
+    total = sizeof(x)
+    isbits(x) && return total
+    haskey(d,x) && return 0; d[x]=1
+    fieldNames = fieldnames(x)
+    if fieldNames != ()
+        for fieldName in fieldNames
+            isdefined(x, fieldName) || continue
+            f = x.(fieldName)
+            fieldBytes = _getbytes(f,d)
+            fieldSize = (isa(f, Union(AbstractArray,CudaArray)) ? (eltype(f),size(f)) : ())
+            # @show (typeof(x), fieldName, fieldSize, fieldBytes)
+            total += fieldBytes
+        end
+    end
+    return total
+end
+
+getbytes(x)=_getbytes(x, ObjectIdDict())
