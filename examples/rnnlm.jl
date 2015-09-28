a9cfb15fad867ca1135ee5d374f2dde203349a0d
Deniz Yuret <denizyuret@gmail.com>
2015-09-28 09:31:27 -0700

    *** empty log message ***

--- examples/rnnlm.jl
+++ examples/rnnlm.jl
@@ -1,18 +1,41 @@
-# Parameters from the Zaremba implementation:
-batch_size=20
-seq_length=20
-layers=2
-decay=2
-rnn_size=200
-dropout=0
-init_weight=0.1
-lr=1
-vocab_size=10000
-max_epoch=4
-max_max_epoch=13
-max_grad_norm=5
+# This is to replicate the language modeling experiments from Zaremba,
+# Wojciech, Ilya Sutskever, and Oriol Vinyals. "Recurrent neural
+# network regularization." arXiv preprint arXiv:1409.2329 (2014).
+#
+# Usage: julia rnnlm.jl ptb.train.txt ptb.valid.txt ptb.test.txt
+# Type julia rnnlm.jl --help for more options
 
+using KUnet, ArgParse
 import Base: start, next, done
+
+function main(args=ARGS)
+    opts = parse_commandline(args)
+    println(opts)
+    opts["seed"] > 0 && setseed(opts["seed"])
+    dict = Dict{Any,Int32}()
+    data = Any[]
+    for f in opts["datafiles"]
+        push!(data, LMData(f;  batch=opts["batch_size"], seqlen=opts["seq_length"], dict=dict))
+    end
+    vocab_size = length(dict)
+    lstms = Any[]
+    for i=1:opts["layers"]; push!(lstms, LSTM(opts["rnn_size"]; dropout=opts["dropout"])); end
+    net = Net(Mmul(opts["rnn_size"]), lstms..., Mmul(vocab_size), Bias(), Soft(), SoftLoss())
+    lr = opts["lr"]
+    setparam!(net, lr=lr, init=rand!, initp=(-opts["init_weight"], opts["init_weight"]))
+    perp = zeros(length(data))
+    wmax = gmax = 0
+    for ep=1:opts["max_max_epoch"]
+        ep > opts["max_epoch"] && (lr /= opts["decay"]; setparam!(net, lr=lr))
+        @date (ltrn,wmax,gmax) = train(net, data[1]; gclip=opts["max_grad_norm"], gcheck=opts["gcheck"], keepstate=true)
+        perp[1] = exp(ltrn/data[1].seqlength)
+        for idata = 2:length(data)
+            @date ldev = test(net, data[idata]; keepstate=true)
+            perp[idata] = exp(ldev/data[idata].seqlength) # TODO: look into reporting loss per sequence rather than per token
+        end
+        @show (ep, perp..., wmax, gmax, lr)
+    end
+end
 
 type LMData; data; dict; batchsize; seqlength; batch; end
 
@@ -61,60 +84,121 @@ function next(d::LMData,nword)                              # d.data is the whol
     ((xbatch, ybatch), nword + d.seqlength * d.batchsize)	# each call to next will deliver d.seqlength words from each of the d.batchsize segments
 end
 
-
-dir = "data"
-trn = LMData("$dir/ptb.train.txt")
-dev = LMData("$dir/ptb.valid.txt"; dict=trn.dict)
-tst = LMData("$dir/ptb.test.txt"; dict=trn.dict)
-
-vocab_size = length(trn.dict)   #DBG
-@assert length(trn.dict) == vocab_size "$((length(trn.dict), vocab_size))"
-
-using KUnet
-
-lstms = Any[]
-for i=1:layers; push!(lstms, LSTM(rnn_size; dropout=dropout)); end
-net = Net(Mmul(rnn_size), lstms..., Mmul(vocab_size), Bias(), Soft(), SoftLoss())
-setparam!(net, lr=lr, init=rand!, initp=(-init_weight, init_weight))
-setseed(42)
-
-for ep=1:max_max_epoch
-    ep > max_epoch && (lr /= decay; setparam!(net, lr=lr))
-    @time (ltrn,w,g) = train(net, trn; gclip=max_grad_norm, keepstate=true)
-    ptrn = exp(ltrn/trn.seqlength)
-    @time ldev = test(net, dev; keepstate=true)
-    pdev = exp(ldev/dev.seqlength)
-    @time ltst = test(net, tst; keepstate=true)
-    ptst = exp(ltst/tst.seqlength)
-    @show (ep, ptrn, pdev, ptst, w, g)
+function parse_commandline(args)
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--batch_size"
+        help = "minibatch size"
+        arg_type = Int
+        default = 20
+        "--seq_length"
+        help = "length of the input sequence"
+        arg_type = Int
+        default = 20
+        "--layers"
+        help = "number of lstm layers"
+        arg_type = Int
+        default = 2
+        "--decay"
+        help = "divide learning rate by this every epoch after max_epoch"
+        arg_type = Float64
+        default = 2.0
+        "--rnn_size"
+        help = "size of the lstm"
+        arg_type = Int
+        default = 200
+        "--dropout"
+        help = "dropout units with this probability"
+        arg_type = Float64
+        default =  0.0
+        "--init_weight"
+        help = "initialize weights uniformly in [-init_weight,init_weight]"
+        arg_type = Float64
+        default =  0.1
+        "--lr"
+        help = "learning rate"
+        arg_type = Float64
+        default = 1.0
+        "--vocab_size"
+        help = "size of the vocabulary"
+        arg_type = Int
+        default = 10000
+        "--max_epoch"
+        help = "number of epochs at initial lr"
+        arg_type = Int
+        default = 4
+        "--max_max_epoch"
+        help = "number of epochs to train"
+        arg_type = Int
+        default = 13
+        "--max_grad_norm"
+        help = "gradient clip"
+        arg_type = Float64
+        default = 5.0
+        "--gcheck"
+        help = "gradient check this many units per layer"
+        arg_type = Int
+        default = 0
+        "--seed"
+        help = "random seed, use 0 to turn off"
+        arg_type = Int
+        default = 42
+        "--preset"
+        help = "load one of the preset option combinations"
+        arg_type = Int
+        default = 0
+        "datafiles"
+        help = "corpus files: first one will be used for training"
+        nargs = '+'
+        required = true
+    end
+    opts = parse_args(args,s)
+    # Parameters from the Zaremba implementation:
+    if opts["preset"] == 1      # Trains 1h and gives test 115 perplexity.
+        opts["batch_size"]=20
+        opts["seq_length"]=20
+        opts["layers"]=2
+        opts["decay"]=2
+        opts["rnn_size"]=200
+        opts["dropout"]=0
+        opts["init_weight"]=0.1
+        opts["lr"]=1
+        opts["vocab_size"]=10000
+        opts["max_epoch"]=4
+        opts["max_max_epoch"]=13
+        opts["max_grad_norm"]=5
+    elseif opts["preset"] == 2  # Train 1 day and gives 82 perplexity.
+        opts["batch_size"]=0
+        opts["seq_length"]=35
+        opts["layers"]=2
+        opts["decay"]=1.15
+        opts["rnn_size"]=1500
+        opts["dropout"]=0.65
+        opts["init_weight"]=0.04
+        opts["lr"]=1
+        opts["vocab_size"]=10000
+        opts["max_epoch"]=14
+        opts["max_max_epoch"]=55
+        opts["max_grad_norm"]=10
+    end
+    return opts
 end
 
-### DEAD CODE:
-# The input is a text file with word tokens, single sentence per line.
-# 1. We can take the whole corpus as one sequence and use truncated BPTT.
-# -- zaremba uses T=5 B=20 blocks
-# -- this requires not resetting the hidden states.
-# -- k1 and k2 are both 5 in Sutskever's truncated BPTT formulation.
-# 2. We can take individual sentences as sequences and train normally.
-# -- means a lot fewer updates (1 per sentence*minibatch)
-# -- similar per epoch time expected with fewer updates
-# -- don't need to change any code
-# -- sort sentences by length to minimize padding
-#
-# Architecture:
-# x,y both come in as sparse matrices size VxB
-# Mmul(HxV) will convert to embedding, needs to handle sparse x
-# Followed by 2xLSTM(H)
-# Followed by Mmul(VxH) to convert to embedding
-# Followed by an XentLoss layer which needs to handle sparse dy, dense y
-#
-# TODO:
-# - make sure Mmul and XentLoss can handle sparse input
-# - Find a way to recover hidden state after back for truncated BPTT.
-# - registers: out, dif, dif-incr
-# - we want to recover out, the hidden states are there
-# - we can get rid of dif and dif-incr? yes we have already performed the update.
-# - actually the states that we want are already in out0! we can recover them after back.
-# - train should pass a keepstate option down to backprop->back and back should recover the state.
-# - or the next forw should set out=out0 instead of out=nothing.
-# - initsequence should do that instead of out=nothing.
+main()
+
+
+### SAMPLE RUN:
+# julia rnnlm.jl --preset 1 data/ptb.valid.txt data/ptb.test.txt 
+# WARNING: requiring "Options" did not define a corresponding module.
+# Dict{AbstractString,Any}("rnn_size"=>200,"lr"=>1,"decay"=>2,"batch_size"=>20,"dropout"=>0,"max_grad_norm"=>5,"preset"=>1,"max_epoch"=>4,"init_weight"=>0.1,"gcheck"=>0,"layers"=>2,"vocab_size"=>10000,"seq_length"=>20,"seed"=>42,"max_max_epoch"=>13,"datafiles"=>Any["data/ptb.valid.txt","data/ptb.test.txt"])
+# INFO: Read data/ptb.valid.txt: 73760 words, 6022 vocab.
+# INFO: Read data/ptb.test.txt: 82430 words, 7596 vocab.
+#  37.057684 seconds (29.17 M allocations: 2.530 GB, 1.31% gc time)
+#   9.723167 seconds (11.06 M allocations: 496.906 MB, 1.30% gc time)
+# (ep,perp...,wmax,gmax,lr) = (1,964.7574812581911,680.8404134165187,359.66156655473776,182.60932042613925,1)
+#  32.492913 seconds (24.07 M allocations: 2.306 GB, 1.39% gc time)
+#   9.682416 seconds (11.04 M allocations: 496.187 MB, 1.23% gc time)
+# (ep,perp...,wmax,gmax,lr) = (2,533.9658674987758,500.7701607049704,376.4021748281246,29.527137764770796,1)
+#  32.433211 seconds (24.07 M allocations: 2.306 GB, 1.39% gc time)
+#   9.759406 seconds (11.05 M allocations: 496.439 MB, 1.30% gc time)
+# (ep,perp...,wmax,gmax,lr) = (3,390.1504382596744,435.37182169855157,390.84524322483344,30.692745370555468,1)
