f6bdbde544f8d3dd812d9a8ca017e5a443077053
Deniz Yuret <denizyuret@gmail.com>
2015-09-28 10:09:32 -0700

    *** empty log message ***

--- examples/mnist2d.jl
+++ examples/mnist2d.jl
@@ -3,6 +3,7 @@
 using Base.Test
 using KUnet
 isdefined(:MNIST) || include("mnist.jl")
+include("mlp.jl")
 setseed(42)
 nbatch=100
 
@@ -14,36 +15,7 @@ y0 = copy(dtrn.data[2])
 
 info("Testing simple mlp")
 
-softmax() = quote
-    x1 = input()
-    x2 = soft(x1)
-    x3 = softloss(x2)
-end
-
-layer(;n=1,f=nothing) = quote
-    x1 = input()
-    w1 = par($n,0)
-    x2 = dot(w1,x1)
-    b2 = par(0)
-    x3 = add(b2,x2)
-    y3 = $(symbol(f))(x3)
-end
-
-function mlp(loss, actf, hidden...)
-    prog = quote
-        x0 = input()
-    end
-    N = length(hidden)
-    for n=1:N
-        x1 = symbol("x$(n-1)")
-        x2 = symbol("x$n")
-        push!(prog.args, :($x2 = layer($x1; n=$(hidden[n]), f=$(n<N ? actf : loss))))
-    end
-    return prog
-end
-
-prog = mlp(softmax, relu, 64, 10)
-
+prog = mlp(layers=(64,10), loss=softmax, actf=relu, winit=Gaussian(0,.01), binit=Constant(0))
 net = Net(prog)
 
 setopt!(net, lr=0.5)
@@ -55,3 +27,20 @@ end
 
 @test isequal(x0,dtrn.data[1])
 @test isequal(y0,dtrn.data[2])
+
+
+### SAMPLE RUN
+
+# INFO: Loading MNIST...
+#   5.736248 seconds (362.24 k allocations: 502.003 MB, 1.35% gc time)
+# INFO: Testing simple mlp
+# (l,w,g) = train(net,dtrn; gclip=0,gcheck=100,getloss=true,getnorm=true,atol=0.01,rtol=0.001) = (0.37387532f0,18.511799f0,2.8433793f0)
+# (test(net,dtrn),accuracy(net,dtrn)) = (0.21288027f0,0.9327666666666666)
+# (test(net,dtst),accuracy(net,dtst)) = (0.2148458f0,0.9289)
+# (l,w,g) = train(net,dtrn; gclip=0,gcheck=100,getloss=true,getnorm=true,atol=0.01,rtol=0.001) = (0.14995994f0,22.26936f0,3.9932733f0)
+# (test(net,dtrn),accuracy(net,dtrn)) = (0.13567321f0,0.9574)
+# (test(net,dtst),accuracy(net,dtst)) = (0.14322147f0,0.9546)
+# (l,w,g) = train(net,dtrn; gclip=0,gcheck=100,getloss=true,getnorm=true,atol=0.01,rtol=0.001) = (0.10628127f0,24.865437f0,3.5134742f0)
+# (test(net,dtrn),accuracy(net,dtrn)) = (0.100041345f0,0.9681833333333333)
+# (test(net,dtst),accuracy(net,dtst)) = (0.114785746f0,0.9641)
+#  10.373502 seconds (11.14 M allocations: 557.680 MB, 1.25% gc time)
