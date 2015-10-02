1d83481baee2a1b00a48099d416895a23d4011c8
Deniz Yuret <denizyuret@gmail.com>
2015-10-02 14:19:03 -0700

    Argument processing.

--- examples/linreg.jl
+++ examples/linreg.jl
@@ -1,6 +1,6 @@
-using Knet
-
 # Simple linear regression.
+
+using Knet, ArgParse
 
 # Data generator:
 import Base: start, next, done
@@ -8,39 +8,55 @@ import Base: start, next, done
 type LinReg; w; batchsize; epochsize; noise; end
 
 function LinReg(outputs,inputs,batchsize,epochsize,noise)
-    LinReg(rand(outputs,inputs),batchsize,epochsize,noise)
+    LinReg(randn(outputs,inputs),batchsize,epochsize,noise)
 end
 
 function next(l::LinReg, n)
     (outputs, inputs) = size(l.w)
-    x = rand(inputs, batchsize)
+    x = rand(inputs, l.batchsize)
     y = l.w * x + scale(l.noise, randn(outputs, l.batchsize))
-    return ((x,y), n+batchsize)
+    return ((x,y), n+l.batchsize)
 end
 
 start(::LinReg)=0
 done(l::LinReg,n)=(n >= l.epochsize)
 
-# main()
+# Linear regression model:
 
-# function main()
-    inputs = 100
-    outputs = 10
-    batchsize = 20
-    epochsize = 1000
-    epochs = 10
-    noise = 0.01
+LinRegModel(n) = quote
+    x = input()
+    w = par($n,0)
+    y = dot(w,x)
+    z = quadloss(y)
+end
+
+# Main loop:
+
+function linreg(args=ARGS)
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        ("--inputs"; arg_type=Int; default=100)
+        ("--outputs"; arg_type=Int; default=10)
+        ("--batchsize"; arg_type=Int; default=20)
+        ("--epochsize"; arg_type=Int; default=10000)
+        ("--epochs"; arg_type=Int; default=5)
+        ("--noise"; arg_type=Real; default=0.01)
+        ("--lr"; arg_type=Real; default=0.05)
+        ("--seed"; arg_type=Int; default=42)
+    end
+    isa(args, AbstractString) && (args=split(args))
+    opts = parse_args(args,s)
+    println(opts)
+    for (k,v) in opts; @eval ($(symbol(k))=$v); end
+    seed > 0 && setseed(seed)
     data = LinReg(outputs, inputs, batchsize, epochsize, noise)
-    prog = quote
-        x = input()
-        w = par($outputs,0)
-        y = dot(w,x)
-        z = qloss(y)
-    end
-    net = Net(prog)
-     net.dbg = true
+    net = Net(LinRegModel(outputs))
+    setopt!(net; lr=lr)
+    lwg = nothing
     for epoch = 1:epochs
-        @show train(net, data)
+        @show lwg = train(net, data)
     end
-# end
+    return lwg
+end
 
+!isinteractive() && !isdefined(:load_only) && linreg(ARGS)
