83ee077bbdf670c62db3b937ac1f2843f0bb68dd
Deniz Yuret <denizyuret@gmail.com>
2015-10-18 01:49:52 +0300

    update!: some parameters can have out or dif = nothing, (s2s)

--- src/update.jl
+++ src/update.jl
@@ -2,25 +2,32 @@ using Base.LinAlg: axpy!, scale!
 
 function update!(p::Par; gclip=0, o...)
     initupdate(p)
-    gclip > 0 && scale!(gclip, p.dif) # this is not a per-parameter deal, we need the gnorm for the whole model
-    nz(p,:l1reg,0) && l1reg!(p.l1reg, p.out, p.dif)
-    nz(p,:l2reg,0) && l2reg!(p.l2reg, p.out, p.dif)
-    nz(p,:adagrad,false) && adagrad!(1e-8, p.ada, p.dif)
-    nz(p,:momentum,0) && momentum!(p.momentum, p.mom, p.dif)
-    nz(p,:nesterov,0) && nesterov!(p.nesterov, p.nes, p.dif)
-    nz(p,:lr,1) && scale!(p.lr, p.dif)
-    axpy!(-1, p.dif, p.out)
-    nz(p,:average,false) && axpy!(1,p.out,p.avg)
-    # nz(p,:maxnorm,0) && maxnorm!(p.maxnorm, p.out)
+    if p.dif != nothing
+        gclip > 0 && scale!(gclip, p.dif) # this is not a per-parameter deal, we need the gnorm for the whole model
+        nz(p,:l1reg,0) && l1reg!(p.l1reg, p.out, p.dif)
+        nz(p,:l2reg,0) && l2reg!(p.l2reg, p.out, p.dif)
+        nz(p,:adagrad,false) && adagrad!(1e-8, p.ada, p.dif)
+        nz(p,:momentum,0) && momentum!(p.momentum, p.mom, p.dif)
+        nz(p,:nesterov,0) && nesterov!(p.nesterov, p.nes, p.dif)
+        nz(p,:lr,1) && scale!(p.lr, p.dif)
+        axpy!(-1, p.dif, p.out)
+    end
+    if p.out != nothing
+        nz(p,:average,false) && axpy!(1,p.out,p.avg)
+    end
 end
 
 nz(p,n,v)=(isdefined(p,n) && (p.(n) != v))
 
 function initupdate(p::Par)
-    nz(p,:average,false) && similar!(p, :avg, p.out; fill=0)
-    nz(p,:adagrad,false) && similar!(p, :ada, p.dif; fill=0)
-    nz(p,:momentum,0) && similar!(p, :mom, p.dif; fill=0)
-    nz(p,:nesterov,0) && similar!(p, :nes, p.dif; fill=0)
+    if p.dif != nothing
+        nz(p,:adagrad,false) && similar!(p, :ada, p.dif; fill=0)
+        nz(p,:momentum,0) && similar!(p, :mom, p.dif; fill=0)
+        nz(p,:nesterov,0) && similar!(p, :nes, p.dif; fill=0)
+    end
+    if p.out != nothing
+        nz(p,:average,false) && similar!(p, :avg, p.out; fill=0)
+    end
 end
 
 l1reg!(l1, w, dw)=for i=1:length(dw); (w[i]>zero(w[i])) ? (dw[i]+=l1) : (w[i]<zero(w[i])) ? (dw[i]-=l1) : 0; end
