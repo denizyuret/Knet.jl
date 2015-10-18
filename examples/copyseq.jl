42b58a71bcb3d3895466a1fc18cc68eef2a87e55
Deniz Yuret <denizyuret@gmail.com>
2015-10-18 13:56:17 +0300

    cleanup.

--- examples/copyseq.jl
+++ examples/copyseq.jl
@@ -5,14 +5,16 @@ function copyseq(args=ARGS)
     s = ArgParseSettings()
     @add_arg_table s begin
         ("datafiles"; nargs='+'; required=true; help="First file used for training")
-        ("--batchsize"; arg_type=Int; default=20)
-        ("--ftype"; default="Float32")
-        ("--dense"; action=:store_true)
         ("--epochs"; arg_type=Int; default=10)
         ("--hidden"; arg_type=Int; default=100)
-        ("--gcheck"; arg_type=Int; default=0)
+        ("--batchsize"; arg_type=Int; default=128)
         ("--gclip"; arg_type=Float64; default=10.0)
         ("--lr"; arg_type=Float64; default=2.0)
+        ("--ftype"; default="Float32")
+        ("--dense"; action=:store_true)
+        ("--getnorm"; action=:store_true)
+        ("--getloss"; action=:store_true)
+        ("--gcheck"; arg_type=Int; default=0)
         ("--seed"; arg_type=Int; default=42)
     end
     isa(args, AbstractString) && (args=split(args))
@@ -23,26 +25,33 @@ function copyseq(args=ARGS)
     data = Any[]
     dict = [ Dict{Any,Int32}() for i=1:2 ]
     for f in datafiles
-        push!(data, S2SData(f, f; batch=batchsize, ftype=eval(parse(ftype)), dense=dense, dict1=dict[1], dict2=dict[2]))
+        push!(data, S2SData(f, f; batch=batchsize, ftype=eval(parse(ftype)), dense=dense, dict1=dict[1], dict2=dict[2])) # , stop=6400)) #DBG
     end
-    # length(data)==1 && push!(data, data[1]) # If no test data specified use the training data
     global model = S2S(lstm; hidden=hidden, vocab=length(dict[2]))
     setopt!(model; lr=lr)
     wmax = gmax = 0
     perp = zeros(length(data))
-    train(model, data[1], softloss; gcheck=gcheck, gclip=gclip) #DBG: pretrain to compile for timing
-    
-    @time for epoch=1:epochs
-        (loss,wmax,gmax) = train(model, data[1], softloss; gcheck=gcheck, gclip=gclip)
+    t0 = time_ns()
+    for epoch=1:epochs
+        (loss,wmax,gmax) = train(model, data[1], softloss; gcheck=gcheck, gclip=gclip, getnorm=getnorm, getloss=getloss)
         perp[1] = exp(loss)
         for d=2:length(data)
             loss = test(model, data[d], softloss)
             perp[d] = exp(loss)
         end
-        println((epoch,perp...,loss,wmax,gmax))
+        myprint(epoch,(time_ns()-t0)/1e9,perp...,loss,wmax,gmax)
     end
     return (perp..., wmax, gmax)
 end
 
+myprint(a...)=(for x in a; @printf("%-6g ",x); end; println(); flush(STDOUT))
+
 !isinteractive() && !isdefined(:load_only) && copyseq(ARGS)
 
+
+### DEAD CODE
+
+    # info("Warm-up epoch")
+    # f=datafiles[1]; mini = S2SData(f, f; batch=batchsize, ftype=eval(parse(ftype)), dense=dense, dict1=dict[1], dict2=dict[2], stop=3200) #DBG
+    # @date train(model, mini, softloss; gcheck=gcheck, gclip=gclip, getnorm=getnorm, getloss=getloss) #DBG: pretrain to compile for timing
+    # info("Starting profile")
