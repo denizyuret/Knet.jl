7dd47c4a97c2e27498a0e3dee9eafa3ab32503e2
Deniz Yuret <denizyuret@gmail.com>
2015-11-05 07:35:22 -0800

    Moving atomicAdd(double) to knet.h

--- src/util/linalg.cu
+++ src/util/linalg.cu
@@ -1,19 +1,5 @@
 #include "../knet.h"
 
-__device__ double atomicAdd(double* address, double val)
-{
-  unsigned long long int* address_as_ull = (unsigned long long int*)address;
-  unsigned long long int old = *address_as_ull, assumed;
-  do {
-    assumed = old;
-    old = atomicCAS(address_as_ull, assumed,
-		    __double_as_longlong(val +
-					 __longlong_as_double(assumed)));
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)} while (assumed != old);
-  } while(assumed != old); 
-  return __longlong_as_double(old);
-}
-
 template<typename dType>
 __global__ void _add_csr_dns_atomic(int m, int n, dType alpha,
 			     int nnzA,
