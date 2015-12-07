dae415c752bb72d8b9eec854dff1cb403087d6bd
Deniz Yuret <denizyuret@gmail.com>
2015-12-06 17:06:32 -0800

    lrn work-in-progress

---
+++ src/op/lrn.jl
@@ -0,0 +1,11 @@
+type LRN <: Op; n; alpha; beta; k; end
+lrn(x,y;n=5,alpha=1e-4,beta=0.75,k=2.0,o...)=(LRN(n,alpha,beta,k),x,y)
+ninputs(::LRN)=1
+overwrites(::LRN)=false
+back_reads_x(::LRN)=true
+back_reads_y(::LRN)=true
+infersize(::LRN,xdims,ydims)=infersize(Sigm(),xdims,ydims)
+forw(l::LRN, x, y; o...)=
+    (cudnnLRNCrossChannelForward(x,y; n=l.n, alpha=l.alpha, beta=l.beta, k=l.k); gpusync(); y)
+back(l::LRN, dy, dx; x=nothing, y=nothing, o...)=
+    (dx!=nothing && cudnnLRNCrossChannelBackward(y,dy,x,dx;n=l.n, alpha=l.alpha, beta=l.beta, k=l.k); gpusync(); dx)
