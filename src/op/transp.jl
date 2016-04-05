28be3a18b657e1005462876f8bca6845f0365ddb
OmerKirnap <okirnap@ku.edu.tr>
2016-04-05 20:29:21 +0300

    tranpose on cpu works

--- src/op/transp.jl
+++ src/op/transp.jl
@@ -12,36 +12,44 @@ function forw(::Transp, x, y; o...)
 end
 
 
-function back(::Transp, dx, dy; o...)
-  if dy != nothing
-    copysync!(dx, dy)
+function back(::Transp, dy, dx; o...)
+  if dx != nothing
+    transpose!(dx, dy)
   end
 end
 
 
 function infersize(::Transp, xdims, ydims)
-    if xdims==ydims==nothing
-        nothing
-    elseif xdims==nothing
-        if length(ydims) == 2
-          xdims=(ydims[2], ydims[1])
-          return (xdims, ydims)
-        else
-          error("Only 2 dimensions can be handled")
-        end
-    elseif ydims==nothing
-        if length(xdims) == 2
-          ydims=(xdims[2], xdims[1])
-          return (xdims, ydims)
-        else
-          error("Only 2 dimensions can be handled")
-        end
+  if xdims==ydims==nothing
+    nothing
+  elseif xdims==nothing
+    if length(ydims) == 2
+      xdims=(ydims[2], ydims[1])
+      return (xdims, ydims)
+    elseif length(ydims) == 1 # in case single column given
+      ydims = (ydims[1], 1)
+      xdims = (ydims[2], ydims[1])
+      return (xdims, ydims)
     else
-        @assert length(xdims) == length(ydims) == length(xdims) == 2
-        if xdims[1] == ydims[2] && xdims[2] == ydims[1]
-          return (xdims, ydims)
-        else
-          throw(DimensionMismatch())
-        end
+      error("Only 2 dimensions can be handled")
     end
+  elseif ydims==nothing
+    if length(xdims) == 2
+      ydims=(xdims[2], xdims[1])
+      return (xdims, ydims)
+    elseif length(xdims) == 1 # in case single column given
+      xdims = (xdims[1], 1)
+      ydims = (xdims[2], xdims[1])
+      return (xdims, ydims)
+    else
+      error("Only 2 dimensions can be handled")
+    end
+  else
+    @assert length(xdims) == length(ydims) == length(xdims) == 2
+    if xdims[1] == ydims[2] && xdims[2] == ydims[1]
+      return (xdims, ydims)
+    else
+      throw(DimensionMismatch())
+    end
+  end
 end
