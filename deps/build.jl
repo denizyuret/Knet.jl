06a74f61c0e661b02a089fc94590e93dd92be721
Deniz Yuret <denizyuret@gmail.com>
2016-09-07 22:10:55 -0700

    Recompile cache after Pkg.build().

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
 try success(`nvcc --version`)
     cd("../src") do
         run(`make libknet8.so`)
+        Base.compilecache("Knet")
     end
 catch
     warn("CUDA not installed, GPU support will not be available.")
