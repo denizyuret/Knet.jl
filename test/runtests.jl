41333a8c6d19fd832e5deb287eefe51936b46517
emre <ekyurdakul@ku.edu.tr>
2016-10-27 11:26:47 +0300

    Moved tests to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,3 +61,21 @@ println((:epoch,0,:loss,test(w,data)))
     w = train(w, data; epochs=1, lr=0.02)
     println((:epoch,epoch,:loss,test(w,data)))
 end
+
+
+#Tests for features I implemented
+#Unpooling
+x = KnetArray(reshape(Float32[1.0:16.0...], (4,4,1,1)))
+y = pool(x)
+y2 = KnetArray(reshape(Float32[6 6 14 14; 6 6 14 14; 8 8 16 16; 8 8 16 16], (4,4,1,1)))
+y3 = KnetArray(reshape(Float32[6 6 6 14 14 14; 6 6 6 14 14 14; 6 6 6 14 14 14; 8 8 8 16 16 16; 8 8 8 16 16 16; 8 8 8 16 16 16], (6,6,1,1)))
+
+@test_approx_eq_eps     myunpool(y)             y2      1e-3
+@test_approx_eq_eps     myunpool(y; window=3)   y3      1e-3
+
+#Deconvolution
+y = KnetArray(reshape(Float32[0 10 20 30; 20 110 170 150; 80 290 350 270; 140 370 420 270], (4,4,1,1)))
+x = KnetArray(reshape(Float32[0.0 10.0; 20.0 30.0], (2,2,1,1)))
+w = KnetArray(reshape(Float32[1.0 2.0 3.0; 4.0 5.0 6.0; 7.0 8.0 9.0], (3,3,1,1)))
+
+@test_approx_eq_eps     deconv4(w,x)            y       1e-3
