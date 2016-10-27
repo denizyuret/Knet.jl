a06054ac07321067509b6ffa0df768bfcadabeb4
emre <ekyurdakul@ku.edu.tr>
2016-10-27 10:05:03 +0300

    Implemented unpooling with avg-pool-wrt-x

--- src/unpool.jl
+++ src/unpool.jl
@@ -19,20 +19,20 @@ function updims{T,N}(x::KnetArray{T,N}; window=2)
     end
 end
 
-
-
-
-
-
-
-
-
-
 """
 Simple unpooling (=upsampling) as in:
 https://swarbrickjones.wordpress.com/2015/04/29/convolutional-autoencoders-in-pythontheanolasagne/
+Example input-output
+x (2x2)
+6   14
+8   16
+unpool (2x2 window)
+6   6   14  14
+6   6   14  14
+8   8   16  16
+8   8   16  16
 """
-function unpool{T,N}(x::Array{T,N}; window=2)
-    #y = similar(x, updims(x; window=window))
-    return repeat(x,inner=[window,window,1,1])
+function unpool(x; window=2)
+    y = similar(x,updims(x; window=window))
+    Knet.poolx(y,x,x*window^2; window=window,mode=1)#where the did window^2 come from ?
 end
