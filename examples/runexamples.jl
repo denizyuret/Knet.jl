92d8695f4d9c3771436247e65e7c141ca42a8be0
Deniz Yuret <denizyuret@gmail.com>
2016-11-10 17:01:04 +0300

    runexamples uses options that will help gradcheck.

--- examples/runexamples.jl
+++ examples/runexamples.jl
@@ -6,10 +6,10 @@ load_only = true
 for (p,f,o1,o2,o3) =
     (
      (:LinReg, "linreg.jl", "--gcheck 2", "--fast", "--fast"),
-     (:Housing, "housing.jl", "--gcheck 2", "--fast", "--fast"),
+     (:Housing, "housing.jl", "--gcheck 2 --atype KnetArray{Float64}", "--fast", "--fast"),
      (:MNIST, "mnist.jl", "--gcheck 2", "--fast", "--fast"),
      (:LeNet, "lenet.jl", "--gcheck 2", "--fast", "--fast"),
-     (:CharLM, "charlm.jl", "--gcheck 2", "--fast", "--fast"),
+     (:CharLM, "charlm.jl", "--gcheck 2 --winit 0.01", "--fast", "--fast"),
     )
     gpu() < 0 && p == :LeNet && continue
     gc()
