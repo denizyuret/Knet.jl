0fa7c60b2375439bcb660910dd259800483c75f8
Ekrem Emre Yurdakul <ekyurdakul@ku.edu.tr>
2016-11-12 20:13:07 +0300

    Unpool fixes

--- src/cuda44.jl
+++ src/cuda44.jl
@@ -419,8 +419,8 @@ function unpool{T}(x::KnetArray{T}; window=2)
     Knet.poolx(y,x,x*window^2; window=window,mode=1)#where the did window^2 come from ?
 end
 
-function unpoolx{T}(x::KnetArray{T}, y::KnetArray{T}, dy::KnetArray{T}; window=2)
-    Knet.poolx(y,x,dy*window^2; window=window,mode=1)
+function unpoolx{T}(dy::KnetArray{T}; window=2)
+    pool(dy*window^2; window=window, mode=1)
 end
 
 #Commented out section -> general case
@@ -440,5 +440,5 @@ function updims{T,N}(x::KnetArray{T,N}; window=2)
 end
 
 
-@primitive unpool(x; o...),dy,y  unpoolx(x,y,dy; o...)
-@zerograd  unpoolx(x,y,dy; o...)
+@primitive unpool(x; o...),dy,y  unpoolx(dy; o...)
+@zerograd  unpoolx(dy; o...)
