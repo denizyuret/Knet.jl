a2f288d66858688815779082892a578804b0c82c
Ekrem Emre Yurdakul <ekyurdakul@ku.edu.tr>
2016-11-13 00:13:13 +0300

    Detailed test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -65,54 +65,115 @@ end
 
 #Tests for features I implemented
 using Base.Test;
-function isapprox4{T}(a::KnetArray{T},b::KnetArray{T},c=1e-3)
-    a = Array(a)
-    b = Array(b)
-    c = reshape(ones(T,length(b))*c,size(b))
-    all(map((x,y,z)->isapprox(x,y;rtol=z), a,b,c))
+function isapprox4{T}(calculated::KnetArray{T},expected::KnetArray{T},c=1e-3)
+    calculated = Array(calculated)
+    expected = Array(expected)
+    c = reshape(ones(T,length(expected))*c,size(expected))
+    all(map((x,y,z)->isapprox(x,y;rtol=z), calculated,expected,c))
 end
 
-#Unpooling--All cases checked
-println("Testing unpooling...")
-x1 = KnetArray(reshape(Float32[6.0  14.0; 8.0  16.0], (2,2,1,1)))
+#Unpooling
+info("Testing unpooling (forward pass)...")
+x1 = KnetArray(reshape(Float32[ 6 14;
+                                8 16], (2,2,1,1)))
 x2 = KnetArray(reshape(Float32[1.0:9.0...], (3,3,1,1)))
-y12 = KnetArray(reshape(Float32[6 6 14 14; 6 6 14 14; 8 8 16 16; 8 8 16 16], (4,4,1,1)))
-y13 = KnetArray(reshape(Float32[6 6 6 14 14 14; 6 6 6 14 14 14; 6 6 6 14 14 14; 8 8 8 16 16 16; 8 8 8 16 16 16; 8 8 8 16 16 16], (6,6,1,1)))
-y22 = KnetArray(reshape(Float32[1.0  1.0  4.0  4.0  7.0  7.0;                                                                                                                                           
- 1.0  1.0  4.0  4.0  7.0  7.0;                                                                                                                                          
- 2.0  2.0  5.0  5.0  8.0  8.0;                                                                                                                                         
- 2.0  2.0  5.0  5.0  8.0  8.0;                                                                                                                                           
- 3.0  3.0  6.0  6.0  9.0  9.0;                                                                                                                                           
- 3.0  3.0  6.0  6.0  9.0  9.0], (6,6,1,1)))
-y23 = KnetArray(reshape(Float32[1.0  1.0  1.0  4.0  4.0  4.0  7.0  7.0  7.0;                                                                                                                            
- 1.0  1.0  1.0  4.0  4.0  4.0  7.0  7.0  7.0;                                                                                                                           
- 1.0  1.0  1.0  4.0  4.0  4.0  7.0  7.0  7.0;                                                                                                                           
- 2.0  2.0  2.0  5.0  5.0  5.0  8.0  8.0  8.0;                                                                                                                           
- 2.0  2.0  2.0  5.0  5.0  5.0  8.0  8.0  8.0;                                                                                                                           
- 2.0  2.0  2.0  5.0  5.0  5.0  8.0  8.0  8.0;                                                                                                                           
- 3.0  3.0  3.0  6.0  6.0  6.0  9.0  9.0  9.0;                                                                                                                           
- 3.0  3.0  3.0  6.0  6.0  6.0  9.0  9.0  9.0;                                                                                                                            
- 3.0  3.0  3.0  6.0  6.0  6.0  9.0  9.0  9.0], (9,9,1,1)))
+y12 = KnetArray(reshape(Float32[6 6 14 14;
+                                6 6 14 14;
+                                8 8 16 16;
+                                8 8 16 16], (4,4,1,1)))
+y13 = KnetArray(reshape(Float32[6 6 6 14 14 14;
+                                6 6 6 14 14 14;
+                                6 6 6 14 14 14;
+                                8 8 8 16 16 16;
+                                8 8 8 16 16 16;
+                                8 8 8 16 16 16], (6,6,1,1)))
+y22 = KnetArray(reshape(Float32[1 1 4 4 7 7;                                                                                                                                           
+                                1 1 4 4 7 7;                                                                                                                                          
+                                2 2 5 5 8 8;                                                                                                                                         
+                                2 2 5 5 8 8;                                                                                                                                           
+                                3 3 6 6 9 9;                                                                                                                                           
+                                3 3 6 6 9 9], (6,6,1,1)))
+y23 = KnetArray(reshape(Float32[1 1 1 4 4 4 7 7 7;                                                                                                                            
+                                1 1 1 4 4 4 7 7 7;                                                                                                                           
+                                1 1 1 4 4 4 7 7 7;                                                                                                                           
+                                2 2 2 5 5 5 8 8 8;                                                                                                                           
+                                2 2 2 5 5 5 8 8 8;                                                                                                                           
+                                2 2 2 5 5 5 8 8 8;                                                                                                                           
+                                3 3 3 6 6 6 9 9 9;                                                                                                                           
+                                3 3 3 6 6 6 9 9 9;                                                                                                                            
+                                3 3 3 6 6 6 9 9 9], (9,9,1,1)))
 #Even input, even and odd windows
 @test isapprox4(unpool(x1),y12)
 @test isapprox4(unpool(x1; window=3),y13)
 #Odd input, even and odd windows
 @test isapprox4(unpool(x2), y22)
 @test isapprox4(unpool(x2; window=3),y23)
+#All possible cases covered
 
 
-#Deconvolution--Check more cases ?
-println("Testing deconvolution...")
+
+
+
+info("Testing unpooling (backward pass)...")
+dy1 = KnetArray(reshape(Float32[1 1 0 0;
+                                1 1 0 0;
+                                0 0 0 0;
+                                0 0 0 0], (4,4,1,1)))
+dy2 = KnetArray(reshape(Float32[0 0 1 1;
+                                0 0 1 1;
+                                0 0 0 0;
+                                0 0 0 0], (4,4,1,1)))
+dy3 = KnetArray(reshape(Float32[0 0 0 0;
+                                0 0 0 0;
+                                1 1 0 0;
+                                1 1 0 0], (4,4,1,1)))
+dy4 = KnetArray(reshape(Float32[0 0 0 0;
+                                0 0 0 0;
+                                0 0 1 1;
+                                0 0 1 1], (4,4,1,1)))
+
+#Gradients should be pool(dy) for the following cases
+@test isapprox4(Knet.unpoolx(dy1), pool(dy1))
+@test isapprox4(Knet.unpoolx(dy2), pool(dy2))
+@test isapprox4(Knet.unpoolx(dy3), pool(dy3))
+@test isapprox4(Knet.unpoolx(dy4), pool(dy4))
+
+dyW = KnetArray(reshape(Float32[1 0 0 0;
+                                1 0 0 0;
+                                0 4 0 0;
+                                1 2 0 0], (4,4,1,1)))
+xW = KnetArray(reshape(Float32[ 0  0;
+                                0  0], (2,2,1,1)))
+
+#Gradient should be zero for the following case, since the output
+#cant't change like this for x under any condition
+@test isapprox4(Knet.unpoolx(dyW), xW)
+#All possible cases covered
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Deconvolution
+info("Testing deconvolution (forward pass)...")
 y = KnetArray(reshape(Float32[0 10 20 30; 20 110 170 150; 80 290 350 270; 140 370 420 270], (4,4,1,1)))
 x = KnetArray(reshape(Float32[0.0 10.0; 20.0 30.0], (2,2,1,1)))
 w = KnetArray(reshape(Float32[1.0 2.0 3.0; 4.0 5.0 6.0; 7.0 8.0 9.0], (3,3,1,1)))
 
 @test isapprox4(deconv4(w,x),y)
 
+info("Testing deconvolution (backward pass)...")
+
 #=
-#Float16
-println("Testing Float16...")
-println("Testing w*x")
+info("Testing Float16...")
 
 #GPU Float64
 w = KnetArray(reshape(Float64[1.0 2.0 3.0 4.0], (2,2)))
@@ -132,7 +193,3 @@ x = KnetArray(reshape(Float16[1.0 1.0 1.0 1.0], (2,2)))
 y = KnetArray(Float16[4.0 4.0; 6.0 6.0])
 @test isapprox4(w*x, y)
 =#
-
-#TODO
-#Float16 conv to do
-#Add unpooling and deconv backwards pass tests
