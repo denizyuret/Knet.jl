fd0be3b195dfe072ebf84a114a66b07c66fdc5a3
Ekrem Emre Yurdakul <ekrememreyurdakul@gmail.com>
2016-11-19 06:03:40 +0300

    cuda11 cuda code generalized

--- src/cuda11gen.jl
+++ src/cuda11gen.jl
@@ -1,36 +1,27 @@
 using Knet: cuda11
 
 function cuda11src(f, j=f, ex="$f(xi,yi)"; BLK=256, THR=256)
+  sprint() do s
+    for (T,F) in [("float","$(f)_32"),("double","$(f)_64")]
+        print(s,
 """
-__global__ void _$(f)_32_11(int n, float *x, float *y, float *z) {
+__global__ void _$(F)_11(int n, $T *x, $T *y, $T *z) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   while (i < n) {
-    float xi=x[i];
-    float yi=y[i];
+    $T xi=x[i];
+    $T yi=y[i];
     z[i] = $ex;
     i += blockDim.x * gridDim.x;
   }
 }
 extern "C" {
-  void $(f)_32_11(int n, float *x, float *y, float *z) {
-    _$(f)_32_11<<<$BLK,$THR>>>(n,x,y,z);
+  void $(F)_11(int n, $T *x, $T *y, $T *z) {
+    _$(F)_11<<<$BLK,$THR>>>(n,x,y,z);
   }    
 }
-__global__ void _$(f)_64_11(int n, double *x, double *y, double *z) {
-  int i = threadIdx.x + blockIdx.x * blockDim.x;
-  while (i < n) {
-    double xi=x[i];
-    double yi=y[i];
-    z[i] = $ex;
-    i += blockDim.x * gridDim.x;
-  }
-}
-extern "C" {
-  void $(f)_64_11(int n, double *x, double *y, double *z) {
-    _$(f)_64_11<<<$BLK,$THR>>>(n,x,y,z);
-  }    
-}
-"""
+""")
+    end
+  end
 end
 
 for a in cuda11
