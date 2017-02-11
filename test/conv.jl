c1a336802ddfbe2fdb5b08992044fe2f907796e7
Deniz Yuret <denizyuret@gmail.com>
2017-02-11 14:22:01 +0300

    conv test wip

--- test/conv.jl
+++ test/conv.jl
@@ -1,15 +1,24 @@
 using Base.Test, Knet
 
+conv41(a;o...)=conv4(a[1],a[2];o...)
+deconv41(a;o...)=deconv4(a[1],a[2];o...)
+
 @testset "conv" begin
-    # conv4
-    # pool
-    # unpool
-    # deconv4
-    # mat -> linalg
-    # permutedims -> linalg
-    # cpu comparisons
-    # gradcheck
-    # Float32,64
+    for T in (Float32,Float64)
+        for D in (4,5)
+            xdims = ((D+1):-1:2...)
+            x = rand!(KnetArray(T,xdims))
+            @test gradcheck(pool, x)
+            @test_broken gradcheck(unpool, x)
+            wdims = ntuple(i->3,D)
+            w = rand!(KnetArray(T,wdims))
+            @test gradcheck(conv41, (w,x))
+            # @test gradcheck(deconv41, (w,x))
+        end
+    end
+    # TODO: other keyword args
+    # TODO: implement cpu versions and compare
+    # TODO: any way to compare conv4 vs deconv4 or pool vs unpool?
 end
 
 nothing
