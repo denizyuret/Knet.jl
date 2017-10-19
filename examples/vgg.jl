10953a5ff6cebabf11aa79a4f383a2d8cbefc779
Deniz Yuret <denizyuret@gmail.com>
2017-10-19 17:30:52 +0300

    julia4 compat fix for vgg.jl

--- examples/vgg.jl
+++ examples/vgg.jl
@@ -72,7 +72,11 @@ function data(img, averageImage)
     i1 = div(size(a1,1)-224,2)
     j1 = div(size(a1,2)-224,2)
     b1 = a1[i1+1:i1+224,j1+1:j1+224]
-    c1 = permutedims(channelview(b1), (3,2,1))
+    if VERSION >= v"0.5.0"
+        c1 = permutedims(channelview(b1), (3,2,1))
+    else
+        c1 = separate(b1)
+    end
     d1 = convert(Array{Float32}, c1)
     e1 = reshape(d1[:,:,1:3], (224,224,3,1))
     f1 = (255 * e1 .- averageImage)
