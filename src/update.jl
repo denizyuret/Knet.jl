95e1cf87e7289ae8dbfd7af31935345366d5da83
CarloLucibello <carlo.lucibello@gmail.com>
2018-05-17 09:49:27 +0200

    optimisers for abstract array

--- src/update.jl
+++ src/update.jl
@@ -499,9 +499,9 @@ that parallel model parameters easy when all of them use the same type
 and options.
 
 """
-optimizers{T<:Number}(::KnetArray{T},otype; o...)=otype(;o...)
-optimizers{T<:Number}(::Array{T},otype; o...)=otype(;o...)
-optimizers(a::Associative,otype; o...)=Dict([ k=>optimizers(v,otype;o...) for (k,v) in a ])
-optimizers(a::Tuple,otype; o...)=map(x->optimizers(x,otype;o...), a)
-optimizers(a::Array,otype; o...)=map(x->optimizers(x,otype;o...), a)
-optimizers(a,otype;o...)=nothing
+optimizers(::KnetArray{<:Number}, otype; o...) = otype(; o...)
+optimizers(::AbstractArray{<:Number}, otype; o...) = otype(; o...)
+optimizers(a::Associative, otype; o...)=Dict([k => optimizers(v,otype;o...) for (k,v) in a])
+optimizers(a::Tuple,otype; o...) = map(x -> optimizers(x,otype;o...), a)
+optimizers(a::AbstractArray, otype; o...) = map(x->optimizers(x,otype;o...), a)
+optimizers(a, otype; o...) = nothing
