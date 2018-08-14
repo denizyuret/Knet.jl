cf0bb3c1aff2c9e7bde1aada62f377d6149701a6
autodocs <autodocs>
2018-08-14 12:23:35 +0000

    build based on 4aa5f92

--- stable/assets/search.js
+++ stable/assets/search.js
@@ -39,7 +39,7 @@ parseUri.options = {
 requirejs.config({
     paths: {
         'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min',
-        'lunr': 'https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.1.3/lunr.min',
+        'lunr': 'https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.3.1/lunr.min',
         'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min',
     }
 });
@@ -198,6 +198,13 @@ require(["jquery", "lunr", "lodash"], function($, lunr, _) {
                 tokens.forEach(function (t) {
                     q.term(t.toString(), {
                         fields: ["title"],
+                        boost: 100,
+                        usePipeline: false,
+                        editDistance: 0,
+                        wildcard: lunr.Query.wildcard.NONE
+                    })
+                    q.term(t.toString(), {
+                        fields: ["title"],
                         boost: 10,
                         usePipeline: false,
                         editDistance: 2,
@@ -207,7 +214,7 @@ require(["jquery", "lunr", "lodash"], function($, lunr, _) {
                         fields: ["text"],
                         boost: 1,
                         usePipeline: true,
-                        editDistance: 2,
+                        editDistance: 0,
                         wildcard: lunr.Query.wildcard.NONE
                     })
                 })
