4e8819fe6648ff1fab1e5fbc2bacef21055f9867
autodocs <autodocs>
2018-08-31 15:28:09 +0000

    build based on 4342175

---
+++ release-1.0/siteinfo.js
@@ -0,0 +1,1 @@
+var DOCUMENTER_CURRENT_VERSION = "release-1.0";
--- versions.js
+++ versions.js
@@ -1,8 +1,10 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-1.0",
   "release-0.9",
   "release-0.8",
+  "v1.0.1",
   "v0.9.2",
   "v0.9.1",
   "v0.9.0",
