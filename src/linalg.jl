e9228a18e4bb43647cf087be5dbf412193d13ba8
Deniz Yuret <denizyuret@gmail.com>
2018-09-17 01:58:24 -0400

    Added @gs after ccall in linalg.

--- src/linalg.jl
+++ src/linalg.jl
@@ -171,7 +171,7 @@ function permutedims(x::KnetArray{T,N}, dims) where {T,N}
             y = similar(x, size(x,dims[1]), size(x,dims[2]))
             @eval ccall(($funcName,libknet8),Nothing,(Ptr{$T},Cint,Cint,Ptr{$T},Cint),
                         $x,size($x,1),size($x,2),$y,size($y,1))
-            return y
+            @gs; return y
             =#
             # Using CUBLAS
             return _transpose(x)
@@ -186,7 +186,7 @@ function permutedims(x::KnetArray{T,N}, dims) where {T,N}
             y = similar(x, size(x,dims[1]), size(x,dims[2]), size(x,dims[3]))
             @eval ccall(($funcName,libknet8),Nothing,(Ptr{$T},Cint,Cint,Cint,Ptr{$T},Cint,Cint),
                         $x,size($x,1),size($x,2),size($x,3),$y,size($y,1),size($y,2))
-            return y
+            @gs; return y
         end
     elseif N == 4
         if dims[1]==1 && dims[2]==2 && dims[3]==3 && dims[4]==4
@@ -196,7 +196,7 @@ function permutedims(x::KnetArray{T,N}, dims) where {T,N}
             y = similar(x, size(x,dims[1]), size(x,dims[2]), size(x,dims[3]), size(x,dims[4]))
             @eval ccall(($funcName,libknet8),Nothing,(Ptr{$T},Cint,Cint,Cint,Cint,Ptr{$T},Cint,Cint,Cint),
                         $x,size($x,1),size($x,2),size($x,3),size($x,4),$y,size($y,1),size($y,2),size($y,3))
-            return y
+            @gs; return y
         end
     elseif N == 5
         if dims[1]==1 && dims[2]==2 && dims[3]==3 && dims[4]==4 && dims[5]==5
@@ -206,7 +206,7 @@ function permutedims(x::KnetArray{T,N}, dims) where {T,N}
             y = similar(x, size(x,dims[1]), size(x,dims[2]), size(x,dims[3]), size(x,dims[4]), size(x,dims[5]))
             @eval ccall(($funcName,libknet8),Nothing,(Ptr{$T},Cint,Cint,Cint,Cint,Cint,Ptr{$T},Cint,Cint,Cint,Cint),
                         $x,size($x,1),size($x,2),size($x,3),size($x,4),size($x,5),$y,size($y,1),size($y,2),size($y,3),size($y,4))
-            return y
+            @gs; return y
         end
     else
         error("Unsupported number of dimensions")
@@ -273,6 +273,7 @@ for (gemm, elty) in ((:dgemm_,:Float64), (:sgemm_,:Float32))
                   alpha, A, lda,
                   B, ldb, 
                   beta, C, ldc)
+            @gs
         end
     end
 end
