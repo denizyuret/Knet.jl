024a5e1953a8251b8eeec173f193bb5c1a594c3b
ekinaykurek <akyurekekin@gmail.com>
2018-09-24 22:25:00 +0300

    modes as Val types

--- src/serialize.jl
+++ src/serialize.jl
@@ -1,72 +1,57 @@
 struct RnnJLD; inputSize; hiddenSize; numLayers; dropout; inputMode; direction; mode; algo; dataType; w; end
 struct KnetJLD; a::Array ; end
-struct ParamJLD; value; end
+struct ParamJLD; value; opt; ParamJLD(x) = new(x); end
 
-mode = true
-serialize(x) = serialize_internal(x, IdDict{Any,Any}(mode=>0))
-gpu(x)       = serialize_internal(x, IdDict{Any,Any}(mode=>1))
-cpu(x)       = serialize_internal(x, IdDict{Any,Any}(mode=>2))
+const JLDMODE=Val(0)
+const GPUMODE=Val(1)
+const CPUMODE=Val(2)
 
+serialize(x) = serialize_internal(x,IdDict(),JLDMODE)
+gpu(x)       = serialize_internal(x,IdDict(),GPUMODE)
+cpu(x)       = serialize_internal(x,IdDict(),CPUMODE)
 
-@inline function serialize_internal(x::KnetArray,stackdict::IdDict)   
-    if stackdict[mode]==0
-        KnetJLD(Array(x))
-    elseif stackdict[mode]==1
-        x
-    else
-        Array(x)
-    end
-end
+serialize_internal(x::KnetArray,stackdict::IdDict,::typeofa(JLDMODE)) = KnetJLD(Array(x))
+serialize_internal(x::KnetArray,stackdict::IdDict,::typeof(GPUMODE))=x
+serialize_internal(x::KnetArray,stackdict::IdDict,::typeof(CPUMODE))=Array(x)
+serialize_internal(x::Array,stackdict::IdDict,::typeof(GPUMODE))=KnetArray(x)
+serialize_internal(x::Array,stackdict::IdDict,::typeof(CPUMODE))=x
 
-serialize_internal(d::KnetJLD,stackdict::IdDict)        = (gpu() >= 0 ? KnetArray(d.a) : d.a)
+serialize_internal(d::KnetJLD,stackdict::IdDict,::typeof(JLDMODE))  =(gpu() >= 0 ? KnetArray(d.a) : d.a)
 
-@inline function serialize_internal(x::Param,stackdict::IdDict)          
-    if stackdict[mode]==0
-        ParamJLD(Array(x))
-    elseif stackdict[mode]==1
-        if typeof(x.value) <:Array
-            isdefined(x,:opt) ? Param(KnetArray(x.value),KnetArray(x.opt)) : Param(KnetArray(x.value))
-        else
-            x
-        end
-    else
-        if typeof(x.value) <: KnetArray
-            isdefined(x,:opt) ? Param(Array(x.value),Array(x.opt)) : Param(Array(x.value))
-        else
-            x
-        end
-    end
-end
-serialize_internal(x::ParamJLD,stackdict::IdDict) = Param(serialize_internal(x.value,stackdict))
+serialize_internal(x::Param{<:KnetArray},stackdict::IdDict,::typeof(JLDMODE))=isdefined(x,:opt) ? ParamJLD(serialize_internal(x.value,stackdict,JLDMODE),serialize_internal(x.opt,stackdict,JLDMODE)) : ParamJLD(serialize_internal(x.value,stackdict,JLDMODE))        
 
-function serialize_internal(x::RNN,stackdict::IdDict)       
-    if stackdict[mode]==0
-          RnnJLD(x.inputSize, x.hiddenSize, x.numLayers, x.dropout, x.inputMode, x.direction, x.mode, x.algo, x.dataType, serialize_internal(x.w,stackdict))
-    else
-        x.w = serialize_internal(x.w,stackdict)
-        return x
-    end
-end
+serialize_internal(x::Param{<:KnetArray},stackdict::IdDict,::typeof(CPUMODE))=isdefined(x,:opt) ? Param(serialize_internal(x.value,stackd\
+ict,JLDMODE),serialize_internal(x.opt,stackdict,JLDMODE)) : Param(serialize_internal(x.value,stackdict,CPUMODE))
 
-serialize_internal(r::RnnJLD,stackdict::IdDict)         = ((x,w) = rnninit(r.inputSize, r.hiddenSize, numLayers=r.numLayers, dropout=r.dropout,
+erialize_internal(x::Param{<:Array},stackdict::IdDict,::typeof(GPUMODE))=isdefined(x,:opt) ? Param(serialize_internal(x.value,stackd\
+ict,GPUMODE),serialize_internal(x.opt,stackdict,GPUMODE)) : Param(serialize_internal(x.value,stackdict,GPUMODE))
+
+serialize_internal(x::ParamJLD,stackdict::IdDict,::typeof(JLDMODE))=isdefined(x,:opt) ? Param(serialize_internal(x.value,stackdict,JLDMODE),serialize_internal(x.value,stackdict,JLDMODE)) : Param(serialize_internal(x.value,stackdict,JLDMODE))
+
+serialize_internal(x::RNN,stackdict::IdDict,::typeof(JLDMODE)) = RnnJLD(x.inputSize, x.hiddenSize, x.numLayers, x.dropout, x.inputMode, x.direction, x.mode, x.algo, x.dataType, serialize_internal(x.w,stackdict,JLDMODE))        
+
+serialize_internal(x::RNN,stackdict::IdDict,mode::Val) = (x.w = serialize_internal(x.w,stackdict,mode); return x)
+
+serialize_internal(r::RnnJLD,stackdict::IdDict,::typeof(JLDMODE))         = ((x,w) = rnninit(r.inputSize, r.hiddenSize, numLayers=r.numLayers, dropout=r.dropout,
                                                                            skipInput=(r.inputMode==1), bidirectional=(r.direction==1),
-                                                                           rnnType=(:relu,:tanh,:lstm,:gru)[1+r.mode], algo=r.algo, dataType=r.dataType); x.w = serialize_internal(r.w,stackdict); x)
-serialize_internal(x::Union{Symbol,Core.MethodInstance,Method,GlobalRef,DataType,Union,Task},
-                  stackdict::IdDict) = x
-serialize_internal(x::Tuple, stackdict::IdDict) =
-    ntuple(i->serialize_internal(x[i], stackdict), length(x))
-serialize_internal(x::Module, stackdict::IdDict) = error("serialize of Modules not supported")
+                                                                           rnnType=(:relu,:tanh,:lstm,:gru)[1+r.mode], algo=r.algo, dataType=r.dataType); x.w = serialize_internal(r.w,stackdict,JLDMODE); x)
 
-function serialize_internal(x::Core.SimpleVector, stackdict::IdDict)
+serialize_internal(x::Union{Symbol,Core.MethodInstance,Method,GlobalRef,DataType,Union,Task},
+                  stackdict::IdDict,::Val) = x
+serialize_internal(x::Tuple, stackdict::IdDict,mode::Val) =
+    ntuple(i->serialize_internal(x[i], stackdict, mode), length(x))
+serialize_internal(x::Module, stackdict::IdDict,::Val) = error("serialize of Modules not supported")
+
+function serialize_internal(x::Core.SimpleVector, stackdict::IdDict,mode::Val)
     if haskey(stackdict, x)
         return stackdict[x]
     end
-    y = Core.svec(Any[serialize_internal(x[i], stackdict) for i = 1:length(x)]...)
+    y = Core.svec(Any[serialize_internal(x[i], stackdict, mode) for i = 1:length(x)]...)
     stackdict[x] = y
     return y
 end
 
-function serialize_internal(x::String, stackdict::IdDict)
+function serialize_internal(x::String, stackdict::IdDict,mode::Val)
     if haskey(stackdict, x)
         return stackdict[x]
     end
@@ -75,7 +60,7 @@ function serialize_internal(x::String, stackdict::IdDict)
     return y
 end
 
-function serialize_internal(@nospecialize(x), stackdict::IdDict)
+function serialize_internal(@nospecialize(x), stackdict::IdDict,mode::Val)
     T = typeof(x)::DataType
     nf = nfields(x)
     (isbitstype(T) || nf == 0) && return x
@@ -89,29 +74,29 @@ function serialize_internal(@nospecialize(x), stackdict::IdDict)
     for i in 1:nf
         if isdefined(x,i)
             ccall(:jl_set_nth_field, Cvoid, (Any, Csize_t, Any), y, i-1,
-                  serialize_internal(getfield(x,i), stackdict))
+                  serialize_internal(getfield(x,i), stackdict,mode))
         end
     end
     return y::T
 end
 
-function serialize_internal(x::Array, stackdict::IdDict)
+function serialize_internal(x::Array, stackdict::IdDict,mode::Val)
     if haskey(stackdict, x)
         return stackdict[x]
     end
-    _serialize_array_t(x, eltype(x), stackdict)
+    _serialize_array_t(x, eltype(x), stackdict,mode)
 end
 
-function _serialize_array_t(@nospecialize(x), T, stackdict::IdDict)
+function _serialize_array_t(@nospecialize(x), T, stackdict::IdDict,mode::Val)
     if isbitstype(T)
         return (stackdict[x]=x)
     end
-    y = map(xi->serialize_internal(xi,stackdict), x)
+    y = map(xi->serialize_internal(xi,stackdict,mode), x)
     stackdict[x] = y
     return y
 end
 
-function serialize_internal(x::Union{Dict,IdDict}, stackdict::IdDict)
+function serialize_internal(x::Union{Dict,IdDict}, stackdict::IdDict,mode::Val)
     if haskey(stackdict, x)
         return stackdict[x]
     end
@@ -123,7 +108,7 @@ function serialize_internal(x::Union{Dict,IdDict}, stackdict::IdDict)
     dest = typeof(x) <: Dict ? Dict() : IdDict()
     stackdict[x] = dest
     for (k, v) in x
-        dest[serialize_internal(k, stackdict)] = serialize_internal(v, stackdict)
+        dest[serialize_internal(k, stackdict,mode)] = serialize_internal(v, stackdict,mode)
     end
     dest
 end
