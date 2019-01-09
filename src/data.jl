a95498adfe6c378172e88e394a7296ee7dc10619
Deniz Yuret <denizyuret@gmail.com>
2019-01-09 09:38:26 -0500

    Minor improvements in data.jl.

--- src/data.jl
+++ src/data.jl
@@ -3,16 +3,24 @@ import Base: length, size, iterate, eltype, IteratorSize, IteratorEltype, haslen
 import .Iterators: cycle, Cycle
 
 "Minibatched data"
-mutable struct Data; x; y; batchsize; length; partial; indices; shuffle; xsize; ysize; xtype; ytype; end
+mutable struct Data; x; y; batchsize; length; partial; imax; indices; shuffle; xsize; ysize; xtype; ytype; end
 
 """
     minibatch(x, [y], batchsize; shuffle, partial, xtype, ytype, xsize, ysize)
 
-Return an iterable of minibatches [(xi,yi)...] given data tensors x, y
-and batchsize.  The last dimension of x and y should match and give
-the number of instances. `y` is optional.  Keyword arguments:
+Return an iterator of minibatches [(xi,yi)...] given data tensors x, y and batchsize.  
 
-- `shuffle=false`: Shuffle the instances before minibatching.
+The last dimension of x and y give the number of instances and should be equal. `y` is
+optional, if omitted a sequence of `xi` will be generated rather than `(xi,yi)` tuples.  Use
+`repeat(d,n)` for multiple epochs, `Iterators.take(d,n)` for a partial epoch, and
+`Iterators.cycle(d)` to cycle through the data forever (this can be used with `converge`).
+If you need the iterator to continue from its last position when stopped early (e.g. by a
+break in a for loop), use `Iterators.Stateful(d)` (by default the iterator would restart
+from the beginning).
+
+Keyword arguments:
+
+- `shuffle=false`: Shuffle the instances every epoch.
 - `partial=false`: If true include the last partial minibatch < batchsize.
 - `xtype=typeof(x)`: Convert xi in minibatches to this type.
 - `ytype=typeof(y)`: Convert yi in minibatches to this type.
@@ -25,27 +33,33 @@ function minibatch(x,y,batchsize; shuffle=false,partial=false,xtype=typeof(x),yt
     x2 = reshape(x, :, nx)
     y2 = reshape(y, :, nx)
     # indices = shuffle ? randperm(nx) : 1:nx --> do this at the beginning of for loop
-    Data(x2,y2,batchsize,nx,partial,1:nx,shuffle,xsize,ysize,xtype,ytype)
+    imax = partial ? nx : nx - batchsize + 1
+    Data(x2,y2,batchsize,nx,partial,imax,1:nx,shuffle,xsize,ysize,xtype,ytype)
 end
 
 function minibatch(x,batchsize; shuffle=false,partial=false,xtype=typeof(x),xsize=size(x))
     nx = size(x)[end]
     x2 = reshape(x, :, nx)
     # indices = shuffle ? randperm(nx) : 1:nx --> do this at the beginning of for loop
-    Data(x2,nothing,batchsize,nx,partial,1:nx,shuffle,xsize,nothing,xtype,nothing)
+    imax = partial ? nx : nx - batchsize + 1
+    Data(x2,nothing,batchsize,nx,partial,imax,1:nx,shuffle,xsize,nothing,xtype,nothing)
 end
 
-@propagate_inbounds function iterate(d::Data,i=0)     # returns data in d.indices[i+1:i+batchsize]
-    if i == 0 && d.shuffle; d.indices = randperm(d.length); end
-    if i >= d.length || (!d.partial && i + d.batchsize > d.length); return nothing; end
-    j=min(i+d.batchsize, d.length)
-    ids = d.indices[i+1:j]
+@propagate_inbounds function iterate(d::Data, i=0)     # returns data in d.indices[i+1:i+batchsize]
+    if i >= d.imax
+        return nothing
+    end
+    if d.shuffle && i == 0
+        d.indices = randperm(d.length)
+    end
+    nexti = min(i + d.batchsize, d.length)
+    ids = d.indices[i+1:nexti]
     xbatch = convert(d.xtype, reshape(d.x[:,ids],d.xsize[1:end-1]...,length(ids)))
     if d.y == nothing
-        return (xbatch,j)
+        return (xbatch,nexti)
     else
         ybatch = convert(d.ytype, reshape(d.y[:,ids],d.ysize[1:end-1]...,length(ids)))
-        return ((xbatch,ybatch),j)
+        return ((xbatch,ybatch),nexti)
     end
 end
 
@@ -60,10 +74,10 @@ end
 
 function rand(d::Data)
     i = rand(0:(d.length-d.batchsize))
-    return next(d, i)[1]
+    return iterate(d, i)[1]
 end
 
-# Use repeat(data,n) for multiple epochs:
+# Use repeat(data,n) for multiple epochs; cycle(data) to go forever, take(data,n) for partial epochs
 
 struct Repeat; data::Data; n::Int; end
 
