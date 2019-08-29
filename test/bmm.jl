84b845bdad6fad79a2950989cf89aa5fdceb2eda
Deniz Yuret <denizyuret@gmail.com>
2019-08-29 03:53:31 +0300

    gitlab debug 3

--- test/bmm.jl
+++ test/bmm.jl
@@ -3,7 +3,7 @@ using Statistics, LinearAlgebra
 
 sizes = [((2,4,3),(4,1,3)),((2,4,5),(4,8,5)),((2,8,4),(8,2,4))]
 @testset "bmm" begin
-    for t in (Float32,) # Float64)
+    for t in (Float32, Float64)
         for s in sizes
             a = t(0.1)*randn(t, s[1]...)
             b = t(0.1)*randn(t, s[2]...)
