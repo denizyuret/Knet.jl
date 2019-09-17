a613cba3e13c27ea58c2fc44f60df52727c8440e
Deniz Yuret <denizyuret@gmail.com>
2019-09-17 23:19:59 +0300

    Avoid using something in statistics.jl

--- src/statistics.jl
+++ src/statistics.jl
@@ -4,10 +4,11 @@ mean(a::KnetArray; o...) = (b = sum(a; o...); b .* convert(eltype(b),(length(b)/
 mean(f, a::KnetArray) = sum(f, a) / length(a)
 std(x::KnetArray, args...; kws...) = sqrt.(var(x, args...; kws...))
 stdm(x::KnetArray, args...; kws...) = sqrt.(varm(x, args...; kws...))
-var(x::KnetArray; corrected::Bool=true, mean=nothing, dims=:)=_varm(x, something(mean, Statistics.mean(x,dims=dims)); corrected=corrected, dims=dims)
+var(x::KnetArray; corrected::Bool=true, mean=nothing, dims=:)=_varm(x, mean; corrected=corrected, dims=dims)
 varm(x::KnetArray, m; corrected::Bool=true, dims=:)=_varm(x, m; corrected=corrected, dims=dims)
 
 function _varm(x, m; corrected::Bool=true, dims=:)
+    if m === nothing; m = mean(x,dims=dims); end
     s = sum(abs2, x .- m; dims=dims)
     r = length(x) ÷ length(s) - Int(corrected)
     s ./ r
