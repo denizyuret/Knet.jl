a61adc67c4258b398eef5fcb2e98ee326b2ed6f3
ekinaykurek <akyurekekin@gmail.com>
2019-09-28 01:15:30 +0300

    update bmm transpose test

--- test/bmm.jl
+++ test/bmm.jl
@@ -22,14 +22,15 @@ sizes = [((2,4,3),(4,1,3)),((2,4,5),(4,8,5)),((2,8,4,3),(8,2,4,3))]
     # Issue #495: transpose support
     using Knet: bmm!
     A = KnetArray(rand(3,4,10))
-    At = KnetArray(rand(4,3,10))
+    At = permutedims(A,(2,1,3))
     B = KnetArray(rand(4,5,10))
-    Bt = KnetArray(rand(5,4,10))
-    C = KnetArray(rand(3,5,10))
-    @test bmm!('N','N',1.0,A,B,0.0,C) === C
-    @test bmm!('N','T',1.0,A,Bt,0.0,C) === C
-    @test bmm!('T','N',1.0,At,B,0.0,C) === C
-    @test bmm!('T','T',1.0,At,Bt,0.0,C) === C
+    Bt = permutedims(B,(2,1,3))
+    C = KnetArray(zeros(3,5,10))
+    ϵ = 1e-9
+    C1 = bmm!('N','N',1.0,A,B,0.0,copy(C))
+    @test mean(abs,bmm!('N','T',1.0,A,Bt,0.0,copy(C))-C1)  <  1e-9
+    @test mean(abs,bmm!('T','N',1.0,At,B,0.0,copy(C))-C1)  <  1e-9
+    @test mean(abs,bmm!('T','T',1.0,At,Bt,0.0,copy(C))-C1) <  1e-9
 end #end of testset
 
 # suppress the return
