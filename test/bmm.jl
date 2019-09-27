ac77cd6fddc939784648c5e6e887a05c2bd91571
Deniz Yuret <denizyuret@gmail.com>
2019-09-27 08:40:23 +0300

    added tests for transposed bmm!

--- test/bmm.jl
+++ test/bmm.jl
@@ -19,6 +19,17 @@ sizes = [((2,4,3),(4,1,3)),((2,4,5),(4,8,5)),((2,8,4,3),(8,2,4,3))]
             end
         end
     end
+    # Issue #495: transpose support
+    using Knet: bmm!
+    A = KnetArray(rand(3,4,10))
+    At = KnetArray(rand(4,3,10))
+    B = KnetArray(rand(4,5,10))
+    Bt = KnetArray(rand(5,4,10))
+    C = KnetArray(rand(3,5,10))
+    @test bmm!('N','N',1.0,A,B,0.0,C) === C
+    @test bmm!('N','T',1.0,A,Bt,0.0,C) === C
+    @test bmm!('T','N',1.0,At,B,0.0,C) === C
+    @test bmm!('T','T',1.0,At,Bt,0.0,C) === C
 end #end of testset
 
 # suppress the return
