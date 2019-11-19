61538d144b3c617900ae6a64682228a3cfe6149e
Alexander Barth <barth.alexander@gmail.com>
2019-11-19 19:56:43 +0100

    alernative implementation of xavier distribution

--- test/distributions.jl
+++ test/distributions.jl
@@ -3,6 +3,7 @@ include("header.jl")
 @testset "distributions" begin
     @test isa(gaussian(10),Array)
     @test isa(xavier(10,10),Array)
+    @test isa(xavier_uniform(10,10),Array)
     @test isa(bilinear(Float32,2,2,128,128),Array)
 end
 
