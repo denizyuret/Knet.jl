f7b9a302d9d27e1918b1a481177337b507346c92
Deniz Yuret <denizyuret@gmail.com>
2020-09-04 19:41:01 +0300

    multiheadattn wip

--- src/cudnn/multiheadattn.jl
+++ src/cudnn/multiheadattn.jl
@@ -1,7 +1,7 @@
-import Base: unsafe_convert
 using Knet.KnetArrays: DevArray
-using AutoGrad: AutoGrad, @primitive1, recording
-using CUDA: CU_NULL
+# @primitive1 is too inefficient for functions with many args, we will define gradients manually:
+using AutoGrad: forw, Value, Arg, recording
+import AutoGrad: back
 AutoGrad.@zerograd sizeof(x)  # TODO: remove after fixing in AutoGrad
 
 using CUDA.CUDNN: 
@@ -164,13 +164,13 @@ function cudnnMultiHeadAttnForward(
     @assert sizeof(workSpace) >= wsSize  "worksSpace should be at least $wsSize bytes"
     @assert sizeof(reserveSpace) >= rsSize  "reserveSpace should be at least $rsSize bytes"
 
-    _cudnnMultiHeadAttnForward(
-        weights, queries, keys, values, residuals;
-        dweights, dqueries, dkeys, dvalues, # dresiduals is equal to dout
-        attnDesc, currIdx, loWinIdx, hiWinIdx,
-        devSeqLengthsQO, devSeqLengthsKV,
-        qDesc, kDesc, vDesc, oDesc,
-        out, workSpace, reserveSpace)
+    forw(_cudnnMultiHeadAttnForward,
+         weights, queries, keys, values, residuals;
+         dweights, dqueries, dkeys, dvalues, # dresiduals is equal to dout
+         attnDesc, currIdx, loWinIdx, hiWinIdx,
+         devSeqLengthsQO, devSeqLengthsKV,
+         qDesc, kDesc, vDesc, oDesc,
+         out, workSpace, reserveSpace)
 end
 
 
@@ -198,46 +198,50 @@ function _cudnnMultiHeadAttnForward(
 end
 
 
-@primitive1(
+# We do all the work during the backward pass for the first arg
+function back(
+    ::typeof(_cudnnMultiHeadAttnForward), ::Type{Arg{1}}, dout, _out, 
+    weights, queries, keys, values, residuals;
+    dweights, dqueries, dkeys, dvalues,
+    attnDesc, currIdx, loWinIdx, hiWinIdx,
+    devSeqLengthsQO, devSeqLengthsKV,
+    qDesc, kDesc, vDesc, oDesc,
+    out, workSpace, reserveSpace)
 
-    (_cudnnMultiHeadAttnForward(
-        weights, queries, keys, values, residuals;
-        dweights, dqueries, dkeys, dvalues,
-        attnDesc, currIdx, loWinIdx, hiWinIdx,
-        devSeqLengthsQO, devSeqLengthsKV,
-        qDesc, kDesc, vDesc, oDesc,
-        out, workSpace, reserveSpace),
-     dout),
+    (weights, queries, keys, values, residuals) = value.((weights, queries, keys, values, residuals))
 
-    (cudnnMultiHeadAttnBackwardData(
+    cudnnMultiHeadAttnBackwardData(
         handle(), attnDesc,
         loWinIdx, hiWinIdx,
         devSeqLengthsQO, devSeqLengthsKV,
         oDesc, dout,
-        qDesc, dqueries, value(queries),
-        kDesc, dkeys, value(keys),
-        vDesc, dvalues, value(values),
-        sizeof(weights), cu_null(value(weights)),
+        qDesc, dqueries, queries,
+        kDesc, dkeys, keys,
+        vDesc, dvalues, values,
+        sizeof(weights), cu_null(weights),
         sizeof(workSpace), cu_null(workSpace),
-        sizeof(reserveSpace), cu_null(reserveSpace));
+        sizeof(reserveSpace), cu_null(reserveSpace))
 
      weights !== nothing && cudnnMultiHeadAttnBackwardWeights(
          handle(), attnDesc,
          CUDNN_WGRAD_MODE_SET,
-         qDesc, value(queries),
-         kDesc, value(keys),
-         vDesc, value(values),
+         qDesc, queries,
+         kDesc, keys,
+         vDesc, values,
          oDesc, dout,
-         sizeof(weights), cu_null(value(weights)), cu_null(dweights),
+         sizeof(weights), cu_null(weights), cu_null(dweights),
          sizeof(workSpace), cu_null(workSpace),
-         sizeof(reserveSpace), cu_null(reserveSpace));
+         sizeof(reserveSpace), cu_null(reserveSpace))
 
-     dweights),
-    dqueries,
-    dkeys,
-    dvalues,
+    return dweights
+end
+
+# The backward pass for the other args only return already computed gradients
+back(::typeof(_cudnnMultiHeadAttnForward), ::Type{Arg{2}}, x...; dqueries, o...) = dqueries
+back(::typeof(_cudnnMultiHeadAttnForward), ::Type{Arg{3}}, x...; dkeys, o...) = dkeys
+back(::typeof(_cudnnMultiHeadAttnForward), ::Type{Arg{4}}, x...; dvalues, o...) = dvalues
+back(::typeof(_cudnnMultiHeadAttnForward), ::Type{Arg{5}}, dout, _out, weights, queries, keys, values, residuals; o...) =
     (residuals === nothing ? nothing : dout)
-)
 
 
 # Residuals: The cudnnMultiHeadAttnBackwardData() function does not output partial
