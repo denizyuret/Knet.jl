db3f2db0538c9bb6e0e408cfc6ef7ba46153625e
Deniz Yuret <denizyuret@gmail.com>
2020-10-22 10:20:10 +0300

    fix #622: replaced array_type with atype

--- src/train20/train_ka.jl
+++ src/train20/train_ka.jl
@@ -1,4 +1,5 @@
+using Knet: atype
 using Knet.KnetArrays: KnetArray
-param(x::KnetArray; atype=array_type[]) = Param(convert(atype,x))
+param(x::KnetArray; atype=atype()) = Param(convert(atype,x))
 update!(w::KnetArray, g, p) = gclip_update!(w, g, p)
 _optimizers(::KnetArray,otype; o...) = otype(;o...)
