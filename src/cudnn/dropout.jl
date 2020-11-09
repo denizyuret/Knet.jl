ad4389237b82badd65175c4231b5f6b07e4b5738
Deniz Yuret <denizyuret@gmail.com>
2020-11-09 12:06:08 +0300

    dropout done, rnn wip

--- src/cudnn/dropout.jl
+++ src/cudnn/dropout.jl
@@ -1,88 +1,9 @@
-using Knet.KnetArrays: DevArray
-using AutoGrad: AutoGrad, @primitive1, recording
-using CUDA: CuArray
-
-using CUDA.CUDNN: 
-   #cudnnDropoutForward,
-    cudnnDropoutBackward,
-    cudnnDropoutDescriptor_t,
-        cudnnCreateDropoutDescriptor,
-        cudnnSetDropoutDescriptor,
-        cudnnGetDropoutDescriptor,
-        cudnnRestoreDropoutDescriptor,
-        cudnnDestroyDropoutDescriptor,
-    cudnnDropoutGetStatesSize,
-    cudnnDropoutGetReserveSpaceSize,
-    handle
-
-
-cudnnDropoutForward(x; o...)                  = cudnnDropoutForwardWithDefaults(x; o...)
-cudnnDropoutForward(x, dropoutDesc; o...)     = cudnnDropoutForwardWithDefaults(x; dropoutDesc, o...)
-cudnnDropoutForward!(y, x; o...)              = cudnnDropoutForwardWithDefaults(x; y, o...)
-cudnnDropoutForward!(y, x, dropoutDesc; o...) = cudnnDropoutForwardWithDefaults(x; y, dropoutDesc, o...)
-
-
-function cudnnDropoutForwardWithDefaults(
-    x;
-    y = similar(x),
-    dropout::Real = 0.5,
-    dropoutDesc::cudnnDropoutDescriptor = cudnnDropoutDescriptor(dropout),
-    xDesc::cudnnTensorDescriptor = cudnnTensorDescriptor(x),
-    yDesc::cudnnTensorDescriptor = xDesc,
-    reserveSpace::DevArray = cudnnDropoutReserveSpace(xDesc)
-)
-    cudnnDropoutForwardAutoGrad(x; xDesc, y, yDesc, dropout, dropoutDesc, reserveSpace)
-end
-
-
-function cudnnDropoutForwardAutoGrad(x; xDesc, y, yDesc, dropout, dropoutDesc, reserveSpace)
-    if !recording()
-        @warn "cudnnDropoutForward called outside of training." maxlog=1
-    end
-    if cudnnDropoutSeed[] >= 0
-        # This is a very expensive call (40x dropout), so only use for debugging
-        @warn "Knet.CUDNN.cudnnDropoutSeed[] >= 0: calling expensive cudnnSetDropoutDescriptor" maxlog=1
-        @retry cudnnSetDropoutDescriptor(dropoutDesc, handle(), dropout, cudnnDropoutState[], sizeof(cudnnDropoutState[]), cudnnDropoutSeed[])
-    end
-    CUDA.CUDNN.cudnnDropoutForward(handle(), dropoutDesc, xDesc, x, yDesc, y, reserveSpace, sizeof(reserveSpace))
-    return y
-end
-
+import CUDA.CUDNN: cudnnDropoutForwardAutoGrad
+using CUDA.CUDNN: cudnnDropoutBackward
+using AutoGrad: AutoGrad, @primitive1, value
 
 @primitive1((cudnnDropoutForwardAutoGrad(x; xDesc, y, yDesc, dropout, dropoutDesc, reserveSpace),
              _dy, _y),
             ((x,y,dy,dx) = (value(x),value(_y),value(_dy),similar(x));
              cudnnDropoutBackward(handle(), dropoutDesc, yDesc, dy, xDesc, dx, reserveSpace, sizeof(reserveSpace));
              dx))
-
-
-function cudnnDropoutReserveSpace(td::cudnnTensorDescriptor)
-    # reserveSpace is ~1/8 of tensor size and passes info between forw and back
-    rss = Csize_t[0]; cudnnDropoutGetReserveSpaceSize(td, rss)
-    return cudnnWorkspace(rss[1])
-end
-
-
-# cudnnDropoutForward() doc says:
-# "This function should not be running concurrently with another cudnnDropoutForward() function using the same states."
-# So I am going to assume using a single buffer is fine until we have to deal with concurrency.
-# TODO: fix this based on default_rng in Random
-cudnnDropoutState = Ref{CuArray{Int128,1}}()
-
-# To debug gradients set cudnnDropoutSeed[] >= 0 which makes all dropout operations deterministic
-cudnnDropoutSeed = Ref{Int}(-1)
-
-
-const DD = cudnnDropoutDescriptor
-
-cudnnDropoutDescriptor(x) = cudnnDropoutDescriptor(convert(Cfloat,x), nothing) # fixes ambiguity issue
-
-function cudnnSetDropoutDescriptorFromFloat(ptr::cudnnDropoutDescriptor_t, dropout::Cfloat, ignore::Nothing)
-    if !isassigned(cudnnDropoutState)
-        ssize = Csize_t[0]; cudnnDropoutGetStatesSize(handle(), ssize)
-        @assert ssize[1] > 0
-        cudnnDropoutState[] = cudnnWorkspace(ssize[1])
-    end
-    seed = floor(Culonglong,time())
-    @retry cudnnSetDropoutDescriptor(ptr, handle(), dropout, cudnnDropoutState[], sizeof(cudnnDropoutState[]), seed)
-end
--- src/cudnn/rnn.jl
+++ src/cudnn/rnn.jl
@@ -1,184 +1,31 @@
-using AutoGrad: AutoGrad, @primitive1, value, recording, Arg
+import CUDA.CUDNN: cudnnRNNForwardAutoGrad
+using CUDA.CUDNN: cudnnRNNBackwardWeights_v8, cudnnRNNBackwardData_v8, handle, @retry_reclaim
+using AutoGrad: AutoGrad, @primitive1, value
 
-using CUDA.CUDNN:
-#cudnnRNNForward,
-cudnnRNNBackwardData_v8,
-cudnnRNNBackwardWeights_v8,
-cudnnRNNDescriptor_t,
-cudnnSetRNNDescriptor_v8,
-cudnnGetRNNWeightSpaceSize,
-cudnnGetRNNTempSpaceSizes,
-cudnnRNNAlgo_t,
-    CUDNN_RNN_ALGO_STANDARD,        # 0, robust performance across a wide range of network parameters
-    CUDNN_RNN_ALGO_PERSIST_STATIC,  # 1, fast when the first dimension of the input tensor is small (meaning, a small minibatch), cc>=6.0
-    CUDNN_RNN_ALGO_PERSIST_DYNAMIC, # 2, similar to static, optimize using the specific parameters of the network and active GPU, cc>=6.0
-    CUDNN_RNN_ALGO_COUNT,           # 3
-cudnnRNNMode_t,
-    CUDNN_RNN_RELU, # 0, /* basic RNN cell type with ReLu activation */
-    CUDNN_RNN_TANH, # 1, /* basic RNN cell type with tanh activation */
-    CUDNN_LSTM,     # 2, /* LSTM with optional recurrent projection and clipping */
-    CUDNN_GRU,      # 3, /* Using h' = tanh(r * Uh(t-1) + Wx) and h = (1 - z) * h' + z * h(t-1); */
-cudnnRNNBiasMode_t,
-    CUDNN_RNN_NO_BIAS,         # 0, /* rnn cell formulas do not use biases */
-    CUDNN_RNN_SINGLE_INP_BIAS, # 1, /* rnn cell formulas use one input bias in input GEMM */
-    CUDNN_RNN_DOUBLE_BIAS,     # 2, /* default, rnn cell formulas use two bias vectors */
-    CUDNN_RNN_SINGLE_REC_BIAS, # 3  /* rnn cell formulas use one recurrent bias in recurrent GEMM */
-cudnnDirectionMode_t,
-    CUDNN_UNIDIRECTIONAL, # 0, /* single direction network */
-    CUDNN_BIDIRECTIONAL,  # 1, /* output concatination at each layer */
-cudnnRNNInputMode_t,
-    CUDNN_LINEAR_INPUT, # 0, /* adjustable weight matrix in first layer input GEMM */
-    CUDNN_SKIP_INPUT,   # 1, /* fixed identity matrix in the first layer input GEMM */
-cudnnMathType_t,
-    CUDNN_DEFAULT_MATH,                    # 0,
-    CUDNN_TENSOR_OP_MATH,                  # 1,
-    CUDNN_TENSOR_OP_MATH_ALLOW_CONVERSION, # 2,
-    CUDNN_FMA_MATH,                        # 3,
-#/* For auxFlags in cudnnSetRNNDescriptor_v8() and cudnnSetRNNPaddingMode() */
-    CUDNN_RNN_PADDED_IO_DISABLED, # 0
-    CUDNN_RNN_PADDED_IO_ENABLED,  # (1U << 0)
-cudnnForwardMode_t,
-    CUDNN_FWD_MODE_INFERENCE, # 0
-    CUDNN_FWD_MODE_TRAINING,  # 1
-cudnnRNNDataDescriptor_t,
-cudnnSetRNNDataDescriptor,
-cudnnRNNDataLayout_t,
-    CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_UNPACKED,   # 0, /* padded, outer stride from one time-step to the next */
-    CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_PACKED,     # 1, /* sequence length sorted and packed as in basic RNN api */
-    CUDNN_RNN_DATA_LAYOUT_BATCH_MAJOR_UNPACKED, # 2, /* padded, outer stride from one batch to the next */
-cudnnWgradMode_t,
-    CUDNN_WGRAD_MODE_ADD, # 0, /* add partial gradients to wgrad output buffers */
-    CUDNN_WGRAD_MODE_SET, # 1, /* write partial gradients to wgrad output buffers */
-cudnnTensorDescriptor_t,
-handle
+# Set fwdMode to TRAINING when one of the inputs w, x, hx, cx is a Value
 
 
-#TODO: write docs
-cudnnRNNForward(w, x; o...)              = cudnnRNNForwardWithDefaults(w, x; o...)
-cudnnRNNForward(w, x, rnnDesc; o...)     = cudnnRNNForwardWithDefaults(w, x; rnnDesc, o...)
-cudnnRNNForward!(y, w, x; o...)          = cudnnRNNForwardWithDefaults(w, x; y, o...)
-cudnnRNNForward!(y, w, x, rnnDesc; o...) = cudnnRNNForwardWithDefaults(w, x; y, rnnDesc, o...)
 
+# Define gradients for the AutoGrad method
 
-function cudnnRNNForwardWithDefaults(
-    w, x;
-
-    # rnnDescriptor parameters
-    algo::cudnnRNNAlgo_t = CUDNN_RNN_ALGO_STANDARD,
-    cellMode::cudnnRNNMode_t = CUDNN_LSTM,
-    biasMode::cudnnRNNBiasMode_t = CUDNN_RNN_DOUBLE_BIAS,
-    dirMode::cudnnDirectionMode_t = CUDNN_UNIDIRECTIONAL,
-    inputMode::cudnnRNNInputMode_t = CUDNN_LINEAR_INPUT,
-    dataType::DataType = eltype(x),
-    mathPrec::DataType = dataType, # has to match dataType with one exception dt=Float16 => mp=Float16|Float32
-    mathType::cudnnMathType_t = cudnnRNNMathType(dataType),
-    inputSize::Integer = size(x,1),
-    hiddenSize::Integer, # TODO: this should be derivable from w/x and not mandatory when rnnDesc is provided
-    projSize::Integer = hiddenSize,
-    numLayers::Integer = 1,
-    dropout::Real = 0,
-    auxFlags::Integer = CUDNN_RNN_PADDED_IO_DISABLED, # When the padded I/O is enabled, layouts CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_UNPACKED and CUDNN_RNN_DATA_LAYOUT_BATCH_MAJOR_UNPACKED are permitted in RNN data descriptors.
-
-    # rnnDescriptor
-    rnnDesc::cudnnRNNDescriptor = cudnnRNNDescriptor(algo, cellMode, biasMode, dirMode, inputMode, cudnnDataType(dataType), cudnnDataType(mathPrec), mathType, Int32(inputSize), Int32(hiddenSize), Int32(projSize), Int32(numLayers), cudnnDropoutDescriptor(dropout), UInt32(auxFlags)),
-
-    # rnnData parameters
-    layout::cudnnRNNDataLayout_t = CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_UNPACKED, # padded [X,B,T] array
-    maxSeqLength::Integer = size(x,3),
-    batchSize::Integer = size(x,2),
-    xVectorSize::Integer = inputSize,
-    yVectorSize::Integer = projSize * (dirMode === CUDNN_UNIDIRECTIONAL ? 1 : 2),
-    hVectorSize::Integer = projSize,
-    cVectorSize::Integer = hiddenSize,
-    seqLengthArray::Vector{Cint} = fill(Cint(size(x,3)), size(x,2)),
-    paddingFill::Ptr{Cvoid} = C_NULL,
-
-    # rnnData descriptors
-    xDesc::cudnnRNNDataDescriptor = cudnnRNNDataDescriptor(cudnnDataType(dataType), layout, Cint(maxSeqLength), Cint(batchSize), Cint(xVectorSize), seqLengthArray, paddingFill),
-    yDesc::cudnnRNNDataDescriptor = cudnnRNNDataDescriptor(cudnnDataType(dataType), layout, Cint(maxSeqLength), Cint(batchSize), Cint(yVectorSize), seqLengthArray, paddingFill),
-    hDesc::cudnnTensorDescriptor = cudnnTensorDescriptor(CUDNN_TENSOR_NCHW, cudnnDataType(dataType), Cint(3), Cint[numLayers * (dirMode === CUDNN_UNIDIRECTIONAL ? 1 : 2), batchSize, hVectorSize]),
-    cDesc::cudnnTensorDescriptor = cudnnTensorDescriptor(CUDNN_TENSOR_NCHW, cudnnDataType(dataType), Cint(3), Cint[numLayers * (dirMode === CUDNN_UNIDIRECTIONAL ? 1 : 2), batchSize, cVectorSize]),
-
-    # rnnForward parameters
-    fwdMode::cudnnForwardMode_t = recording() ? CUDNN_FWD_MODE_TRAINING : CUDNN_FWD_MODE_INFERENCE,
-    devSeqLengths::DevArray{Cint,1} = CuArray(seqLengthArray),
-    workSpace::Union{DevArray,Nothing} = nothing,
-    reserveSpace::Union{DevArray,Nothing} = nothing,
-
-    # output buffers
-    y = similar(x, yVectorSize, size(x)[2:end]...),
-    hy = nothing,
-    cy = nothing,
-
-    # input hidden vectors
-    hx = nothing,
-    cx = nothing,
-
-    # gradient buffers
-    dw = (recording() ? similar(w) : nothing),
-    dx = (recording() ? similar(x) : nothing),
-    dhx = (recording() && hx !== nothing ? similar(hx) : nothing),
-    dcx = (recording() && cx !== nothing ? similar(cx) : nothing),
-)
-    wSize = cudnnRNNWeightSpaceSize(rnnDesc)
-    @assert sizeof(w) >= wSize "RNN weights should be at least $wSize bytes."
-    (wsSize, rsSize) = cudnnRNNTempSpaceSizes(rnnDesc, fwdMode, xDesc)
-    if wsSize > 0 && workSpace === nothing; workSpace = cudnnWorkspace(wsSize); end
-    if rsSize > 0 && reserveSpace === nothing; reserveSpace = cudnnWorkspace(rsSize); end
-    @assert sizeof(workSpace) >= wsSize  "worksSpace should be at least $wsSize bytes"
-    @assert sizeof(reserveSpace) >= rsSize  "reserveSpace should be at least $rsSize bytes"
-    if w isa Value || x isa Value || hx isa Value || cx isa Value
-        yhc = forw(cudnnRNNForwardAutoGrad, w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx)
-    else
-        yhc = cudnnRNNForwardAutoGrad(w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx)
-    end
-    return yhc[1]
-end
-
-
-function cudnnRNNForwardAutoGrad(w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx)
-    CUDA.CUDNN.cudnnRNNForward(handle(), rnnDesc, fwdMode, devSeqLengths, xDesc, x, yDesc, y, hDesc, cu_null(hx), cu_null(hy), cDesc, cu_null(cx), cu_null(cy), sizeof(w), w, sizeof(workSpace), cu_null(workSpace), sizeof(reserveSpace), cu_null(reserveSpace))
-    return (y, hy, cy)
-end
+@primitive1((cudnnRNNForwardAutoGrad(w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx),
+             dout,out),
+            (cudnnRNNBackward(dout, out, w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx); dw),
+            dx, dhx, dcx)
 
 
 # We do all the work during the backward pass for the first arg
 # The cudnnRNNBackwardData_v8() function must be called after cudnnRNNForward().
 # The cudnnRNNBackwardWeights_v8() function should be invoked after cudnnRNNBackwardData().
 
-function back(::typeof(cudnnRNNForwardAutoGrad), ::Type{Arg{1}}, dout, out, w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx)
+function cudnnRNNBackward(dout, out, w, x, hx, cx; rnnDesc, fwdMode, devSeqLengths, xDesc, yDesc, y, hDesc, hy, cDesc, cy, workSpace, reserveSpace, dw, dx, dhx, dcx)
     (_y,_hy,_cy) = value.(out)
     (dy,dhy,dcy) = value.(dout)
     (w,x,hx,cx) = value.((w,x,hx,cx))
-    cudnnRNNBackwardData_v8(handle(), rnnDesc, devSeqLengths, yDesc, _y, dy, xDesc, dx, hDesc, cu_null(hx), cu_null(dhy), cu_null(dhx), cDesc, cu_null(cx), cu_null(dcy), cu_null(dcx), sizeof(w), w, sizeof(workSpace), cu_null(workSpace), sizeof(reserveSpace), cu_null(reserveSpace))
+    @retry_reclaim (x->x!=CUDNN_STATUS_SUCCESS) cudnnRNNBackwardData_v8(handle(), rnnDesc, devSeqLengths, yDesc, _y, dy, xDesc, dx, hDesc, something(hx, CU_NULL), something(dhy, CU_NULL), something(dhx, CU_NULL), cDesc, something(cx, CU_NULL), something(dcy, CU_NULL), something(dcx, CU_NULL), sizeof(w), w, sizeof(workSpace), something(workSpace, CU_NULL), sizeof(reserveSpace), something(reserveSpace, CU_NULL))
     # Currently, the cudnnRNNBackwardWeights_v8() function supports the CUDNN_WGRAD_MODE_ADD mode only so the dweightSpace buffer should be zeroed by the user before invoking the routine for the first time. 
     addGrad = CUDNN_WGRAD_MODE_ADD 
     dw .= 0
-    cudnnRNNBackwardWeights_v8(handle(), rnnDesc, addGrad, devSeqLengths, xDesc, x, hDesc, cu_null(hx), yDesc, _y, sizeof(w), dw, sizeof(workSpace), cu_null(workSpace), sizeof(reserveSpace), cu_null(reserveSpace))
+    @retry_reclaim (x->x!=CUDNN_STATUS_SUCCESS) cudnnRNNBackwardWeights_v8(handle(), rnnDesc, addGrad, devSeqLengths, xDesc, x, hDesc, something(hx, CU_NULL), yDesc, _y, sizeof(w), dw, sizeof(workSpace), something(workSpace, CU_NULL), sizeof(reserveSpace), something(reserveSpace, CU_NULL))
     return dw
 end
-
-back(::typeof(cudnnRNNForwardAutoGrad), ::Type{Arg{2}}, x...; dx, o...) = dx
-back(::typeof(cudnnRNNForwardAutoGrad), ::Type{Arg{3}}, x...; dhx, o...) = dhx
-back(::typeof(cudnnRNNForwardAutoGrad), ::Type{Arg{4}}, x...; dcx, o...) = dcx
-
-
-function cudnnRNNWeightSpaceSize(rnnDesc::cudnnRNNDescriptor)
-    ws = Csize_t[0]
-    cudnnGetRNNWeightSpaceSize(handle(), rnnDesc, ws)
-    ws[1]
-end
-
-function cudnnRNNTempSpaceSizes(rnnDesc::cudnnRNNDescriptor, fwdMode::cudnnForwardMode_t, xDesc::cudnnRNNDataDescriptor)
-    ws = Csize_t[0]; rs = Csize_t[0]
-    cudnnGetRNNTempSpaceSizes(handle(), rnnDesc, fwdMode, xDesc, ws, rs)
-    ws[1], rs[1]
-end
-
-
-# TODO: test math types
-cudnnRNNMathType(::Type{Float16})=CUDNN_TENSOR_OP_MATH
-cudnnRNNMathType(::Type{Float32})=CUDNN_TENSOR_OP_MATH_ALLOW_CONVERSION
-cudnnRNNMathType(::Type{Float64})=CUDNN_DEFAULT_MATH
-
-
