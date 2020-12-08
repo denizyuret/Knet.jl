ae7471f71d8077f9f5629f8b6338052a1c8235ec
Deniz Yuret <denizyuret@gmail.com>
2020-12-08 23:25:37 +0300

    cudnn/softmax tests pass

--- src/cudnn/softmax.jl
+++ src/cudnn/softmax.jl
@@ -5,5 +5,6 @@ using AutoGrad: AutoGrad, @primitive1, value
 @primitive1((cudnnSoftmaxForwardAutoGrad(x; algo, mode, alpha, xDesc, beta, yDesc, y),
              _dy,_y),
             ((x,y,dy,dx) = (value(x),value(_y),value(_dy),similar(x));
+             if alpha[] != 1; y = y ./ alpha[]; end;
              cudnnSoftmaxBackward(handle(), algo, mode, alpha, yDesc, y, yDesc, dy, beta, xDesc, dx);
              dx))
--- test/cudnn/softmax.jl
+++ test/cudnn/softmax.jl
@@ -1,16 +1,34 @@
-using Test, AutoGrad
-using CUDA.CUDNN: CUDNN_SOFTMAX_FAST, CUDNN_SOFTMAX_ACCURATE, CUDNN_SOFTMAX_LOG, CUDNN_SOFTMAX_MODE_INSTANCE, CUDNN_SOFTMAX_MODE_CHANNEL
-#using Knet.CUDNN: cudnnSoftmaxForward
+using Test, CUDA, Knet, AutoGrad
+using CUDA.CUDNN:
+    cudnnSoftmaxForward,
+    cudnnSoftmaxForward!,
+    cudnnSoftmaxBackward,
+    cudnnSoftmaxAlgorithm_t,
+        CUDNN_SOFTMAX_FAST,     # 0, /* straightforward implementation */
+        CUDNN_SOFTMAX_ACCURATE, # 1, /* subtract max from every point to avoid overflow */
+        CUDNN_SOFTMAX_LOG,      # 2
+    cudnnSoftmaxMode_t,
+        CUDNN_SOFTMAX_MODE_INSTANCE, # 0, /* compute the softmax over all C, H, W for each N */
+        CUDNN_SOFTMAX_MODE_CHANNEL,  # 1  /* compute the softmax over all C for each H, W, N */
+    handle
 
 
 if CUDA.functional(); @testset "cudnn/softmax" begin
 
-    x = Param(CUDA.randn(Float64,10,10))
-    @test @gcheck cudnnSoftmaxForward(x, algo=CUDNN_SOFTMAX_FAST, mode=CUDNN_SOFTMAX_MODE_INSTANCE)
-    @test @gcheck cudnnSoftmaxForward(x, algo=CUDNN_SOFTMAX_ACCURATE, mode=CUDNN_SOFTMAX_MODE_INSTANCE)
-    @test @gcheck cudnnSoftmaxForward(x, algo=CUDNN_SOFTMAX_LOG, mode=CUDNN_SOFTMAX_MODE_INSTANCE)
-    @test @gcheck cudnnSoftmaxForward(x, algo=CUDNN_SOFTMAX_FAST, mode=CUDNN_SOFTMAX_MODE_CHANNEL)
-    @test @gcheck cudnnSoftmaxForward(x, algo=CUDNN_SOFTMAX_ACCURATE, mode=CUDNN_SOFTMAX_MODE_CHANNEL)
-    @test @gcheck cudnnSoftmaxForward(x, algo=CUDNN_SOFTMAX_LOG, mode=CUDNN_SOFTMAX_MODE_CHANNEL)
+    x = CUDA.randn(Float64,10,10)
+    x2 = Param(x)
+    x3 = Param(reshape(x, 10, 10, 1))
+    @test @gcheck cudnnSoftmaxForward(x2, alpha=1, algo=CUDNN_SOFTMAX_FAST,     mode=CUDNN_SOFTMAX_MODE_INSTANCE)
+    @test @gcheck cudnnSoftmaxForward(x2, alpha=2, algo=CUDNN_SOFTMAX_FAST,     mode=CUDNN_SOFTMAX_MODE_INSTANCE)
+    @test @gcheck cudnnSoftmaxForward(x2, alpha=1, algo=CUDNN_SOFTMAX_ACCURATE, mode=CUDNN_SOFTMAX_MODE_INSTANCE)
+    @test @gcheck cudnnSoftmaxForward(x2, alpha=2, algo=CUDNN_SOFTMAX_ACCURATE, mode=CUDNN_SOFTMAX_MODE_INSTANCE)
+    @test @gcheck cudnnSoftmaxForward(x2, alpha=1, algo=CUDNN_SOFTMAX_LOG,      mode=CUDNN_SOFTMAX_MODE_INSTANCE)
+    @test @gcheck cudnnSoftmaxForward(x2, alpha=2, algo=CUDNN_SOFTMAX_LOG,      mode=CUDNN_SOFTMAX_MODE_INSTANCE)
+    @test @gcheck cudnnSoftmaxForward(x3, alpha=1, algo=CUDNN_SOFTMAX_FAST,     mode=CUDNN_SOFTMAX_MODE_CHANNEL)
+    @test @gcheck cudnnSoftmaxForward(x3, alpha=2, algo=CUDNN_SOFTMAX_FAST,     mode=CUDNN_SOFTMAX_MODE_CHANNEL)
+    @test @gcheck cudnnSoftmaxForward(x3, alpha=1, algo=CUDNN_SOFTMAX_ACCURATE, mode=CUDNN_SOFTMAX_MODE_CHANNEL)
+    @test @gcheck cudnnSoftmaxForward(x3, alpha=2, algo=CUDNN_SOFTMAX_ACCURATE, mode=CUDNN_SOFTMAX_MODE_CHANNEL)
+    @test @gcheck cudnnSoftmaxForward(x3, alpha=1, algo=CUDNN_SOFTMAX_LOG,      mode=CUDNN_SOFTMAX_MODE_CHANNEL)
+    @test @gcheck cudnnSoftmaxForward(x3, alpha=2, algo=CUDNN_SOFTMAX_LOG,      mode=CUDNN_SOFTMAX_MODE_CHANNEL)
 
 end; end
