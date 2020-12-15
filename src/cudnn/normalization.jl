2442748f6001ea9798e821ca3db3ddc18285b17d
Deniz Yuret <denizyuret@gmail.com>
2020-12-15 23:02:05 +0300

    cudnn/normalization passes tests

--- src/cudnn/normalization.jl
+++ src/cudnn/normalization.jl
@@ -17,12 +17,12 @@ function cudnnNormalizationBack(dy, x, scale, bias, z; mean, variance, y, mode, 
     dready[] = true
     (x, scale, bias, z) = value.((x, scale, bias, z))
     # Allocate gradient buffers if necessary
-    if dx[] === nothing; dx[] = similar(x); end; @assert issimilar(x, dx[])
-    if dscale[] === nothing; dscale[] = similar(scale); end; @assert issimilar(scale, dscale[])
-    if dbias[] === nothing; dbias[] = similar(bias); end; @assert issimilar(bias, dbias[])
-    if z !== nothing && dz[] === nothing; dz[] = similar(z); end; @assert issimilar(z, dz[])
-    (alphaDataDiff, betaDataDiff, alphaParamDiff, betaParamDiff) = (a->scalingParameter(eltype(x),a)).((1,0,1,0))
-    cudnnNormalizationBackward(handle(), mode, normOps, algo, alphaDataDiff, betaDataDiff, alphaParamDiff, betaParamDiff, xDesc, x, yDesc, y, yDesc, dy, something(zDesc,C_NULL), something(dz[],CU_NULL), xDesc, dx[], normScaleBiasDesc, scale, bias, dscale[], dbias[], epsilon, normMeanVarDesc, something(savedMean[],CU_NULL), something(savedInvVariance[],CU_NULL), activationDesc, something(workspace[],CU_NULL), sizeof(workspace[]), something(reserveSpace[],CU_NULL), sizeof(reserveSpace[]), groupCnt)
+    !isassigned(dx) ?     dx[]     = similar(x) :     @assert issimilar(x, dx[])
+    !isassigned(dscale) ? dscale[] = similar(scale) : @assert issimilar(scale, dscale[])
+    !isassigned(dbias) ?  dbias[]  = similar(bias) :  @assert issimilar(bias, dbias[])
+    z === nothing ? dz[] = nothing : !isassigned(dz) ? dz[] = zero(z) : (@assert issimilar(z, dz[]); dz[] .= 0) # z may not be used, dz may not be modified, should be zeroed
+    s0,s1 = scalingParameter(eltype(x),0),scalingParameter(eltype(x),1)
+    cudnnNormalizationBackward(handle(), mode, normOps, algo, alpha, s0, alpha, s0, xDesc, x, yDesc, y, yDesc, dy, something(zDesc,C_NULL), something(dz[],CU_NULL), xDesc, dx[], normScaleBiasDesc, scale, bias, dscale[], dbias[], epsilon, something(normMeanVarDesc,C_NULL), something(savedMean,CU_NULL), something(savedInvVariance,CU_NULL), something(activationDesc,C_NULL), something(workspace,CU_NULL), sizeof(workspace), something(reserveSpace,CU_NULL), sizeof(reserveSpace), groupCnt)
 end
 
 
@@ -35,26 +35,27 @@ function forw(f::typeof(cudnnNormalizationForwardAutoGrad), x, scale, bias, z; m
     @assert novalue !== args    # there should be some tracked args for forw to be called (maybe Params)
     x, scale, bias, z = novalue
     if recording()              # we are taking gradients
-        if savedMean[] === nothing; savedMean[] = similar(mean); end; @assert issimilar(mean, savedMean[])
-        if savedInvVariance[] === nothing; savedInvVariance[] = similar(variance); end; @assert issimilar(variance, savedInvVariance[])
-        workspaceSize, reserveSpaceSize = cudnnNormalizationTempSpaceSizes(mode, normOps, algo, xDesc, zDesc, yDesc, normScaleBiasDesc, activationDesc, normMeanVarDesc, workspaceSize, groupCnt)
+        mean === nothing ? savedMean = nothing : savedMean === nothing ? savedMean = similar(mean) : @assert issimilar(mean, savedMean)
+        variance === nothing ? savedInvVariance = nothing : savedInvVariance === nothing ? savedInvVariance = similar(variance) : @assert issimilar(variance, savedInvVariance)
+        workspaceSize, reserveSpaceSize = cudnnNormalizationTempSpaceSizes(mode, normOps, algo, xDesc, zDesc, yDesc, normScaleBiasDesc, activationDesc, normMeanVarDesc, groupCnt)
         if reserveSpaceSize > 0 && reserveSpace === nothing; reserveSpace = cudnnTempSpace(reserveSpaceSize); end
         @assert sizeof(reserveSpace) >= reserveSpaceSize  "reserveSpace should be at least $(reserveSpaceSize) bytes"
         if workspaceSize > 0 && workspace === nothing; workspace = cudnnTempSpace(workspaceSize); end
         @assert sizeof(workspace) >= workspaceSize  "workspace should be at least $(workspaceSize) bytes"
-        cudnnNormalizationForwardTraining(handle(), mode, normOps, algo, alpha, beta, xDesc, x, normScaleBiasDesc, scale, bias, exponentialAverageFactor, normMeanVarDesc, something(mean,CU_NULL), something(variance,CU_NULL), epsilon, something(savedMean[],CU_NULL), something(savedInvVariance[],CU_NULL), activationDesc, zDesc, z, yDesc, y, something(workspace,CU_NULL), sizeof(workspace), something(reserveSpace,CU_NULL), sizeof(reserveSpace), groupCnt)
+        cudnnNormalizationForwardTraining(handle(), mode, normOps, algo, alpha, beta, xDesc, x, normScaleBiasDesc, scale, bias, exponentialAverageFactor, something(normMeanVarDesc,C_NULL), something(mean,CU_NULL), something(variance,CU_NULL), epsilon, something(savedMean,CU_NULL), something(savedInvVariance,CU_NULL), something(activationDesc,C_NULL), something(zDesc,C_NULL), something(z,CU_NULL), yDesc, y, something(workspace,CU_NULL), sizeof(workspace), something(reserveSpace,CU_NULL), sizeof(reserveSpace), groupCnt)
         y = Result(y, f, args, (; mean, variance, y, mode, normOps, algo, alpha, beta, epsilon, groupCnt, exponentialAverageFactor, savedMean, savedInvVariance, activationDesc, xDesc, yDesc, zDesc, normScaleBiasDesc, normMeanVarDesc, workspace, reserveSpace, dx, dscale, dbias, dz, dready))
     else                        # we are not taking gradients
-        cudnnNormalizationForwardInference(handle(), mode, normOps, algo, alpha, beta, xDesc, x, normScaleBiasDesc, scale, bias, normMeanVarDesc, something(mean,CU_NULL), something(variance,CU_NULL), zDesc, z, activationDesc, yDesc, y, epsilon, groupCnt)
+        @assert mean !== nothing && variance !== nothing && normMeanVarDesc !== nothing "normalization mean and variance are required in inference mode."
+        cudnnNormalizationForwardInference(handle(), mode, normOps, algo, alpha, beta, xDesc, x, normScaleBiasDesc, scale, bias, normMeanVarDesc, mean, variance, something(zDesc,C_NULL), something(z,CU_NULL), something(activationDesc,C_NULL), yDesc, y, epsilon, groupCnt)
     end
     return y
 end
 
 
 # Helper functions
-function cudnnNormalizationTempSpaceSizes(mode, normOps, algo, xDesc, zDesc, yDesc, normScaleBiasDesc, activationDesc, normMeanVarDesc, workspaceSize, groupCnt)
-    workspaceSize = Ref{Csize_t}(0); reserveSpaceSize = Ref{Csize_t}(0)
-    cudnnGetNormalizationForwardTrainingWorkspaceSize(handle(), mode, normOps, algo, xDesc, zDesc, yDesc, normScaleBiasDesc, activationDesc, normMeanVarDesc, workspaceSize, groupCnt)
-    cudnnGetNormalizationTrainingReserveSpaceSize(handle(), mode, normOps, algo, activationDesc, xDesc, reserveSpaceSize, groupCnt)
+function cudnnNormalizationTempSpaceSizes(mode, normOps, algo, xDesc, zDesc, yDesc, normScaleBiasDesc, activationDesc, normMeanVarDesc, groupCnt)
+    workspaceSize, reserveSpaceSize = Ref{Csize_t}(0), Ref{Csize_t}(0)
+    cudnnGetNormalizationForwardTrainingWorkspaceSize(handle(), mode, normOps, algo, xDesc, something(zDesc,C_NULL), yDesc, normScaleBiasDesc, something(activationDesc,C_NULL), something(normMeanVarDesc,C_NULL), workspaceSize, groupCnt)
+    cudnnGetNormalizationTrainingReserveSpaceSize(handle(), mode, normOps, algo, something(activationDesc,C_NULL), xDesc, reserveSpaceSize, groupCnt)
     workspaceSize[], reserveSpaceSize[]
 end
---
+++ test/cudnn/normalization.jl
@@ -0,0 +1,69 @@
+using Test, Random, CUDA, Knet, AutoGrad, Statistics
+
+using CUDA.CUDNN:
+    cudnnNormalizationForward,
+    cudnnNormalizationForward!,
+    cudnnNormalizationForwardInference,
+    cudnnNormalizationForwardTraining,
+    cudnnNormalizationBackward,
+    cudnnActivationDescriptor,
+    cudnnNormMode_t,
+        CUDNN_NORM_PER_ACTIVATION, # 0, bnScale, bnBias tensor dims are 1xCxHxWx.. (one value per CHW...-slice, normalized over N slice)
+        CUDNN_NORM_PER_CHANNEL,    # 1, bnScale, bnBias tensor dims are 1xCx1x1 (one value per C-dim normalized over Nx1xHxW subtensors)
+    cudnnNormOps_t,
+        CUDNN_NORM_OPS_NORM,                # 0, /* do normalization only */
+        CUDNN_NORM_OPS_NORM_ACTIVATION,     # 1, /* do Norm, then activation */
+        CUDNN_NORM_OPS_NORM_ADD_ACTIVATION, # 2, /* do Norm, then elemWiseAdd, then activation */
+    cudnnNormAlgo_t,
+        CUDNN_NORM_ALGO_STANDARD, # 0
+        CUDNN_NORM_ALGO_PERSIST,  # 1
+    cudnnActivationMode_t,
+        CUDNN_ACTIVATION_SIGMOID,      # 0
+        CUDNN_ACTIVATION_RELU,         # 1
+        CUDNN_ACTIVATION_TANH,         # 2
+        CUDNN_ACTIVATION_CLIPPED_RELU, # 3
+        CUDNN_ACTIVATION_ELU,          # 4
+        CUDNN_ACTIVATION_IDENTITY,     # 5
+    cudnnNanPropagation_t,
+        CUDNN_NOT_PROPAGATE_NAN, # 0
+        CUDNN_PROPAGATE_NAN,     # 1
+    cudnnTensorFormat_t,
+        CUDNN_TENSOR_NCHW,        # 0, /* row major (wStride = 1, hStride = w) */
+        CUDNN_TENSOR_NHWC,        # 1, /* feature maps interleaved ( cStride = 1 )*/
+        CUDNN_TENSOR_NCHW_VECT_C, # 2, /* each image point is vector of element of C, vector length in data type */
+    handle
+
+
+@testset "cudnn/normalization" begin
+
+    x, s, b, s2, b2, s3, b3 = (Param(CUDA.randn(Float64,x...)) for x in ((5,4,3,2),(1,1,3,1),(1,1,3,1),(5,4,3,1),(5,4,3,1),(5,1,1,1),(5,1,1,1)))
+
+    # @gcheck needs Inference and Training to compute the same function
+    # For this they need to use the same mean/var
+    # @gcheck sums the output of non-scalar functions by default
+    # This has the effect of multiplying scale with a 0 mean array
+    # So we'll use getindex(y,1) instead
+    function normtest(x,s,b; dims=(1,2,4), o...)
+        m = mean(value(x);dims)
+        v = var(value(x);mean=m,corrected=false,dims)
+        y = cudnnNormalizationForward(x,s,b,m,v; o...)
+        y[1]
+    end
+
+    @test normtest(x,s,b) ≈ value(@diff(normtest(x,s,b)))
+    @test @gcheck normtest(x,s,b)
+    @test @gcheck normtest(x,s2,b2; mode = CUDNN_NORM_PER_ACTIVATION, dims=4)
+    @test @gcheck normtest(x,s,b; algo = CUDNN_NORM_ALGO_PERSIST)
+    @test @gcheck normtest(x,s3,b3; algo = CUDNN_NORM_ALGO_PERSIST, format = CUDNN_TENSOR_NHWC, dims=(2,3,4))
+    @test @gcheck normtest(x,s,b; alpha = 2)
+    @test @gcheck normtest(x,s,b; epsilon = 0)
+    @test @gcheck normtest(x,s3,b3; format = CUDNN_TENSOR_NHWC, dims=(2,3,4))
+    @test @gcheck normtest(x,s,b; exponentialAverageFactor = 0.01)
+    @test @gcheck normtest(x,s,b; savedMean = similar(s))
+    @test @gcheck normtest(x,s,b; savedInvVariance = similar(s))
+    # cudnn-8.0.5: Currently, CUDNN_NORM_OPS_NORM_ACTIVATION and CUDNN_NORM_OPS_NORM_ADD_ACTIVATION are not supported.
+    #@test_skip @gcheck normtest(x,s,b; normOps = CUDNN_NORM_OPS_NORM_ACTIVATION, activationMode = CUDNN_ACTIVATION_RELU)
+    #@test_skip @gcheck normtest(x,s,b; normOps = CUDNN_NORM_OPS_NORM_ADD_ACTIVATION, activationMode = CUDNN_ACTIVATION_RELU, z)
+    #@test_skip @gcheck normtest(x,s,b; groupCnt = 2) # Currently only groupCnt=1 is supported
+    #@test_skip @gcheck normtest(x,s,b; beta = 2) # we don't use beta in functional code
+end
