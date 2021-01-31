a63e258194a4346f06676ce1ef76ac957714cfd1
Deniz Yuret <denizyuret@gmail.com>
2021-01-31 21:53:51 +0300

    Fix #653: Knet.@save macro

--- src/knetarrays/jld2.jl
+++ src/knetarrays/jld2.jl
@@ -9,8 +9,8 @@ JLD2.rconvert(::Type{KnetArray{T,N}}, x::JLD2KnetArray{T,N}) where {T,N} = KnetA
 # For people who use Knet.load, Knet.@save etc.
 save(x...; o...) = FileIO.save(x...; o...)
 load(x...; o...) = FileIO.load(x...; o...)
-macro save(x...); a=:(JLD2.@save); append!(a.args,x); a; end
-macro load(x...); a=:(JLD2.@load); append!(a.args,x); a; end
+macro save(x...); a=:(JLD2.@save); append!(a.args,x); esc(a); end
+macro load(x...); a=:(JLD2.@load); append!(a.args,x); esc(a); end
 
 # To load a file saved with Knet-1.4.3 or earlier:
 load143(x...; o...)=jld2serialize(FileIO.load(x...; o...))
