e9ce1d48e99896570b5ded8d171226d7ec31db1b
Deniz Yuret <denizyuret@gmail.com>
2021-02-27 22:05:04 +0300

    python imagenet eval scripts

---
+++ src/models21/tf-eval.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+validation_dir="/datasets/ImageNet/ILSVRC/Data/CLS-LOC/val" #"val"
+BATCH_SIZE = 32
+IMG_SIZE = (224,224)
+
+ds = tf.keras.preprocessing.image_dataset_from_directory(
+    validation_dir,
+    shuffle=False,
+    batch_size=BATCH_SIZE,
+    image_size=IMG_SIZE)
+
+def resize_with_crop(image, label):
+    i = image
+    i = tf.cast(i, tf.float32)
+    i = tf.image.resize_with_crop_or_pad(i, 224, 224)
+    i = tf.keras.applications.mobilenet_v2.preprocess_input(i)
+    return (i, label)
+
+# Preprocess the images
+ds = ds.map(resize_with_crop)
+
+# Compile the model
+pretrained_model = tf.keras.applications.MobileNetV2(include_top=True, 
+                                                     weights='imagenet')
+pretrained_model.trainable = False
+pretrained_model.compile(optimizer='adam', 
+                         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+                         metrics=['accuracy'])
+decode_predictions = tf.keras.applications.mobilenet_v2.decode_predictions
+
+# Print Accuracy
+result = pretrained_model.evaluate(ds)
+print(dict(zip(pretrained_model.metrics_names, result)))
