4ebc35d1c07b94916c4e971f4c2ac376e2245460
Deniz Yuret <denizyuret@gmail.com>
2021-03-03 23:05:50 +0300

    consolidated mobilenet models

--- src/models21/Models21.jl
+++ src/models21/Models21.jl
@@ -2,6 +2,4 @@ include("deepmap.jl")
 include("fileio.jl")
 include("imagenet.jl")
 include("resnet.jl"); export ResNet
-include("mobilenet_v1.jl"); export MobileNetV1
-include("mobilenet_v2.jl"); export MobileNetV2
-include("mobilenet_v3.jl"); export MobileNetV3
+include("mobilenet.jl"); export MobileNet
---
+++ src/models21/mobilenet.jl
@@ -0,0 +1,220 @@
+export MobileNet
+
+import Knet
+using Knet.Layers21: Conv, BatchNorm, Linear, Sequential, Op, Residual, SqueezeExcitation
+using Knet.Ops21: relu, hardswish, hardsigmoid
+using Knet.Ops20: pool
+using Artifacts
+
+
+"""
+--DRAFT--
+
+    MobileNet(; kwargs...)
+    MobileNet(name::String; pretrained=true)
+
+Return a MobileNet model. The first call above returns a randomly initialized model, the
+second loads a pretrained model.
+
+Pretrained models:
+
+    name                           size  time  top1  settings
+    ----                           ----  ----  ----  --------
+    mobilenet_v1_100_224_tf
+    mobilenet_v2_100_224_tf
+    mobilenet_v2_100_224_pt
+    mobilenet_v3_large_100_224_pt
+    mobilenet_v3_small_100_224_pt
+
+Keyword arguments:
+
+
+References:
+* https://arxiv.org/abs/1704.04861
+* https://arxiv.org/abs/1801.04381
+* https://arxiv.org/abs/1905.02244
+
+"""
+function MobileNet(
+    ;
+    width = 1,
+    resolution = 224,
+    input = 32,
+    output = (),
+    classes = 1000,
+    activation = relu6,
+    tfpadding = false,          # torch:1 keras:((0,1),(0,1)); torch:2, keras:((1,2),(1,2))
+    bnupdate = 0.1,             # torch:0.1, keras.MobileNetV1:0.01, keras.MobileNetV2:0.001
+    bnepsilon = 1e-5,           # torch:1e-5, keras:0.001
+    dropout = 0.001,
+    block = mobilenet_v3_block,
+    layout = mobilenet_v3_large_layout,
+    preprocess = torch_mobilenet_preprocess, # (keras|torch)_mobilenet_preprocess
+)
+    global _mobilenet_config = (; tfpadding, bnupdate, bnepsilon)
+    α(x) = round(Int, width*x)
+    s = Sequential()
+    push!(s, preprocess(resolution))
+    push!(s, conv_bn(3, 3, 3, α(input); stride=2, activation))
+    channels = input
+    for l in layout
+        for r in 1:l.repeat
+            push!(s, block(channels, l.output; layout=l, repeat=r, width))
+            channels = l.output
+        end
+    end
+    if isempty(output)
+        push!(s, adaptive_avg_pool)
+        push!(s, Linear(α(channels), classes; binit=zeros, dropout))
+    else
+        push!(s, conv_bn(1, 1, α(channels), α(output[1]); activation))
+        push!(s, adaptive_avg_pool)
+        for o in 2:length(output)
+            push!(s, Linear(α(output[o-1]), α(output[o]); binit=zeros, activation))
+        end
+        push!(s, Linear(α(output[end]), classes; binit=zeros, dropout))
+    end
+    return s
+end    
+
+
+torch_mobilenet_preprocess(resolution) = Op(imagenet_preprocess; normalization="torch", format="whcn", resolution)
+keras_mobilenet_preprocess(resolution) = Op(imagenet_preprocess; normalization="tf", format="whcn", resolution)
+
+
+function conv_bn(w,h,x,y; groups=1, stride=1, activation=nothing)
+    c = _mobilenet_config
+    p = (w-1)÷2
+    padding = (w > 1 && stride > 1 && c.tfpadding ? ((p-1,p),(p-1,p)) : p)
+    normalization=BatchNorm(; update=c.bnupdate, epsilon=c.bnepsilon)
+    Conv(w,h,x,y; normalization, groups, stride, padding, activation)
+end
+
+
+function adaptive_avg_pool(x)
+    y = pool(x; mode=1, window=size(x)[1:end-2])
+    reshape(y, size(y,3), size(y,4))
+end
+
+
+function relu6(x)
+    relu(x; max_value=6)
+end
+
+
+function mobilenet_v1_block(x, y; layout, repeat, width)
+    stride = (repeat == 1 ? layout.stride : 1)
+    activation = relu6
+    x, y = round(Int,width*x), round(Int,width*y)
+    Sequential(
+        conv_bn(3, 3, 1, x; groups=x, stride, activation),
+        conv_bn(1, 1, x, y; activation),
+    )
+end
+
+
+const mobilenet_v1_layout = (
+    (repeat=1, output=64, stride=1),
+    (repeat=2, output=128, stride=2),
+    (repeat=2, output=256, stride=2),
+    (repeat=6, output=512, stride=2),
+    (repeat=2, output=1024, stride=2),
+)
+
+
+function mobilenet_v2_block(x, y; layout, repeat, width)
+    stride = (repeat == 1 ? layout.stride : 1)
+    activation = relu6
+    x, y = round(Int,width*x), round(Int,width*y)
+    b = layout.expansion * x
+    s = Sequential()
+    b != x && push!(s, conv_bn(1, 1, x, b; activation))
+    push!(s, conv_bn(3, 3, 1, b; groups=b, stride, activation))
+    push!(s, conv_bn(1, 1, b, y))
+    x == y ? Residual(s) : s
+end
+
+
+const mobilenet_v2_layout = (
+    (repeat=1, output=16, stride=1, expansion=1),
+    (repeat=2, output=24, stride=2, expansion=6),
+    (repeat=3, output=32, stride=2, expansion=6),
+    (repeat=4, output=64, stride=2, expansion=6),
+    (repeat=3, output=96, stride=1, expansion=6),
+    (repeat=3, output=160, stride=2, expansion=6),
+    (repeat=1, output=320, stride=1, expansion=6),
+)
+
+
+function mobilenet_v3_block(input, output; layout, repeat, width)
+    stride = (repeat == 1 ? layout.stride : 1)
+    kernel, activation = layout.kernel, layout.activation
+    input, output, expand, squeeze = (x->round(Int,width*x)).((input, output, layout.expand, layout.squeeze))
+    s = Sequential()
+    channels = input
+    if expand > 0
+        push!(s, conv_bn(1, 1, channels, expand; activation))
+        channels = expand
+    end
+    push!(s, conv_bn(kernel, kernel, 1, channels; activation, groups=channels, stride))
+    if squeeze > 0
+        push!(s, SqueezeExcitation(
+            Conv(1, 1, channels, squeeze; binit=zeros, activation=relu),
+            Conv(1, 1, squeeze, channels; binit=zeros, activation=hardsigmoid),
+        ))
+    end
+    push!(s, conv_bn(1, 1, channels, output))
+    return input == output && stride == 1 ? Residual(s) : s
+end
+
+
+mobilenet_v3_large_layout = (
+    # first input=16, last output through 160=>960 hardswish before pool, then linear 960=>1280=>1000
+    (repeat=1, input=16, expand=0, squeeze=0, output=16, kernel=3, stride=1, activation=relu),
+    (repeat=1, input=16, expand=64, squeeze=0, output=24, kernel=3, stride=2, activation=relu),
+    (repeat=1, input=24, expand=72, squeeze=0, output=24, kernel=3, stride=1, activation=relu),
+    (repeat=1, input=24, expand=72, squeeze=24, output=40, kernel=5, stride=2, activation=relu),
+    (repeat=2, input=40, expand=120, squeeze=32, output=40, kernel=5, stride=1, activation=relu),
+    (repeat=1, input=40, expand=240, squeeze=0, output=80, kernel=3, stride=2, activation=hardswish),
+    (repeat=1, input=80, expand=200, squeeze=0, output=80, kernel=3, stride=1, activation=hardswish),
+    (repeat=2, input=80, expand=184, squeeze=0, output=80, kernel=3, stride=1, activation=hardswish),
+    (repeat=1, input=80, expand=480, squeeze=120, output=112, kernel=3, stride=1, activation=hardswish),
+    (repeat=1, input=112, expand=672, squeeze=168, output=112, kernel=3, stride=1, activation=hardswish),
+    (repeat=1, input=112, expand=672, squeeze=168, output=160, kernel=5, stride=2, activation=hardswish),
+    (repeat=2, input=160, expand=960, squeeze=240, output=160, kernel=5, stride=1, activation=hardswish),
+)
+
+
+mobilenet_v3_small_layout = (
+    # first input=16, last output through 96=>576 hardswish before pool, then linear 576=>1024=>1000
+    (repeat=1, input=16, expand=0, squeeze=8, output=16, kernel=3, stride=2, activation=relu),
+    (repeat=1, input=16, expand=72, squeeze=0, output=24, kernel=3, stride=2, activation=relu),
+    (repeat=1, input=24, expand=88, squeeze=0, output=24, kernel=3, stride=1, activation=relu),
+    (repeat=1, input=24, expand=96, squeeze=24, output=40, kernel=5, stride=2, activation=hardswish),
+    (repeat=2, input=40, expand=240, squeeze=64, output=40, kernel=5, stride=1, activation=hardswish),
+    (repeat=1, input=40, expand=120, squeeze=32, output=48, kernel=5, stride=1, activation=hardswish),
+    (repeat=1, input=48, expand=144, squeeze=40, output=48, kernel=5, stride=1, activation=hardswish),
+    (repeat=1, input=48, expand=288, squeeze=72, output=96, kernel=5, stride=2, activation=hardswish),
+    (repeat=2, input=96, expand=576, squeeze=144, output=96, kernel=5, stride=1, activation=hardswish),
+)
+
+
+## Pretrained models
+
+mobilenet_models = Dict{String,NamedTuple}(
+    "mobilenet_v1_100_224_tf" => (width=1, resolution=224, input=32, output=(), classes=1000, activation=relu6, tfpadding=true, bnupdate=0.01, bnepsilon=0.001, dropout=0.001, block=mobilenet_v1_block, layout=mobilenet_v1_layout, preprocess=keras_mobilenet_preprocess),
+    "mobilenet_v2_100_224_tf" => (width=1, resolution=224, input=32, output=1280, classes=1000, activation=relu6, tfpadding=true, bnupdate=0.001, bnepsilon=0.001, dropout=0.2, block=mobilenet_v2_block, layout=mobilenet_v2_layout, preprocess=keras_mobilenet_preprocess),
+    "mobilenet_v2_100_224_pt" => (width=1, resolution=224, input=32, output=1280, classes=1000, activation=relu6, tfpadding=false, bnupdate=0.1, bnepsilon=1e-5, dropout=0.2, block=mobilenet_v2_block, layout=mobilenet_v2_layout, preprocess=torch_mobilenet_preprocess),
+    "mobilenet_v3_large_100_224_pt" => (width=1, resolution=224, input=16, output=(960,1280), classes=1000, activation=hardswish, tfpadding=false, bnupdate=0.01, bnepsilon=0.001, dropout=0.2, block=mobilenet_v3_block, layout=mobilenet_v3_large_layout, preprocess=torch_mobilenet_preprocess),
+    "mobilenet_v3_small_100_224_pt" => (width=1, resolution=224, input=16, output=(576,1024), classes=1000, activation=hardswish, tfpadding=false, bnupdate=0.01, bnepsilon=0.001, dropout=0.2, block=mobilenet_v3_block, layout=mobilenet_v3_small_layout, preprocess=torch_mobilenet_preprocess),
+)
+
+function MobileNet(s::String; pretrained=true)
+    @assert haskey(mobilenet_models, s)  "Please choose from known MobileNet models:\n$(collect(keys(mobilenet_models)))"
+    kwargs = mobilenet_models[s]
+    model = MobileNet(; kwargs...)
+    res = kwargs.resolution
+    model(Knet.atype(zeros(Float32,res,res,3,1)))
+    pretrained && setweights!(model, joinpath(@artifact_str(s), "$s.jld2"))
+    return model
+end
