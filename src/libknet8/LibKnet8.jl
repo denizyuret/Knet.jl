ee2b77425ad3888bb96090735319a28108dcfb2d
Manuel Waechtershaeuser <m.waechtershaeuser@web.de>
2022-06-08 20:12:11 +0200

    solve mergeconflict

--- src/libknet8/LibKnet8.jl
+++ src/libknet8/LibKnet8.jl
@@ -4,7 +4,7 @@ module LibKnet8
 
 export libknet8, @knet8, @knet8r, gpu
 using CUDA, Libdl, Pkg.Artifacts
-const libknet8 = Libdl.find_library(["libknet8"], @static (Base.Sys.isapple() && Base.Sys.ARCH==:aarch64) ? [] : [artifact"libknet8"])
+const libknet8 = Libdl.find_library(["libknet8"], try [artifact"libknet8"]; catch ex; []; end)
 #DBG const libknet8 = Libdl.find_library(["libknet8"], [@__DIR__])
 
 include("ops.jl")
